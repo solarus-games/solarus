cf53b7ef5ebb78e7e0fbf900209cc319c87f2bbe
Christopho <christopho@solarus-engine.org>
2007-09-06 10:36:01 +0000

    Forgot those files
    git-svn-id: svn://solarus-engine.org/solarus@95 08e91b48-8f19-0410-8fc9-9db6b7bce602

---
+++ include/maps/MapLinkHouseSecretRoom.h
@@ -0,0 +1,18 @@
+#ifndef ZSDX_MAP_LINKHOUSESECRETROOM_H
+#define ZSDX_MAP_LINKHOUSESECRETROOM_H
+
+#include "Map.h"
+
+/*
+ * Map generated automatically by the tileset editor.
+ */
+class MapLinkHouseSecretRoom: public Map {
+
+ public:
+  MapLinkHouseSecretRoom(void);
+  inline ~MapLinkHouseSecretRoom(void) { }
+
+  void load(void);
+};
+
+#endif
---
+++ include/maps/MapRupeeHouse.h
@@ -0,0 +1,18 @@
+#ifndef ZSDX_MAP_RUPEEHOUSE_H
+#define ZSDX_MAP_RUPEEHOUSE_H
+
+#include "Map.h"
+
+/*
+ * Map generated automatically by the tileset editor.
+ */
+class MapRupeeHouse: public Map {
+
+ public:
+  MapRupeeHouse(void);
+  inline ~MapRupeeHouse(void) { }
+
+  void load(void);
+};
+
+#endif

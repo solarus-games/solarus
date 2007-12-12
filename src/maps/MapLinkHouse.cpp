c58a945880a13bd794db5fe14571fed6bf72a831
Christopho <christopho@solarus-engine.org>
2007-12-12 18:44:10 +0000

    Fix link's initial position
    git-svn-id: svn://solarus-engine.org/solarus@141 08e91b48-8f19-0410-8fc9-9db6b7bce602

--- src/maps/MapLinkHouse.cpp
+++ src/maps/MapLinkHouse.cpp
@@ -12,7 +12,7 @@ void MapLinkHouse::load(void) {
 
 #include "../src/maps/MapLinkHouseEntities.inc"
 
-  ENTRANCE(LAYER_LOW, 128, 216, 1);
+  ENTRANCE(LAYER_LOW, 128, 200, 1);
   ENTRANCE(LAYER_LOW, 352, 128, 2);
 
   EXIT(LAYER_LOW, 120, 240, 16, 16, MAP_LINKHOUSESECRETROOM, 0);

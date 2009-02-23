b17b08c5b4f3d851dbb5a7c186f28152055ebf39
Christopho <christopho@solarus-engine.org>
2009-02-23 07:12:57 +0000

    Forgot 2 files
    git-svn-id: svn://solarus-engine.org/solarus@706 08e91b48-8f19-0410-8fc9-9db6b7bce602

---
+++ include/movements/PathMovement.h
@@ -0,0 +1,46 @@
+#ifndef ZSDX_PATH_MOVEMENT_H
+#define ZSDX_PATH_MOVEMENT_H
+
+#include "Common.h"
+#include "movements/MovementWithCollision.h"
+
+/**
+ * Movement for an entity that follows a path.
+ * The path is a succession of basic moves, where
+ * each basic move is an 8-pixel movement in one
+ * of the 8 main directions.
+ */
+class PathMovement: public MovementWithCollision {
+
+ private:
+
+  const string path;          /**< the path: each character is a direction ('0' to '7') */
+  int current_direction;      /**< direction of the current move (0 to 7) */
+  unsigned int next_index;    /**< index of the next move in the path */
+  int distance_covered_x;     /**< counts the number of x pixels covered during the current move */
+  int distance_covered_y;     /**< counts the number of y pixels covered during the current move */
+
+  const bool loop;            /**< should the movement return to the beginning once finished?  */
+  const bool with_collisions; /**< indicates whether this movement is sensitive
+			       * to the obstacles */
+
+  bool finished;              /**< indicates that the path is finished
+			       * (only possible if loop is false) */
+
+ public:
+
+  PathMovement(Map *map, string path, int speed,
+	       bool loop, bool with_collisions);
+
+  ~PathMovement(void);
+
+  bool is_finished(void);
+
+  int get_current_direction(void);
+  void start_next_move(void);
+  virtual void set_x(int x);
+  virtual void set_y(int y);
+
+};
+
+#endif
---
+++ src/movements/PathMovement.cpp
@@ -0,0 +1,104 @@
+#include "movements/PathMovement.h"
+
+/**
+ * Creates a path movement object.
+ * @param map the map (can be NULL if with_collisions is false)
+ * @param path the succession of basic moves
+ * composing this movement (each character represents
+ * a direction between '0' and '7')
+ * @param speed speed of the movement
+ * @param loop true to make the movement return to the beginning
+ * once finished
+ * @param with_collisions true to make the movement sensitive to obstacles
+ */
+PathMovement::PathMovement(Map *map, string path, int speed,
+			   bool loop, bool with_collisions):
+  MovementWithCollision(map),
+  path(path), next_index(0), loop(loop),
+  with_collisions(with_collisions), finished(false) {
+
+  set_speed(speed);
+  start_next_move();
+}
+
+/**
+ * Destructor.
+ */
+PathMovement::~PathMovement(void) {
+
+}
+
+/**
+ * Sets the x position of the entity.
+ * This is a redefinition of MovementWithCollision::set_x() because we need
+ * the number of pixels covered.
+ * @param x the new x position
+ */
+void PathMovement::set_x(int x) {
+
+  if (x != get_x()) {
+    distance_covered_x++;
+    if (distance_covered_x == 8) {
+      start_next_move();
+    }
+  }
+  MovementWithCollision::set_x(x);
+}
+
+/**
+ * Sets the y position of the entity.
+ * This is a redefinition of MovementWithCollision::set_y() because we need
+ * the number of pixels covered.
+ * @param y the new y position
+ */
+void PathMovement::set_y(int y) {
+
+  if (y != get_y()) {
+    distance_covered_y++;
+    if (distance_covered_y == 8) {
+      start_next_move();
+    }
+  }
+  MovementWithCollision::set_y(y);
+}
+
+/**
+ * Returns whether the end of the path was reached
+ * (only possible when the movement does not loop).
+ * @return true if the end of the path was reached
+ */
+bool PathMovement::is_finished(void) {
+  return finished;
+}
+
+/**
+ * Starts the next step of the movement.
+ */
+void PathMovement::start_next_move(void) {
+
+  if (next_index == path.size()) {
+    if (loop) {
+      next_index = 0;
+    }
+    else {
+      finished = true;
+      stop();
+    }
+  }
+
+  if (!finished) {
+    current_direction = path[next_index] - '0';
+    set_direction(current_direction * 45);
+    next_index++;
+    distance_covered_x = 0;
+    distance_covered_y = 0;
+  }
+}
+
+/**
+ * Returns the direction of the current move, between 0 and 7.
+ * @return the direction of the current move
+ */
+int PathMovement::get_current_direction(void) {
+  return current_direction;
+}

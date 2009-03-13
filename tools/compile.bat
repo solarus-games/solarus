a35fee71e701aba23dd5f27b45f9f341d2916747
Christopho <christopho@solarus-engine.org>
2009-03-13 15:44:45 +0000

    Add an NPC from Newlink
    git-svn-id: svn://solarus-engine.org/solarus@756 08e91b48-8f19-0410-8fc9-9db6b7bce602

--- tools/compile.bat
+++ tools/compile.bat
@@ -1,1 +1,1 @@
-javac -classpath .;.\ini4j.jar zsdx\*.java zsdx\entities\*.java zsdx\gui\*.java zsdx\gui\edit_entities\*.java zsdx\map_editor_actions\*.java zsdx\map_editor_actions\edit_entities\*.java
+javac -classpath .;.\ini4j.jar zsdx\*.java zsdx\entities\*.java zsdx\gui\*.java zsdx\gui\edit_entities\*.java zsdx\map_editor_actions\*.java

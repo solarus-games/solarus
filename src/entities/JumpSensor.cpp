d242df967bfd596a8941e48680145fb7d32135ff
Christopho <christopho@solarus-engine.org>
2009-06-29 17:51:06 +0000

    Jump sensors + Lyriann cave
    git-svn-id: svn://solarus-engine.org/solarus@901 08e91b48-8f19-0410-8fc9-9db6b7bce602

--- src/entities/JumpSensor.cpp
+++ src/entities/JumpSensor.cpp
@@ -103,7 +103,7 @@ bool JumpSensor::check_collision_custom(MapEntity *entity) {
   // its shape is exactly its rectangle
   if (direction % 2 == 0) {
 
-    if (!hero->is_moving_towards(direction / 2)) {
+    if (!check_collision_rectangle(hero) || !hero->is_moving_towards(direction / 2)) {
       return false;
     }
 

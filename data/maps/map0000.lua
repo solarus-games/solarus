db37ec9d2397991ab5e009effaf9d4027477cbd4
Christopho <christopho@solarus-engine.org>
2009-10-01 14:42:51 +0000

    Intro
    git-svn-id: svn://solarus-engine.org/solarus@1062 08e91b48-8f19-0410-8fc9-9db6b7bce602

--- data/maps/map0000.lua
+++ data/maps/map0000.lua
@@ -2,8 +2,28 @@
 -- Intro script --
 ------------------
 
+fresco = 0
+
 function event_map_started(destination_point_name)
   hud_set_enabled(false)
   dialog_set_style(1)
+  start_message("intro0")
+end
+
+function event_message_sequence_finished(first_message_id)
+
+  if fresco == 0 then
+    tile_set_enabled("black_screen", false)
+  end
+
+  if fresco < 6 then
+    fresco = fresco + 1
+    interactive_entity_set_animation("fresco", fresco)
+    start_message("intro"..fresco)
+  else
+    dialog_set_style(0)
+    hud_set_enabled(true)
+    hero_set_map(28, "from_intro", 1)
+  end
 end
 

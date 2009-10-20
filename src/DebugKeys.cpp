859ae16c6cff12c024c7a29465891ff3fce26b2b
Christopho <christopho@solarus-engine.org>
2009-10-20 06:57:05 +0000

    Small fixes
    git-svn-id: svn://solarus-engine.org/solarus@1092 08e91b48-8f19-0410-8fc9-9db6b7bce602

--- src/DebugKeys.cpp
+++ src/DebugKeys.cpp
@@ -78,7 +78,7 @@ void DebugKeys::key_pressed(const SDL_keysym &keysym) {
 	equipment->stop_removing_magic();
       }
       break;
-
+/*
     case SDLK_t:
       equipment->give_inventory_item(INVENTORY_BOW);
       equipment->set_max_arrows(10);
@@ -101,6 +101,7 @@ void DebugKeys::key_pressed(const SDL_keysym &keysym) {
       equipment->set_item_assigned(0, INVENTORY_BOOMERANG);
       equipment->set_item_assigned(1, INVENTORY_BOTTLE_2);
       break;
+	  */
 
     case SDLK_g:
       equipment->add_arrows(7);

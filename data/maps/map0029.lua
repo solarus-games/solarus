d0e2b1c22189b6932d1906ac4f28f91fd95637fc
Christopho <christopho@solarus-engine.org>
2009-11-15 11:41:17 +0000

    Twin caves
    git-svn-id: svn://solarus-engine.org/solarus/trunk@1120 08e91b48-8f19-0410-8fc9-9db6b7bce602

--- data/maps/map0029.lua
+++ data/maps/map0029.lua
@@ -1,5 +1,7 @@
 -- Twin caves script
 
+current_switch_name = ""
+
 function event_map_started(destination_point_name)
   
   if savegame_get_boolean(69) then
@@ -9,19 +11,35 @@ end
 
 function event_switch_enabled(switch_name)
   
+  current_switch_name = switch_name
   if switch_name == "barrier_switch" then
     move_camera(616, 672, 15)
+  elseif switch_name == "door_switch" then
+    move_camera(376, 384, 15)
+  elseif switch_name == "final_barrier_switch" then
+    tiles_set_enabled("final_barrier", false)
+    play_sound("secret")
   end
 end
 
 function event_camera_reached_target()
-  start_timer(1000, "barrier_timer", false)
+  if current_switch_name == "door_switch" then
+    start_timer(1000, "door_timer", false)
+  elseif current_switch_name == "barrier_switch" then
+    start_timer(1000, "barrier_timer", false)
+  end
 end
 
 function barrier_timer()
   tile_set_enabled("barrier", false)
   play_sound("secret")
   savegame_set_boolean(69, true)
+  start_timer(1000, "restore_camera", false)
+end
+
+function door_timer()
+  door_open("door")
+  play_sound("secret")
   start_timer(1000, "restore_camera", false)
 end
 

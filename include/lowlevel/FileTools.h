ca38e3943f1c4d43866c5a32ce91b5d6d2f32917
Christopho <christopho@solarus-engine.org>
2010-05-21 09:24:28 +0000

    More comments
    git-svn-id: svn://solarus-engine.org/solarus/trunk@1307 08e91b48-8f19-0410-8fc9-9db6b7bce602

--- include/lowlevel/FileTools.h
+++ include/lowlevel/FileTools.h
@@ -23,7 +23,8 @@
 /**
  * This class provides some functions to open and analyze a data file.
  * It encapsulates how the access to data files is done.
- * This class is the only one that knows how to access a data file,
+ * This class is the only one that knows how to access a data file
+ * (including language-specific ones)
  * and the only one that calls the PHYSFS library to get data files from
  * the data archive when necessary.
  */

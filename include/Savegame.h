c295251a07984e603435f9f7855cbe2a16fe955f
Christopho <christopho@solarus-engine.org>
2010-09-05 09:41:11 +0000

    Changing savegame format to make equipment items independent of the engine. Existing savegame files will not be compatible anymore and should be deleted.
    git-svn-id: svn://solarus-engine.org/solarus/trunk@1411 08e91b48-8f19-0410-8fc9-9db6b7bce602

--- include/Savegame.h
+++ include/Savegame.h
@@ -37,9 +37,9 @@ class Savegame {
      * The engine uses strings and integers. The booleans are only used the maps.
      */
     typedef struct SavedData {
-      char strings[64][64];     /**< 64 NULL-terminated strings of 64 characters each (4 Ko) */
-      uint32_t integers[2048];  /**< 2048 integers (8 Ko) */
-      uint32_t booleans[1024];  /**< 32768 boolean values (4 Ko) */
+      char strings[64][64];		/**< 64 NULL-terminated strings of 64 bytes each (4 Ko) */
+      uint32_t integers[2048];		/**< 2048 integers (8 Ko) */
+      uint32_t booleans[1024];		/**< 32768 boolean values (4 Ko) */
 
     } SavedData;
 
@@ -54,8 +54,14 @@ class Savegame {
      * Values above 64 are available for the maps in reading and writing.
      */
     enum StringIndex {
-      PLAYER_NAME                     = 0,
-      STARTING_POINT                  = 1, /**< the player appears on this destination point on the starting map */
+      // 0: unused for now
+      PLAYER_NAME			= 1,
+      STARTING_POINT			= 2,		/**< the player appears on this destination point on the starting map */
+
+      ITEM_SLOT_0			= 3,		/**< current item associated to the first item slot
+							 * (X button by default), an empty string means no item */
+      ITEM_SLOT_1			= 4,		/**< current item associated to the second slot
+							 * (V button by default), an empty string means no item */
 
       /**
        * @name Joypad customizable controls.
@@ -65,15 +71,15 @@ class Savegame {
        * Examples: "button 1", "axis 1 +", "hat 1 left"
        * @{
        */
-      JOYPAD_ACTION_KEY              = 10,
-      JOYPAD_SWORD_KEY               = 11,
-      JOYPAD_ITEM_1_KEY              = 12,
-      JOYPAD_ITEM_2_KEY              = 13,
-      JOYPAD_PAUSE_KEY               = 14,
-      JOYPAD_RIGHT_KEY               = 15,
-      JOYPAD_UP_KEY                  = 16,
-      JOYPAD_LEFT_KEY                = 17,
-      JOYPAD_DOWN_KEY                = 18
+      JOYPAD_ACTION_KEY			= 10,
+      JOYPAD_SWORD_KEY			= 11,
+      JOYPAD_ITEM_1_KEY			= 12,
+      JOYPAD_ITEM_2_KEY			= 13,
+      JOYPAD_PAUSE_KEY			= 14,
+      JOYPAD_RIGHT_KEY			= 15,
+      JOYPAD_UP_KEY			= 16,
+      JOYPAD_LEFT_KEY			= 17,
+      JOYPAD_DOWN_KEY			= 18
       /**
        * @}
        */
@@ -94,15 +100,9 @@ class Savegame {
        * @name Last game status
        * @{
        */
-      STARTING_MAP                     = 0, /**< when starting the game, this map is loaded */
-      // 1: unused
-      PAUSE_LAST_SUBMENU               = 2, /**< last submenu shown in the pause menu */
-      INVENTORY_LAST_ROW               = 3, /**< row of the last item selected in the inventory */
-      INVENTORY_LAST_COLUMN            = 4, /**< column of the last item selected in the inventory */
-      ITEM_SLOT_0                      = 5, /**< current object associated to the first item slot
-					     * (X button by default), -1 means no item */
-      ITEM_SLOT_1                      = 6, /**< current object associated to the second slot
-					     * (V button by default), -1 means no item */
+      STARTING_MAP			= 0,		/**< when starting the game, this map is loaded */
+      PAUSE_LAST_SUBMENU		= 1,		/**< last submenu shown in the pause menu */
+      INVENTORY_LAST_ITEM_INDEX		= 2,		/**< index of the last item selected in the inventory */
       /**
        * @}
        */
@@ -111,15 +111,9 @@ class Savegame {
        * @name Current value of rupees, hearts and others
        * @{
        */
-      CURRENT_HEARTS                   = 10, /**< current number of hearts filled (counted in 1/4 of hearts) */
-      CURRENT_RUPEES                   = 11, /**< current number of rupees */
-      CURRENT_MAGIC                    = 12, /**< current level of magic (0 to 84) */
-      CURRENT_BOMBS                    = 13, /**< current number of bombs */
-      CURRENT_ARROWS                   = 14, /**< current number of arrows */
-      CURRENT_PAINS_AU_CHOCOLAT        = 15, /**< current number of pains au chocolat (0 to 10) */
-      CURRENT_CROISSANTS               = 16, /**< current number of croissants (0 to 10) */
-      CURRENT_APPLES                   = 17, /**< current number of apples (0 to 10) */
-      NB_FIRE_STONES                   = 18, /**< number of fire stones already found (0 to 3) */
+      CURRENT_LIFE			= 10,		/**< current level of life */
+      CURRENT_MONEY			= 11,		/**< current amount of money */
+      CURRENT_MAGIC			= 12,		/**< current level of magic */
       /**
        * @}
        */
@@ -128,24 +122,9 @@ class Savegame {
        * @name Maximum values
        * @{
        */
-      MAX_HEARTS                       = 20, /**< maximum number of hearts (counted in entire hearts) */
-      MAX_RUPEES                       = 21, /**< maximum number of rupees (100, 300 or 999) */
-      MAX_MAGIC                        = 22, /**< maximum level of magic (0, 42 or 84 points) */
-      MAX_BOMBS                        = 23, /**< maximum number of bombs (0, 10, 30 or 99) */
-      MAX_ARROWS                       = 24, /**< maximum number of arrows (0, 10, 30 or 99) */
-      /**
-       * @}
-       */
-
-      /**
-       * @name Equipment and quest status
-       * @{
-       */
-      PIECES_OF_HEART                  = 30, /**< current pieces of heart number (0 to 3) */
-      HERO_TUNIC                       = 31, /**< the tunic (0: green, 1: blue, 2: red) */
-      HERO_SHIELD                      = 32, /**< the shield (0: no shield, 1 to 3: shields 1 to 3) */
-      HERO_SWORD                       = 33, /**< the sword (0: no sword, 1 to 4: swords 1 to 4) */
-      WORLD_MAP                        = 34, /**< has the player got the world map? (0: no, 1: yes) */
+      MAX_LIFE				= 20,		/**< maximum level of life */
+      MAX_MONEY				= 21,		/**< maximum amount of money */
+      MAX_MAGIC				= 22,		/**< maximum level of magic */
       /**
        * @}
        */
@@ -158,67 +137,53 @@ class Savegame {
        * Each integer corresponds to a value of the Input::KeyboardKey enumeration.
        * @{
        */
-      KEYBOARD_ACTION_KEY              = 35,
-      KEYBOARD_SWORD_KEY               = 36,
-      KEYBOARD_ITEM_1_KEY              = 37,
-      KEYBOARD_ITEM_2_KEY              = 38,
-      KEYBOARD_PAUSE_KEY               = 39,
-      KEYBOARD_RIGHT_KEY               = 40,
-      KEYBOARD_UP_KEY                  = 41,
-      KEYBOARD_LEFT_KEY                = 42,
-      KEYBOARD_DOWN_KEY                = 43,
-      KEYBOARD_ENUM_VERSION            = 49, /**< Indicates the version of the Input::KeyboardKey enumeration this savegame was created with.
-                                              * If the version saved is different from Input::KEYBOARD_ENUM_VERSION, then
-					      * we know it is obsolete and the customization is reset to some default values. */
+      KEYBOARD_ACTION_KEY		= 35,
+      KEYBOARD_SWORD_KEY		= 36,
+      KEYBOARD_ITEM_1_KEY		= 37,
+      KEYBOARD_ITEM_2_KEY		= 38,
+      KEYBOARD_PAUSE_KEY		= 39,
+      KEYBOARD_RIGHT_KEY		= 40,
+      KEYBOARD_UP_KEY			= 41,
+      KEYBOARD_LEFT_KEY			= 42,
+      KEYBOARD_DOWN_KEY			= 43,
+      KEYBOARD_ENUM_VERSION		= 49,		/**< Indicates the version of the Input::KeyboardKey enumeration this savegame was created with.
+							 * If the version saved is different from Input::KEYBOARD_ENUM_VERSION, then
+							 * we know it is obsolete and the customization is reset to some default values. */
       /**
        * @}
        */
 
       /**
-       * @name Items of the inventory
-       * Variables 100 to 127 indicate whether the player has got each inventory item
-       * (see enum InventoryItemId to know the item numbers).
-       * Each value saved here corresponds to a slot in the inventory.
-       * Zero indicates that the player does not have the item
-       * of this slot.
-       * Some slots can contains several items during the game:
-       * the current variant of the item is then indicated by the value saved
-       * (1 for the first variant, 2 for the second one, etc.).
+       * @name General abilities.
        * @{
        */
-      FIRST_INVENTORY_ITEM             = 100, /**< 0 if the player does not have item #0, 1 if he has
-					       * its first variant, 2 if he has the second one, etc. */
-      LAST_INVENTORY_ITEM              = 127, /**< same thing for item #27 */
-      EXTENSION_INVENTORY_ITEM         = 199, /**< variables 128 to 199 are reserved for possible
-					       * additional items in the future */
+      ABILITY_TUNIC			= 50,
+      ABILITY_SWORD			= 51,
+      ABILITY_SHIELD			= 52,
+      ABILITY_LIFT			= 53,
+      ABILITY_SWIM			= 54,
+      ABILITY_SEE_IN_DARK		= 55,
+      ABILITY_DETECT_WEAK_WALLS		= 56,
       /**
        * @}
        */
 
       /**
-       * @name Dungeon items
-       * For each dungeon, 10 variables are reserved:
-       * - the map,
-       * - the compass,
-       * - the small keys,
-       * - the big key,
-       * - the boss key,
-       * - whether the dungeon is finished
-       * and 4 unused variables for future extension.
-       * Up to 20 dungeons can be saved. Thus, 200 variables are used for the dungeon states (20*10).
+       * @name Dungeon-specific abilities and information.
+       * Up to 40 dungeons can be saved.
        */
-      FIRST_DUNGEON_MAP                = 200, /**< 1 if the player has the map of dungeon #1 */
-      FIRST_DUNGEON_COMPASS            = 201, /**< 1 if the player has the map of dungeon #1 */
-      FIRST_DUNGEON_BIG_KEY            = 202, /**< 1 if the player has the big key of dungeon #1 */
-      FIRST_DUNGEON_BOSS_KEY           = 203, /**< 1 if the player has the boss key of dungeon #1 */
-      FIRST_DUNGEON_SMALL_KEYS         = 204, /**< number of small keys found in dungeon #1 */
-      FIRST_DUNGEON_FINISHED           = 205, /**< 1 if the player has finished dungeon #1 */
-      FIRST_DUNGEON_UNUSED_1           = 206, /**< empty place for future new data in dungeon #1 */
-      FIRST_DUNGEON_UNUSED_2           = 207, /**< empty place for future new data in dungeon #1 */
-      FIRST_DUNGEON_UNUSED_3           = 208, /**< empty place for future new data in dungeon #1 */
-      FIRST_DUNGEON_UNUSED_4           = 209, /**< empty place for future new data in dungeon #1 */
+      DUNGEON_1_FINISHED			= 200,
+      DUNGEON_1_ABILITY_SEE_MINIMAP_ROOMS	= 201,
+      DUNGEON_1_ABILITY_SEE_MINIMAP_ELEMENTS	= 202,
+      DUNGEON_1_ABILITY_OPEN_BIG_LOCKS		= 203,
+      DUNGEON_1_ABILITY_OPEN_BOSS_LOCK		= 204,
+      DUNGEON_1_SMALL_KEYS			= 205,		/**< small key counter in dungeon #1 */
+      DUNGEON_1_UNUSED_1			= 206,		/**< empty place for future new data in dungeon #1 */
+      DUNGEON_1_UNUSED_2			= 207,		/**< empty place for future new data in dungeon #1 */
+      DUNGEON_1_UNUSED_3			= 208,		/**< empty place for future new data in dungeon #1 */
+      DUNGEON_1_UNUSED_4			= 209,		/**< empty place for future new data in dungeon #1 */
       // then, same thing for other dungeons
-      LAST_DUNGEON_UNUSED_4            = 399  /**< empty place for future new data in dungeon #20 */
+      DUNGEON_40_UNUSED_4			= 599,		/**< empty place for future new data in dungeon #40 */
 
       // values above 1024 are available to the maps
     };

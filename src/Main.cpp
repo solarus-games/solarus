c755412c8634ae4e23abe68514b89569974ac314
Christopho <christopho@solarus-engine.org>
2010-11-01 16:26:22 +0100

    Making a testing system (not finished yet)

--- src/Main.cpp
+++ src/Main.cpp
@@ -14,6 +14,8 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+#ifndef SOLARUS_NOMAIN
+
 #include "Solarus.h"
 
 /**
@@ -32,4 +34,6 @@ int main(int argc, char **argv) {
   Solarus(argc, argv).main_loop();
   return 0;
 }
+
+#endif
 

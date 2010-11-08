1a3b34580fed9749a02a092184924f4be5a7fa44
Christopho <christopho@solarus-engine.org>
2010-11-08 13:30:34 +0100

    Thinking about a simpler API for movements in scripts

--- quests/zsdx/data/items/fairy.lua
+++ quests/zsdx/data/items/fairy.lua
@@ -1,7 +1,13 @@
 function event_appear(variant)
-  sol.main.create_movement("m", "random_movement")
+  sol.main.movement_create("m", "random_movement")
   sol.main.movement_set_property("m", "speed", 32)
   sol.main.movement_set_property("m", "max_distance", 40)
   sol.item.set_movement("m")
 end
+
+-- TODO?
+-- function event_appear(variant)
+--   m = sol.main.random_movement_create(32, 40)
+--   sol.item.set_movement(m)
+-- end
 

c99d43f012edf1fe09464e8f2e183935df5cbec7
Christopho <christopho@solarus-engine.org>
2010-11-16 19:03:23 +0100

    Fix pieces of heart

--- quests/zsdx/data/items/piece_of_heart.lua
+++ quests/zsdx/data/items/piece_of_heart.lua
@@ -10,10 +10,10 @@ function event_obtained(variant)
   nb_pieces_of_heart = sol.game.savegame_get_integer(1030)
   sol.game.savegame_set_integer(1030, (nb_pieces_of_heart + 1) % 4)
   if (nb_pieces_of_heart == 3) then
-    sol.game.equipment.add_max_life(4)
+    sol.game.equipment_add_max_life(4)
     sol.game.equipment_add_life(sol.game.equipment_get_max_life())
   end
-  sol.map.dialog_start(message_id[nb_pieces_of_heart])
+  sol.map.dialog_start(message_id[nb_pieces_of_heart+1])
 
 end
 

287d9913d803530c19257df941dd912bda164d34
Christopho <christopho@solarus-engine.org>
2010-11-16 14:07:10 +0100

    Improve some items

--- quests/zsdx/data/items/feather.lua
+++ quests/zsdx/data/items/feather.lua
@@ -1,6 +1,7 @@
 function event_use()
-  direction = sol.map.hero_get_direction()
-  sol.map.hero_start_jumping(direction, 32, false)
+  sol.main.play_sound("jump")
+  direction4 = sol.map.hero_get_direction()
+  sol.map.hero_start_jumping(direction4 * 2, 32, false)
   sol.item.set_finished()
 end
 

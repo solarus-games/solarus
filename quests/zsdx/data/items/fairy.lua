01b1b09c069730a4b144448a69b0f37e62c55f2c
Christopho <christopho@solarus-engine.org>
2010-11-22 11:53:42 +0100

    Implement the script of the fairy (not fully tested yet)

--- quests/zsdx/data/items/fairy.lua
+++ quests/zsdx/data/items/fairy.lua
@@ -1,6 +1,26 @@
 movement = nil
 sprite = nil
 
+-- Returns the item name of an empty bottle, or an empty string if there is no empty bottle
+function get_first_empty_bottle()
+
+  result = ""
+
+  if sol.game.equipment_get_item("bottle_1") == 1 then
+    result = "bottle_1"
+  elseif sol.game.equipment_get_item("bottle_2") == 1 then
+    result = "bottle_2"
+  elseif sol.game.equipment_get_item("bottle_3") == 1 then
+    result = "bottle_3"
+  elseif sol.game.equipment_get_item("bottle_4") == 1 then
+    result = "bottle_4"
+  end
+
+  return result
+
+end
+
+-- A fairy appears on the map: set its movement
 function event_appear(variant)
   sprite = sol.item.get_sprite()
   movement = sol.main.random_movement_create(32)
@@ -8,14 +28,56 @@ function event_appear(variant)
   sol.item.start_movement(movement)
 end
 
+-- The direction of the movement may have changed,
+-- update the direction of the fairy's sprite
 function event_movement_changed()
 
-  -- the direction of the movement may have changed, update the direction of the fairy's sprite
   angle = tonumber(sol.main.movement_get_property(movement, "angle"))
   if angle >= 90 and angle < 270 then
     sol.main.sprite_set_direction(sprite, 1)
   else
     sol.main.sprite_set_direction(sprite, 0)
+  end
+end
+
+-- Obtaining a fairy
+function event_obtaining(variant, savegame_variable)
+
+  if sol.game.equipment_has_item("bottle_1")
+    or sol.game.equipment_has_item("bottle_2")
+    or sol.game.equipment_has_item("bottle_3")
+    or sol.game.equipment_has_item("bottle_4") then
+
+    sol.map.dialog_start("_found_fairy")
+
+  else
+    sol.game.equipment_add_life(7 * 4)
+  end
+end
+
+function event_dialog_finished(first_message_id, answer)
+
+  if first_message_id == "_found_fairy" then
+
+    if answer ~= 1 then
+      -- restore 7 hearts
+      sol.game.equipment_add_life(7 * 4)
+    else
+      -- keep the fairy in a bottle
+      first_empty_bottle == get_first_empty_bottle()
+      if first_empty_bottle == "" then
+        -- no empty bottle
+        sol.map.dialog_start("_found_fairy.no_empty_bottle")
+        sol.main.play_sound("wrong")
+      else
+        -- okay, empty bottle 
+        sol.game.equipment_set_item(first_empty_bottle, 6)
+        sol.main.play_sound("danger")
+      end
+    end
+
+  elseif first_message_id == "_found_fairy.no_empty_bottle" then
+    sol.game.equipment_add_life(7 * 4)
   end
 end
 

a2c0e5292928036c344e5d03c4ff00b164b634a3
Christopho <christopho@solarus-engine.org>
2010-11-29 22:55:08 +0100

    Starting to implement the bottle script

--- src/InventoryItem.cpp
+++ src/InventoryItem.cpp
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#include "InventoryItem.h" // TODO many useless headers here
+#include "InventoryItem.h"
 #include "Game.h"
 #include "Equipment.h"
 #include "lua/ItemScript.h"

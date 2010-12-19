2c862eec54c245b7ddde41a49a35a91c54667995
Christopho <christopho@solarus-engine.org>
2010-12-19 13:40:13 +0100

    Bomb cave

---
+++ quests/zsdx/data/maps/map0035.lua
@@ -0,0 +1,23 @@
+------------------
+-- Bomb cave 2F --
+------------------
+
+function event_map_started(destination_point_name)
+  sol.map.door_set_open("door", true)
+end
+
+function event_hero_on_sensor(sensor_name)
+
+  if sensor_name == "close_door_sensor" then
+    sol.map.door_close("door")
+  end
+end
+
+function event_opening_transition_finished(destination_point_name)
+
+  -- TODO
+  if destination_point_name == "from_3F" then
+    sol.map.door_close("door")
+  end
+end
+

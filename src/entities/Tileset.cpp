b06653f45f92811ab47cd648ea803ae3f9d92f2f
unknown <Christophe@.(none)>
2010-12-22 10:40:25 +0100

    Fix compilation warning

--- src/entities/Tileset.cpp
+++ src/entities/Tileset.cpp
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2009 Christopho, Solarus - http://www.solarus-engine.org
- * 
+ *
  * Solarus is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Solarus is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -47,7 +47,7 @@ Tileset::~Tileset() {
  * @brief Adds a tile pattern to this tileset.
  *
  * This function is called by load().
- * 
+ *
  * @param id id of this tile pattern (1 to 1024)
  * @param tile_pattern the tile pattern to add
  */
@@ -109,7 +109,7 @@ void Tileset::load() {
       FileTools::read(iss, width);
       FileTools::read(iss, height);
 
-      TilePattern *pattern;
+      TilePattern *pattern = NULL;
       if (animation == 0) {
 	pattern = new SimpleTilePattern(Obstacle(obstacle), x, y, width, height);
       }

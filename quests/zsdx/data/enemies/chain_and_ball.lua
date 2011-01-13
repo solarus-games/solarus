9d557ff54b43b48ae9905e53e64ca83acc14e2d5
Christopho <christopho@solarus-engine.org>
2011-01-13 23:57:08 +0100

    Finish the list of functions of C++/Lua interface for enemy scripts

--- quests/zsdx/data/enemies/chain_and_ball.lua
+++ quests/zsdx/data/enemies/chain_and_ball.lua
@@ -16,8 +16,8 @@ display_in_y_order = false
 nb_links = 10
 link_sprite = nil
 link_xy = {x = 0, y = 0}
-creator_name = ""                  -- name of the enemy the chain and ball is attached to
-creator_dxy = {x = 0, y = 0};      -- center point of the circles, relative to the enemy
+father_name = ""                  -- name of the enemy the chain and ball is attached to if any
+center_xy = {x = 0, y = 0}        -- center point of the circles, relative to the father enemy if any, absolute otherwise
 
 function event_appear()
 
@@ -31,11 +31,13 @@ function event_appear()
     link_xy[i].y = 0
   end
 
-  -- get the creator
-  creator_name = sol.enemy.get_creator()
-  x, y = sol.enemy.get_creator_relative_position()
-  center_enemy_dxy.x = x
-  center_enemy_dxy.y = y
+  -- get the difference of coordinates between me and my father
+  father_name = sol.enemy.get_father()
+  if father_name ~= "" then
+    x, y = sol.enemy.get_position()
+    father_x, father_y = sol.map.enemy_get_position(father_name)
+    center_xy = father_x - x, father_y - y
+  end
 end
 
 function event_pre_display()
@@ -48,24 +50,25 @@ end
 function event_position_changed(x, y)
 
   -- recalculate the chain position
-  if creator_name ~= "" then
+  if father_name ~= "" then
+    -- the center is relative to the father
+    x, y = sol.map.enemy_get_position(father_name)
+    x1, y1 = x + center_xy.x, y + center_xy.y;
+  else
+    -- the center is absolute
+    x1, y1 = center_xy
+  end
+  x2, y2 = sol.enemy.get_position();
 
-    x, y = sol.map.enemy_get_position(creator_name)
-    x1 = x + center_enemy_dxy.x;
-    y1 = y + center_enemy_dxy.y;
-    x2 = get_x();
-    y2 = get_y();
-
-    for i = 0, nb_links - 1 do
-      link_xy[i].x = x1 + (x2 - x1) * i / nb_links;
-      link_xy[i].y = y1 + (y2 - y1) * i / nb_links;
-    end
+  for i = 0, nb_links - 1 do
+    link_xy[i].x = x1 + (x2 - x1) * i / nb_links;
+    link_xy[i].y = y1 + (y2 - y1) * i / nb_links;
   end
 end
 
 function event_enabled()
 
-  m = sol.main.circle_movement_create(7, creator_name, 64)
+  m = sol.main.circle_movement_create(7, father_name, 64)
   sol.main.movement_set_property(m, "center_dx", -16)
   sol.main.movement_set_property(m, "center_dy", -33)
   sol.main.movement_set_property(m, "radius_speed", 50)

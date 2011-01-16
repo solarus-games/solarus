a076adb956c7da9c554b8319706876690812253b
Christopho <christopho@solarus-engine.org>
2011-01-16 20:09:10 +0100

    Remove the enemy classes, they all are replaced by Lua scripts now

--- src/entities/CustomEnemy.cpp
+++ src/entities/CustomEnemy.cpp
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#include "enemies/CustomEnemy.h"
+#include "entities/CustomEnemy.h"
 #include "lua/EnemyScript.h"
 #include "Map.h"
 

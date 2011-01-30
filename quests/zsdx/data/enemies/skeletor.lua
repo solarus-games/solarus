c8d76a83116deda2920d9bfc4a22fac7fcb3ea26
Christopho <christopho@solarus-engine.org>
2011-01-30 14:17:10 +0100

    Add two new enemies: Skeletor and Mandible

---
+++ quests/zsdx/data/enemies/skeletor.lua
@@ -0,0 +1,24 @@
+-- Globul
+
+function event_appear()
+
+  sol.enemy.set_life(3)
+  sol.enemy.set_damage(2)
+  sol.enemy.create_sprite("enemies/skeletor")
+  sol.enemy.set_size(16, 16)
+  sol.enemy.set_origin(8, 13)
+  sol.enemy.set_hurt_sound_style("monster")
+
+  m = sol.main.target_movement_create(48)
+  sol.enemy.start_movement(m)
+end
+
+function event_movement_changed()
+
+  m = sol.enemy.get_movement()
+  direction4 = sol.main.movement_get_property(m, "displayed_direction")
+  sprite = sol.enemy.get_sprite()
+  sol.main.sprite_set_direction(sprite, direction4)
+end
+
+

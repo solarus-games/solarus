44c4122de5cd9083bea7f43756dbec81b4b7a7cf
Sam101 <gsam101@gmail.com>
2011-02-11 22:35:43 +0100

    Feather and pegasus shoes are now funny.

--- quests/zsxd/data/items/feather.lua
+++ quests/zsxd/data/items/feather.lua
@@ -2,7 +2,16 @@ function event_use()
   sol.main.play_sound("jump")
   direction4 = sol.map.hero_get_direction()
 
-  sol.map.hero_start_jumping(direction4 * 2, math.random(16,200), false)
+  randdirection = math.random(1,4)
+  diagonale = 0  
+
+  if randdirection == 1 then
+    diagonale = 1
+  elseif randdirection == 4 then
+    diagonale = -1
+  end
+
+  sol.map.hero_start_jumping((direction4 * 2 + diagonale ) % 8, math.random(16,100), false)
   sol.item.set_finished()
 end
 

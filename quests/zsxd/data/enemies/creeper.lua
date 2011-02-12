123423d77f25b3a0d52c9e18ce69e3b1e8a90d42
Christopho <christopho@solarus-engine.org>
2011-02-12 23:45:24 +0100

    Improve Creeper

--- quests/zsxd/data/enemies/creeper.lua
+++ quests/zsxd/data/enemies/creeper.lua
@@ -9,31 +9,31 @@ function event_appear()
   sol.enemy.create_sprite("enemies/creeper")
   sol.enemy.set_size(16, 16)
   sol.enemy.set_origin(8, 13)
-  m = sol.main.path_finding_movement_create(32)
+  m = sol.main.target_movement_create(40)
   sol.enemy.start_movement(m)
 end
 
 function event_restart()
-  sol.main.timer_start(200, "check_hero", false)
+  sol.main.timer_start(100, "check_hero", false)
 end
 
 function check_hero()
 
-  near_hero = sol.enemy.get_distance_to_hero() < 40
+  near_hero = sol.enemy.get_distance_to_hero() < 50
   if near_hero and not explosion_soon then
     explosion_soon = true
-    sol.enemy.stop_movement()
     sprite = sol.enemy.get_sprite()
     sol.main.sprite_set_animation(sprite, "hurt")
     sol.main.timer_start(400, "explode", false)
   else
-    sol.main.timer_start(200, "check_hero", false)
+    sol.main.timer_start(100, "check_hero", false)
   end
 end
 
 function explode()
   x, y = sol.enemy.get_position()
   layer = sol.enemy.get_layer()
+  sol.main.play_sound("explosion")
   sol.map.explosion_create(x, y, layer)
   sol.map.explosion_create(x + 32, y, layer)
   sol.map.explosion_create(x + 24, y - 24, layer)

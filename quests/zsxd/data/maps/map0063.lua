743122e468f6236f89d2f34b95b410aac8d703b3
Christopho <christopho@solarus-engine.org>
2011-02-17 23:53:40 +0100

    Working on the dungeon

--- quests/zsxd/data/maps/map0063.lua
+++ quests/zsxd/data/maps/map0063.lua
@@ -1,4 +1,15 @@
+-- Dungeon 1 1F SE (Stupidities Temple)
+
 function event_map_started(destination_point_name)
 --  sol.map.light_set(0)
 end
+
+function event_map_opening_transition_finished(destination_point_name)
+
+  -- show the welcome message
+  if destination_point_name == "from_outside" then
+    sol.map.dialog_start("dungeon_1.welcome")
+  end
+end
+
 

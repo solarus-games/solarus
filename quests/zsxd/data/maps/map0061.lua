d54f71f3e169646c46e2ff21dcda943613fe6ec1
Christopho <christopho@solarus-engine.org>
2011-02-25 00:16:39 +0100

    Temple of stupidities

--- quests/zsxd/data/maps/map0061.lua
+++ quests/zsxd/data/maps/map0061.lua
@@ -7,3 +7,11 @@ function event_door_open(door_name)
     sol.main.play_sound("secret")
   end
 end
+
+function event_treasure_obtained(item_name, variant, savegame_variable)
+
+  if savegame_variable == 248 then
+    sol.map.dialog_start("small_key_danger_east")
+  end
+end
+

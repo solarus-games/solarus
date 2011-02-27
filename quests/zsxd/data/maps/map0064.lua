a2da3198a5f757a28a3a3934a30ef7a76572eac1
Christopho <christopho@solarus-engine.org>
2011-02-27 19:29:33 +0100

    Temple

--- quests/zsxd/data/maps/map0064.lua
+++ quests/zsxd/data/maps/map0064.lua
@@ -5,3 +5,16 @@ function event_map_started(destination_point_name)
   sol.map.light_set(0)
 end
 
+function event_switch_activated(switch_name)
+
+  sol.main.play_sound("secret")
+  sol.map.door_open("creeper_door")
+end
+
+function event_hero_on_sensor(sensor_name)
+
+  if sol.map.door_is_open("creeper_door")
+      and sol.map.hero_get_direction() == 2 then
+    sol.map.door_close("creeper_door")
+  end
+end
--- quests/zsxd/data/maps/map0066.lua
+++ quests/zsxd/data/maps/map0066.lua
@@ -6,5 +6,42 @@ function event_map_started(destination_point_name)
   if sol.game.savegame_get_boolean(283) then
     sol.map.tile_set_group_enabled("water", false)
   end
+
+  -- fight room
+  sol.map.door_set_open("fight_door", true)
+  if sol.game.savegame_get_boolean(244) then
+    sol.map.enemy_remove_group("fight")
+  else
+    -- TODO sol.map.enemy_set_group_enabled
+    sol.map.enemy_set_enabled("fight_1", false)
+    sol.map.enemy_set_enabled("fight_2", false)
+    sol.map.enemy_set_enabled("fight_3", false)
+    sol.map.enemy_set_enabled("fight_4", false)
+    sol.map.chest_set_enabled("fight_chest", false)
+  end
 end
 
+function event_hero_on_sensor(sensor_name)
+
+  if sensor_name == "fight_sensor" 
+      and not sol.game.savegame_get_boolean(244)
+      and sol.map.door_is_open("fight_door")
+      and not sol.map.chest_is_enabled("fight_chest") then
+    sol.map.door_close("fight_door")
+    sol.map.enemy_set_enabled("fight_1", true)
+    sol.map.enemy_set_enabled("fight_2", true)
+    sol.map.enemy_set_enabled("fight_3", true)
+    sol.map.enemy_set_enabled("fight_4", true)
+  end
+end
+
+function event_enemy_dead(enemy_name)
+
+  if string.match(enemy_name, "^fight")
+      and sol.map.enemy_is_group_dead("fight") then
+
+    sol.main.play_sound("chest_appears")
+    sol.map.chest_set_enabled("fight_chest", true)
+    sol.map.door_open("fight_door")
+  end
+end

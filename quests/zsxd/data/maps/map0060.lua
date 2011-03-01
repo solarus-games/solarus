a7860b625894fb340e75e1e87bff174f0ef14cf2
Christopho <christopho@solarus-engine.org>
2011-03-01 15:16:50 +0100

    Consider the whole stupid code

--- quests/zsxd/data/maps/map0060.lua
+++ quests/zsxd/data/maps/map0060.lua
@@ -1,7 +1,9 @@
 -- Temple of Stupidities 1F NW
 
 switches_good_order = {
-3,1,4,2,3
+3,1,4,2,3,2,1,1,4,3,2,2,
+1,3,2,3,3,1,4,4,2,1,2,3,
+3,2,1,1,3,4,2,2,1,3,4,1
 }
 next_switch_index = 1
 camera_timer = ""

cb95a8214fcabf777883d97892ae23fd30d34bf3
MTLZ <thomas.noury@gmail.com>
2011-03-01 22:17:17 +0100

    Divers ajouts dans le bureau des objets trouves

--- quests/zsxd/data/maps/map0020.lua
+++ quests/zsxd/data/maps/map0020.lua
@@ -4,14 +4,14 @@
 
 function event_map_started(destination_point_name)
 	if not sol.game.savegame_get_boolean(101) then
-		sol.map.chest_set_enabled("empty_chest_1", false)
+		sol.map.chest_set_enabled("CV3", false)
 	end
 end
 
 function event_switch_activated(switch_name)
-	if switch_name == "empty_chest_1_switch" then
+	if switch_name == "CS3" then
 		-- Faire apparaitre le coffre vide 1
-		sol.map.chest_set_enabled("empty_chest_1", true)
+		sol.map.chest_set_enabled("CV3", true)
 		sol.game.savegame_set_boolean(101, true)
 		sol.main.play_sound("chest_appears")
 	end

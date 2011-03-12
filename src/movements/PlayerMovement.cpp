fa8f41ff2e7e031bbf743d4b7a1eedaae07dfb14
Christopho <christopho@solarus-engine.org>
2011-03-12 22:57:16 +0100

    The movement of the hero didn't really stop when activating a sensor

--- src/movements/PlayerMovement.cpp
+++ src/movements/PlayerMovement.cpp
@@ -52,13 +52,15 @@ void PlayerMovement::update() {
     direction8 = -1;
     compute_movement();
   }
+  else {
 
-  // check whether the wanted direction has changed
-  GameControls& controls = get_entity()->get_game().get_controls();
-  int wanted_direction8 = controls.get_wanted_direction8();
-  if (wanted_direction8 != direction8 && !is_suspended()) {
-    direction8 = wanted_direction8;
-    compute_movement();
+    // check whether the wanted direction has changed
+    GameControls& controls = get_entity()->get_game().get_controls();
+    int wanted_direction8 = controls.get_wanted_direction8();
+    if (wanted_direction8 != direction8 && !is_suspended()) {
+      direction8 = wanted_direction8;
+      compute_movement();
+    }
   }
 }
 

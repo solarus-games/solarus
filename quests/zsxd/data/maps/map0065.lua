386368a8e32925b9d98757f250c6f68d508053fd
Christopho <christopho@solarus-engine.org>
2011-03-19 02:47:17 +0100

    Make Ganon (boss of the temple of stupidities)

--- quests/zsxd/data/maps/map0065.lua
+++ quests/zsxd/data/maps/map0065.lua
@@ -25,6 +25,12 @@ function event_map_started(destination_point_name)
   else
     sol.map.block_set_enabled("fake_remove_water_block", false)
   end
+
+  -- boss
+  sol.map.door_set_open("boss_door", true)
+  if sol.game.savegame_get_boolean(306) then
+    sol.map.tile_set_enabled("boss_gate", true)
+  end
 end
 
 function event_switch_activated(switch_name)
@@ -123,6 +129,12 @@ function event_hero_on_sensor(sensor_name)
 
     sol.main.timer_start(500, "remove_2f_sw_water", false)
     will_remove_water = true
+  elseif sensor_name == "start_boss_sensor" then
+    if sol.map.door_is_open("boss_door") and not sol.game.savegame_get_boolean(306) then
+      sol.map.door_close("boss_door")
+      sol.map.hero_freeze()
+      sol.main.timer_start(1000, "start_boss", false)
+    end
   end
 end
 
@@ -134,6 +146,57 @@ function remove_2f_sw_water()
   sol.game.savegame_set_boolean(283, true)
 end
 
+function start_boss()
 
+  sol.map.enemy_set_enabled("boss", true)
+  sol.main.play_music("boss.spc")
+  sol.main.timer_start(1000, "ganon_dialog", false)
+  sol.map.hero_unfreeze()
+end
 
+function ganon_dialog()
+  sol.map.dialog_start("dungeon_1.ganon")
+end
+
+function event_enemy_dead(enemy_name)
+
+  if enemy_name == "boss" then
+    sol.map.tile_set_enabled("boss_gate", true) 
+    sol.game.savegame_set_boolean(62, true) -- open the door of Link's cave
+    sol.main.play_sound("secret")
+  end
+end
+
+function event_hero_interaction(entity_name)
+
+  if entity_name == "boss_hint_stone" then
+    sol.main.timer_start(9000, "another_castle", false)
+    sol.main.play_music("victory.spc")
+    sol.map.hero_set_direction(3)
+    sol.map.hero_freeze()
+  end
+end
+
+function another_castle()
+
+  sol.map.dialog_start("dungeon_1.boss_hint_stone")
+  sol.map.dialog_set_variable("dungeon_1.boss_hint_stone", sol.game.savegame_get_name())
+end
+
+function event_dialog_finished(first_message_id)
+
+  if first_message_id == "dungeon_1.boss_hint_stone" then
+    sol.main.timer_start(1000, "victory", false)
+  end
+end
+
+function victory()
+  sol.map.hero_start_victory_sequence()
+  sol.main.timer_start(2000, "leave_dungeon", false)
+end
+
+function leave_dungeon()
+  sol.main.play_sound("warp")
+  sol.map.hero_set_map(4, "from_temple_of_stupidities", 1)
+end
 

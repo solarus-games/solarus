d368390b2f0a052002c9bfd10581828472689f5f
Christopho <christopho@solarus-engine.org>
2011-03-23 18:01:30 +0100

    Other corrections in the found items house

--- quests/zsxd/data/maps/map0022.lua
+++ quests/zsxd/data/maps/map0022.lua
@@ -114,6 +114,10 @@ function event_dialog_finished(first_message_id, answer)
 	if first_message_id == "crazy_house.vieillard_riz_ok" then
 		sol.map.treasure_give("bocal_epice", 1, -1)
 		sol.game.remove_item_amount("sac_riz_counter", 5)
+		-- branche 1411 finie
+		if sol.game.get_item_amount("balai_counter") > 0 then
+			sol.game.savegame_set_integer(1410, 9)
+		end
 	elseif first_message_id == "crazy_house.guichet_22A" then
 		if answer == 0 then
 			if sol.game.get_item_amount("roc_magma_counter") < 3 then
@@ -135,6 +139,10 @@ function event_dialog_finished(first_message_id, answer)
 	elseif first_message_id == "crazy_house.guichet_22_rm_ok" then
 		sol.map.treasure_give("balai", 1, -1)
 		sol.game.remove_item_amount("roc_magma_counter", 3)
+		-- branche 1412 finie
+		if sol.game.get_item_amount("bocal_epice") > 0 then
+			sol.game.savegame_set_integer(1410, 9)
+		end
 	elseif first_message_id == "crazy_house.guichet_22_sr_ok" then
 		sol.map.treasure_give("tapisserie", 1, -1)
 		sol.game.add_item_amount("tapisserie_counter", 2)

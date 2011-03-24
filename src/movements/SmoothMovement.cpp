solarus-games/solarus

commit fdd64b82b5b23fcb41df17efd5eb6cb9f071989a

Fix the movement of Globul

src/movements/SmoothMovement.cpp
@@ -90,39 +90,45 @@ void SmoothMovement::update_x() {
 	  next_move_date_x_increment = (int) (x_delay / Geometry::SQRT_2);
 	}
       }
-      else if (y_move == 0) {
-	// the move on x is not possible: let's try
-	// to add a move on y to make a diagonal move
+      else {
+        if (y_move != 0 && !test_collision_with_obstacles(0, y_move)) {
+          translate_y(y_move);
+          set_next_move_date_y(get_next_move_date_y() + get_y_delay() / Geometry::SQRT_2);
+        }
+        else {
+          // the move on x is not possible: let's try
+          // to add a move on y to make a diagonal move
 
-	if (!test_collision_with_obstacles(x_move, 1)) {
-	  translate_xy(x_move, 1);
-	  next_move_date_x_increment = (int) (x_delay * Geometry::SQRT_2); // fix the speed
-	}
-	else if (!test_collision_with_obstacles(x_move, -1)) {
-	  translate_xy(x_move, -1);
-	  next_move_date_x_increment = (int) (x_delay * Geometry::SQRT_2);
-	}
-	else {
+          if (!test_collision_with_obstacles(x_move, 1)) {
+            translate_xy(x_move, 1);
+            next_move_date_x_increment = (int) (x_delay * Geometry::SQRT_2); // fix the speed
+          }
+          else if (!test_collision_with_obstacles(x_move, -1)) {
+            translate_xy(x_move, -1);
+            next_move_date_x_increment = (int) (x_delay * Geometry::SQRT_2);
+          }
+          else {
 
-	  /* The diagonal moves didn't work either.
-	   * So we look for a place (up to 8 pixels up and down)
-	   * where the required move would be allowed.
-	   * If we find a such place, then we move towards this place.
-	   */
+            /* The diagonal moves didn't work either.
+             * So we look for a place (up to 8 pixels up and down)
+             * where the required move would be allowed.
+             * If we find a such place, then we move towards this place.
+             */
 
-	  bool moved = false;
-	  for (int i = 1; i <= 8 && !moved; i++) {
+            bool moved = false;
+            for (int i = 1; i <= 8 && !moved; i++) {
 
-	    if (!test_collision_with_obstacles(x_move, i) && !test_collision_with_obstacles(0, 1)) {
-	      translate_y(1);
-	      moved = true;
-	    }
-	    else if (!test_collision_with_obstacles(x_move, -i) && !test_collision_with_obstacles(0, -1)) {
-	      translate_y(-1);
-	      moved = true;
-	    }
-	  }
-	}
+              if (!test_collision_with_obstacles(x_move, i) && !test_collision_with_obstacles(0, 1)) {
+                translate_y(1);
+                moved = true;
+              }
+              else if (!test_collision_with_obstacles(x_move, -i) && !test_collision_with_obstacles(0, -1)) {
+                translate_y(-1);
+                moved = true;
+              }
+            }
+          }
+        }
       }
       set_next_move_date_x(get_next_move_date_x() + next_move_date_x_increment);
     }
@@ -166,38 +172,45 @@ void SmoothMovement::update_y() {
 	  next_move_date_y_increment = (int) (y_delay / Geometry::SQRT_2);
 	}
       }
-      else if (x_move == 0) {
-	// The move on y is not possible: let's try
-	// to add a move on x to make a diagonal move.
+      else {
 
-	if (!test_collision_with_obstacles(1, y_move)) {
-	  translate_xy(1, y_move);
-	  next_move_date_y_increment = (int) (y_delay * Geometry::SQRT_2); // fix the speed
-	}
-	else if (!test_collision_with_obstacles(-1, y_move)) {
-	  translate_xy(-1, y_move);
-	  next_move_date_y_increment = (int) (y_delay * Geometry::SQRT_2);
-	}
-	else {
-	  /* The diagonal moves didn't work either.
-	   * So we look for a place (up to 8 pixels on the left and on the right)
-	   * where the required move would be allowed.
-	   * If we find a such place, then we move towards this place.
-	   */
+        if (x_move != 0 && !test_collision_with_obstacles(x_move, 0)) {
+          translate_x(x_move);
+          set_next_move_date_x(get_next_move_date_x() + get_x_delay() / Geometry::SQRT_2);
+        }
+        else {
+          // The move on y is not possible: let's try
+          // to add a move on x to make a diagonal move.
 
-	  bool moved = false;
-	  for (int i = 1; i <= 8 && !moved; i++) {
+          if (!test_collision_with_obstacles(1, y_move)) {
+            translate_xy(1, y_move);
+            next_move_date_y_increment = (int) (y_delay * Geometry::SQRT_2); // fix the speed
+          }
+          else if (!test_collision_with_obstacles(-1, y_move)) {
+            translate_xy(-1, y_move);
+            next_move_date_y_increment = (int) (y_delay * Geometry::SQRT_2);
+          }
+          else {
+            /* The diagonal moves didn't work either.
+             * So we look for a place (up to 8 pixels on the left and on the right)
+             * where the required move would be allowed.
+             * If we find a such place, then we move towards this place.
+             */
 
-	    if (!test_collision_with_obstacles(i, y_move) && !test_collision_with_obstacles(1, 0)) {
-	      translate_x(1);
-	      moved = true;
-	    }
-	    else if (!test_collision_with_obstacles(-i, y_move) && !test_collision_with_obstacles(-1, 0)) {
-	      translate_x(-1);
-	      moved = true;
-	    }
-	  }
-	}
+            bool moved = false;
+            for (int i = 1; i <= 8 && !moved; i++) {
+
+              if (!test_collision_with_obstacles(i, y_move) && !test_collision_with_obstacles(1, 0)) {
+                translate_x(1);
+                moved = true;
+              }
+              else if (!test_collision_with_obstacles(-i, y_move) && !test_collision_with_obstacles(-1, 0)) {
+                translate_x(-1);
+                moved = true;
+              }
+            }
+          }
+        }
       }
       set_next_move_date_y(get_next_move_date_y() + next_move_date_y_increment);
     }

include/movements/RectilinearMovement.h
@@ -34,6 +34,7 @@ class RectilinearMovement: public Movement {
   private:
 
     // speed vector
+    double angle;                               /**< angle between the speed vector and the horizontal axis in radians */
     double x_speed;				/**< X speed of the object to move in pixels per second.
 						 * 0: stopped
 						 * positive value: moving to the right

include/movements/TargetMovement.h
@@ -18,7 +18,7 @@
 #define SOLARUS_TARGET_MOVEMENT_H
 
 #include "Common.h"
-#include "movements/RectilinearMovement.h"
+#include "movements/SmoothMovement.h"
 
 /**
  * @brief Movement of an object that goes to a target point.
@@ -29,7 +29,7 @@
  * - speed
  * - displayed_direction (read-only)
  */
-class TargetMovement: public RectilinearMovement {
+class TargetMovement: public SmoothMovement {
 
   protected:
 

quests/zsdx/data/maps/map0044.dat
@@ -3328,16 +3328,26 @@
 0	1	2880	224	8	16	71
 0	1	2888	216	16	32	2
 0	1	2904	224	8	16	71
-0	1	112	2144	80	8	70
-0	1	104	2144	8	8	69
-0	1	192	2144	8	8	69
-0	1	104	2128	8	16	71
-0	1	104	2120	8	8	69
-0	1	112	1704	80	8	70
-0	1	104	1704	8	8	69
-0	1	192	1704	8	8	69
-0	1	104	1688	8	16	71
-0	1	104	1680	8	8	69
+0	1	104	2168	32	8	70
+0	1	96	2168	8	8	69
+0	1	136	2168	8	8	69
+0	1	96	2160	8	8	71
+0	1	96	2152	8	8	69
+0	1	96	2112	8	8	69
+0	1	96	2096	8	16	71
+0	1	96	2088	8	8	69
+0	1	104	2088	32	8	70
+0	1	136	2088	8	8	69
+0	1	184	2168	32	8	70
+0	1	176	2168	8	8	69
+0	1	216	2168	8	8	69
+0	1	176	2088	8	8	69
+0	1	184	2088	32	8	70
+0	1	216	2088	8	8	69
+0	1	216	2160	8	8	71
+0	1	216	2152	8	8	69
+0	1	216	2112	8	8	69
+0	1	216	2096	8	16	71
 12	1	152	2216	16	16	custom_obstacle_74	0	1	0	0
 1	1	160	2229	from_outside	1	0
 12	1	328	2128	16	16	custom_obstacle	0	1	0	0
@@ -3701,10 +3711,10 @@
 12	1	2760	112	16	16	custom_obstacle_378	0	1	0	0
 12	1	2888	200	16	16	custom_obstacle_379	0	1	0	0
 2	1	152	2240	16	16	teletransporter	0	1	4	from_chest_game_cave
-7	1	464	2149	enemy	3	simple_green_soldier	0	-1	_random	1	-1
-7	1	160	1912	enemy_2	3	bubble	0	-1	_random	1	-1
-7	1	160	1693	enemy_3	3	tentacle	0	-1	_random	1	-1
 7	1	160	2125	enemy_4	3	globul	0	-1	_random	1	-1
+7	1	160	1693	enemy_3	3	tentacle	0	-1	_random	1	-1
+7	1	160	1912	enemy_2	3	bubble	0	-1	_random	1	-1
+7	1	464	2149	enemy	3	simple_green_soldier	0	-1	_random	1	-1
 0	2	112	2032	96	8	411
 0	2	296	1856	8	96	416
 0	2	112	1808	96	8	411

src/movements/Movement.cpp
@@ -332,7 +332,6 @@ bool Movement::test_collision_with_obstacles(int dx, int dy) {
 
   if (collision) {
     last_collision_box_on_obstacle = collision_box;
-    entity->notify_movement_changed();
   }
 
   return collision;

src/movements/RectilinearMovement.cpp
@@ -29,6 +29,7 @@
  */
 RectilinearMovement::RectilinearMovement(bool ignore_obstacles):
   Movement(ignore_obstacles),
+  angle(0),
   x_speed(0),
   y_speed(0),
   next_move_date_x(System::now()),
@@ -100,6 +101,7 @@ void RectilinearMovement::set_x_speed(double x_speed) {
     }
     set_next_move_date_x(now + x_delay);
   }
+  angle = Geometry::get_angle(0, 0, (int) (x_speed * 100), (int) (y_speed * 100));
 
   if (get_entity() != NULL) {
     get_entity()->notify_movement_changed();
@@ -134,6 +136,7 @@ void RectilinearMovement::set_y_speed(double y_speed) {
     }
     set_next_move_date_y(now + y_delay);
   }
+  angle = Geometry::get_angle(0, 0, (int) (x_speed * 100), (int) (y_speed * 100));
 
   if (get_entity() != NULL) {
     get_entity()->notify_movement_changed();
@@ -154,11 +157,14 @@ void RectilinearMovement::set_speed(double speed) {
   }
 
   // compute the new speed vector
-  double angle = Geometry::get_angle(0, 0,
-                                     (int) (x_speed * 100),
-                                     (int) (y_speed * 100)); // angle in radians
+  double old_angle = this->angle;
   set_x_speed(speed * std::cos(angle));
   set_y_speed(-speed * std::sin(angle));
+  this->angle = old_angle;
+
+  if (get_entity() != NULL) {
+    get_entity()->notify_movement_changed();
+  }
 }
 
 /**
@@ -174,10 +180,16 @@ bool RectilinearMovement::is_started() {
  */
 void RectilinearMovement::stop() {
 
+  double old_angle = this->angle;
   set_x_speed(0);
   set_y_speed(0);
   set_x_move(0);
   set_y_move(0);
+  this->angle = old_angle;
+
+  if (get_entity() != NULL) {
+    get_entity()->notify_movement_changed();
+  }
 }
 
 /**
@@ -216,7 +228,7 @@ void RectilinearMovement::set_next_move_date_y(uint32_t next_move_date_y) {
  */
 double RectilinearMovement::get_angle() {
 
-  return Geometry::get_angle(0, 0, (int) (get_x_speed() * 1000), (int) (get_y_speed() * 1000));
+  return angle;
 }
 
 /**
@@ -230,12 +242,12 @@ double RectilinearMovement::get_angle() {
  */
 void RectilinearMovement::set_angle(double angle) {
 
-  Debug::check_assertion(x_speed != 0 || y_speed != 0,
-    StringConcat() << "Cannot set the angle when the speed is zero (entity: " << get_entity() << ")");
-
-  double speed = get_speed();
-  set_x_speed(speed * std::cos(angle));
-  set_y_speed(-speed * std::sin(angle));
+  if (!is_stopped()) {
+    double speed = get_speed();
+    set_x_speed(speed * std::cos(angle));
+    set_y_speed(-speed * std::sin(angle));
+  }
+  this->angle = angle;
 
   if (get_entity() != NULL) {
     get_entity()->notify_movement_changed();
@@ -248,7 +260,6 @@ void RectilinearMovement::set_angle(double angle) {
  */
 int RectilinearMovement::get_displayed_direction4() {
 
-  double angle = get_angle();
   int direction = (Geometry::radians_to_degrees(angle) + 45 + 360) / 90;
   return direction % 4;
 }
@@ -456,9 +467,6 @@ void RectilinearMovement::set_property(const std::string &key, const std::string
   else if (key == "angle") {
     double angle;
     iss >> angle;
-    if (get_speed() == 0) {
-      set_speed(1);
-    }
     set_angle(angle);
   }
   else if (key == "ignore_obstacles") {

src/movements/TargetMovement.cpp
@@ -30,7 +30,7 @@
  */
 TargetMovement::TargetMovement(int target_x, int target_y, int speed):
 
-  RectilinearMovement(true),
+  SmoothMovement(true),
   target_x(target_x),
   target_y(target_y),
   target_entity(NULL),
@@ -40,6 +40,7 @@ TargetMovement::TargetMovement(int target_x, int target_y, int speed):
   next_recomputation_date(System::now()),
   finished(false) {
 
+  set_ignore_obstacles(true);
 }
 
 /**
@@ -52,7 +53,7 @@ TargetMovement::TargetMovement(int target_x, int target_y, int speed):
  */
 TargetMovement::TargetMovement(MapEntity *target_entity, int speed):
 
-  RectilinearMovement(true),
+  SmoothMovement(true),
   target_x(target_entity->get_x()),
   target_y(target_entity->get_y()),
   target_entity(target_entity),
@@ -62,6 +63,7 @@ TargetMovement::TargetMovement(MapEntity *target_entity, int speed):
   next_recomputation_date(System::now()),
   finished(false) {
 
+  set_ignore_obstacles(true);
 }
 
 /**
@@ -117,7 +119,7 @@ void TargetMovement::update() {
     finished = true;
   }
 
-  RectilinearMovement::update();
+  SmoothMovement::update();
 }
 
 /**

commit 06ea772cfec64df491bf14be08bd200d8181ea68

The screen was shaking in some diagonal movement situations.
This bug was introduced by commit
fdd64b82b5b23fcb41df17efd5eb6cb9f071989a.

src/movements/SmoothMovement.cpp
@@ -94,8 +94,9 @@ void SmoothMovement::update_x() {
         if (y_move != 0 && !test_collision_with_obstacles(0, y_move)) {
           translate_y(y_move);
           set_next_move_date_y(get_next_move_date_y() + get_y_delay() / Geometry::SQRT_2);
+          // FIXME SQRT_2 is really correct only for diagonal angles
         }
-        else {
+        else if (y_move == 0) {
           // the move on x is not possible: let's try
           // to add a move on y to make a diagonal move
 
@@ -178,7 +179,7 @@ void SmoothMovement::update_y() {
           translate_x(x_move);
           set_next_move_date_x(get_next_move_date_x() + get_x_delay() / Geometry::SQRT_2);
         }
-        else {
+        else if (x_move == 0) {
           // The move on y is not possible: let's try
           // to add a move on x to make a diagonal move.
 
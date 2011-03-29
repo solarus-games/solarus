fc8f5169f183eaa8aaa0588e46d46a40270b3063
Christopho <christopho@solarus-engine.org>
2011-03-29 08:24:15 +0200

    Ganon was way too easy

--- quests/zsxd/data/enemies/ganon.lua
+++ quests/zsxd/data/enemies/ganon.lua
@@ -1,12 +1,17 @@
 -- Ganon in the temple of stupidities (2F NE)
 
+being_pushed = false
+
 function event_appear()
 
   sol.enemy.set_life(100000)
-  sol.enemy.set_damage(2)
+  sol.enemy.set_damage(4)
   sol.enemy.create_sprite("enemies/ganon")
   sol.enemy.set_size(16, 16)
   sol.enemy.set_origin(8, 13)
+  sol.enemy.set_invincible()
+  sol.enemy.set_attack_consequence("sword", "custom")
+  sol.enemy.set_attack_consequence("arrow", 1)
 end
 
 function event_restart()
@@ -16,13 +21,15 @@ end
 
 function event_movement_changed()
 
-  m = sol.enemy.get_movement()
-  direction4 = sol.main.movement_get_property(m, "displayed_direction")
-  sprite = sol.enemy.get_sprite()
-  if direction4 == 1 then
-    sol.main.sprite_set_direction(sprite, 1)
-  else
-    sol.main.sprite_set_direction(sprite, 0)
+  if not being_pushed then
+    m = sol.enemy.get_movement()
+    direction4 = sol.main.movement_get_property(m, "displayed_direction")
+    sprite = sol.enemy.get_sprite()
+    if direction4 == 1 then
+      sol.main.sprite_set_direction(sprite, 1)
+    else
+      sol.main.sprite_set_direction(sprite, 0)
+    end
   end
 end
 
@@ -35,6 +42,30 @@ function event_update()
     sprite = sol.enemy.get_sprite()
     sol.main.sprite_set_animation(sprite, "hurt")
     sol.main.play_sound("boss_killed")
+  elseif x > 1176 and not being_pushed then
+    event_restart()
   end
 end
+
+function event_custom_attack_received(attack, sprite)
+
+  if attack == "sword" then
+    sol.main.play_sound("sword_tapping")
+    being_pushed = true
+    x, y = sol.enemy.get_position()
+    hero_x, hero_y = sol.map.hero_get_position()
+    angle = sol.main.get_angle(hero_x, hero_y, x, y)
+    movement = sol.main.temporal_movement_create(128, angle, 200)
+    sol.enemy.start_movement(movement)
+  end
+end
+
+function event_movement_finished(movement)
+
+  if being_pushed then
+    event_restart()
+  end
+end
+
+
 

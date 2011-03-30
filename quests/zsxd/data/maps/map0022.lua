8c5b0a31985a128c194f42a203fa82183feb3803
Christopho <christopho@solarus-engine.org>
2011-03-30 13:52:32 +0200

    Replace the fake chest by an interactive entity. Fix a sound.

--- quests/zsxd/data/maps/map0022.lua
+++ quests/zsxd/data/maps/map0022.lua
@@ -7,8 +7,8 @@ locked_door_A_value = 0
 
 function event_map_started(destination_point_name)
 	-- Coffre farceur	
-	sol.map.chest_set_enabled("prankster_chest_bottom", false)
-	sol.map.chest_set_enabled("prankster_chest_middle", false)
+	sol.map.interactive_entity_set_enabled("prankster_chest_bottom", false)
+	sol.map.interactive_entity_set_enabled("prankster_chest_middle", false)
 
 	-- Interrupteurs
 	if sol.game.savegame_get_boolean(127) then
@@ -169,14 +169,14 @@ end
 function event_hero_on_sensor(sensor_name)
 	-- Mécanisme du coffre farceur dans la salle aux trois portes	
 	if sensor_name == "prankster_sensor_top" then
-		sol.map.chest_set_enabled("prankster_chest_bottom", false)
-		sol.map.chest_set_enabled("prankster_chest_middle", true)
+		sol.map.interactive_entity_set_enabled("prankster_chest_bottom", false)
+		sol.map.interactive_entity_set_enabled("prankster_chest_middle", true)
 	elseif sensor_name == "prankster_sensor_middle" then
-		sol.map.chest_set_enabled("prankster_chest_bottom", true)
-		sol.map.chest_set_enabled("prankster_chest_middle", false)	
+		sol.map.interactive_entity_set_enabled("prankster_chest_bottom", true)
+		sol.map.interactive_entity_set_enabled("prankster_chest_middle", false)	
 	elseif sensor_name == "prankster_sensor_bottom" then
-		sol.map.chest_set_enabled("prankster_chest_bottom", false)
-		sol.map.chest_set_enabled("prankster_chest_middle", true)
+		sol.map.interactive_entity_set_enabled("prankster_chest_bottom", false)
+		sol.map.interactive_entity_set_enabled("prankster_chest_middle", true)
 	end
 end
 

solarus-games/solarus

commit 4fd09499ea80ec75fc4ac1b06adac3d770547ce3

Fix smooth movement again.
The hero could miss things when walking diagonally and
the speed of enemies with target movement was not accurate.

src/movements/TargetMovement.cpp
@@ -21,6 +21,7 @@
 #include "lowlevel/Debug.h"
 #include "lowlevel/StringConcat.h"
 #include <sstream>
+#include <cmath>
 
 /**
  * @brief Creates a new target movement towards a fixed point.
@@ -145,8 +146,10 @@ void TargetMovement::recompute_movement() {
     sign_x = (dx >= 0) ? 1 : -1;
     sign_y = (dy >= 0) ? 1 : -1;
 
-    set_speed(speed);
-    set_angle(angle);
+    if (std::fabs(angle - get_angle()) > 1E-6) {
+      set_speed(speed); // FIXME is this useful?
+      set_angle(angle);
+    }
   }
 }
 

src/movements/SmoothMovement.cpp
@@ -69,7 +69,7 @@ void SmoothMovement::update_x() {
   int x_move = get_x_move();
   int y_move = get_y_move();
   uint32_t x_delay = get_x_delay();
- 
+
   if (x_move != 0) { // the entity wants to move on x
 
     // by default, next_move_date_x will be incremented by x_delay,
@@ -87,16 +87,11 @@ void SmoothMovement::update_x() {
 	if (y_move != 0 && test_collision_with_obstacles(0, y_move)) {
 	  // if there is also a y move, and if this y move is illegal,
 	  // we still allow the x move and we give it all the speed
-	  next_move_date_x_increment = (int) (x_delay / Geometry::SQRT_2);
+	  next_move_date_x_increment = (int) (1000.0 / get_speed());
 	}
       }
       else {
-        if (y_move != 0 && !test_collision_with_obstacles(0, y_move)) {
-          translate_y(y_move);
-          set_next_move_date_y((int) (get_next_move_date_y() + get_y_delay() / Geometry::SQRT_2));
-          // FIXME SQRT_2 is really correct only for diagonal angles
-        }
-        else if (y_move == 0) {
+        if (y_move == 0) {
           // the move on x is not possible: let's try
           // to add a move on y to make a diagonal move
 
@@ -148,7 +143,6 @@ void SmoothMovement::update_y() {
     RectilinearMovement::update_y();
     return;
   }
-
   int x_move = get_x_move();
   int y_move = get_y_move();
   uint32_t y_delay = get_y_delay();
@@ -170,16 +164,11 @@ void SmoothMovement::update_y() {
 	if (x_move != 0 && test_collision_with_obstacles(x_move, 0)) {
 	  // if there is also an x move, and if this x move is illegal,
 	  // we still allow the y move and we give it all the speed
-	  next_move_date_y_increment = (int) (y_delay / Geometry::SQRT_2);
+	  next_move_date_y_increment = (int) (1000.0 / get_speed());
 	}
       }
       else {
-
-        if (x_move != 0 && !test_collision_with_obstacles(x_move, 0)) {
-          translate_x(x_move);
-          set_next_move_date_x((int) (get_next_move_date_x() + get_x_delay() / Geometry::SQRT_2));
-        }
-        else if (x_move == 0) {
+        if (x_move == 0) {
           // The move on y is not possible: let's try
           // to add a move on x to make a diagonal move.
 

commit 26b5e7caa5d6bb7cfabc2cc719b7d2f0261df743

TargetMovement with angle 0 was broken since 4fd09499.

src/CMakeLists.txt
@@ -37,8 +37,8 @@ add_executable(solarus
   ${main_source_file}
 )
 
-set_target_properties(solarus_static PROPERTIES COMPILE_FLAGS "-Wall -Werror")
-set_target_properties(solarus PROPERTIES COMPILE_FLAGS "-Wall -Werror")
+set_target_properties(solarus_static PROPERTIES COMPILE_FLAGS "-g -Wall -Werror")
+set_target_properties(solarus PROPERTIES COMPILE_FLAGS "-g -Wall -Werror")
 
 if (DEFINED DATAPATH)
   add_definitions(-DDATAPATH=\"${DATAPATH}\")

src/movements/TargetMovement.cpp
@@ -146,7 +146,7 @@ void TargetMovement::recompute_movement() {
     sign_x = (dx >= 0) ? 1 : -1;
     sign_y = (dy >= 0) ? 1 : -1;
 
-    if (std::fabs(angle - get_angle()) > 1E-6) {
+    if (std::fabs(angle - get_angle()) > 1E-6 || get_speed() < 1E-6) {
       set_speed(speed); // FIXME is this useful?
       set_angle(angle);
     }
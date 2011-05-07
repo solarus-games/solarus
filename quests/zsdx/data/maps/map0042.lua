2aa88158cd6aee3924ddec74d253a9f6bc766728
Christopho <christopho@solarus-engine.org>
2011-05-07 18:07:56 +0200

    Continue the dungeon 3 of zsdx

--- quests/zsdx/data/maps/map0042.lua
+++ quests/zsdx/data/maps/map0042.lua
@@ -4,4 +4,14 @@ function event_map_started(destination_point_name)
   sol.map.light_set(0)
 end
 
+function event_switch_activated(switch_name)
+
+  if switch_name == "se_door_switch" and not sol.map.door_is_open("se_door") then
+    sol.main.play_sound("secret")
+    sol.map.door_open("se_door")
+  elseif switch_name == "nc_door_switch" and not sol.map.door_is_open("nc_door") then
+    sol.main.play_sound("secret")
+    sol.map.door_open("nc_door")
+  end
+end
 

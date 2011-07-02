7997a64f9031d8869a028760ca455a7bad2aa1ea
Christopho <christopho@solarus-engine.org>
2011-07-02 11:43:55 +0200

    Dungeon 4: block and switches room

---
+++ quests/zsdx/data/maps/map0058.lua
@@ -0,0 +1,10 @@
+-- Dungeon 6 1F
+
+function event_map_opening_transition_finished(destination_point_name)
+
+  -- show the welcome message
+  if destination_point_name == "from_outside" then
+    sol.map.dialog_start("dungeon_6.welcome")
+  end
+end
+

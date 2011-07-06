75f979fd7d494a321fde58b8b8e4a7fc0138b472
Christopho <christopho@solarus-engine.org>
2011-07-06 12:09:58 +0200

    Improve dungeon 4

--- quests/zsdx/data/maps/map0059.lua
+++ quests/zsdx/data/maps/map0059.lua
@@ -1,5 +1,17 @@
 -- Dungeon 4 B1
 
+function event_map_started(destination_point_name)
+
+  x,y = sol.map.hero_get_position()
+  if y < 520 then
+    -- the player is coming from the north part of the map:
+    -- place the block so that the special torch is already disabled
+    sol.map.tile_set_group_enabled("special_torch", false)
+    sol.map.switch_set_activated("special_torch_switch", true)
+    sol.map.block_set_position("block", 752, 453, 1);
+  end
+end
+
 function event_switch_activated(switch_name)
 
   if switch_name == "door_a_switch" then
@@ -9,9 +21,11 @@ function event_switch_activated(switch_name)
     sol.map.door_open("door_b")
     sol.main.play_sound("secret")
   elseif switch_name == "special_torch_switch" then
-    sol.map.tile_set_group_enabled("special_torch", false)
-    sol.main.play_sound("secret")
- end
+    if sol.map.tile_is_enabled("special_torch") then
+      sol.map.tile_set_group_enabled("special_torch", false)
+      sol.main.play_sound("secret")
+    end
+  end
 end
 
 function event_switch_inactivated(switch_name)
@@ -31,7 +45,6 @@ function event_hero_on_sensor(sensor_name)
       and not sol.map.door_is_open("door_b") then
     sol.map.door_close("door_a")
     sol.map.switch_set_activated("door_a_switch", false)
-    sol.map.tile_set_group_enabled("special_torch", true)
   elseif sensor_name == "sensor_1" and sol.map.tile_is_enabled("sensor_1_off") then
     sol.main.play_sound("switch")
     sol.map.tile_set_group_enabled("sensor_1_on", true)

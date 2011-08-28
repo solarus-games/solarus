6cf4ba0cd33c6d255dcd7dc0788194c3933605e3
Christopho <christopho@solarus-engine.org>
2011-08-28 02:15:30 +0200

    Beaumont hill entrance

--- quests/zsdx/data/maps/map0006.lua
+++ quests/zsdx/data/maps/map0006.lua
@@ -1,11 +1,10 @@
------------------------------
 -- Outside world D1 script --
------------------------------
 
 tom_sprite = nil
 
 function event_map_started(destination_point_name)
 
+  -- dungeon 1 ladder
   if is_ladder_activated() then
     sol.map.tile_set_group_enabled("ladder_step", true)
     sol.map.tile_set_group_enabled("no_ladder", false)
@@ -16,19 +15,35 @@ function event_map_started(destination_point_name)
     sol.map.tile_set_group_enabled("no_ladder", true)
     tom_sprite = sol.map.npc_get_sprite("tom")
   end
+
+  -- Beaumont cave entrance
+  if not is_beaumont_cave_open() then
+    sol.map.tile_set_enabled("beaumont_cave_hole", false)
+    sol.map.teletransporter_set_enabled("to_beaumont_cave", false)
+  end
 end
 
 function is_ladder_activated()
   return sol.game.savegame_get_boolean(52)
 end
 
+function is_beaumont_cave_open()
+  return sol.game.savegame_get_boolean(153)
+end
+
+
 function event_hero_on_sensor(sensor_name)
 
   has_finished_tom_cave = sol.game.savegame_get_boolean(37)
 
-  if sensor_name == "tom_appears_sensor" and
-    has_finished_tom_cave and not is_ladder_activated() then
+  if sensor_name == "tom_appears_sensor"
+      and has_finished_tom_cave
+      and not is_ladder_activated() then
     sol.map.dialog_start("outside_world.tom_dungeon_1_entrance.hey")
+  elseif sensor_name == "edelweiss_sensor"
+      and sol.game.get_item("level_4_way") == 3 -- the player has the edelweiss
+      and not is_beaumont_cave_open() then
+    put_edelweiss()
   end
 end
 
@@ -53,6 +68,9 @@ function event_dialog_finished(first_message_id, answer)
   elseif first_message_id == "outside_world.tom_dungeon_1_entrance.open" then
     sol.main.sprite_set_animation(tom_sprite, "walking")
     sol.main.timer_start(300, "tom_timer_3", false)
+  elseif first_message_id == "outside_world.beaumont_hill_put_edelweiss" then
+    sol.map.hero_freeze()
+    sol.main.timer_start(1000, "edelweiss_explode", false)
   end
 
 end
@@ -112,5 +130,25 @@ end
 
 function event_npc_dialog(npc_name)
   sol.map.dialog_start("outside_world.tom_dungeon_1_entrance.finished")
+end
+
+function put_edelweiss()
+  sol.map.dialog_start("outside_world.beaumont_hill_put_edelweiss")
+end
+
+function edelweiss_explode()
+
+  sol.main.play_sound("explosion")
+  sol.map.explosion_create(160, 72, 0)
+  sol.map.tile_set_enabled("beaumont_cave_hole", true)
+  sol.map.teletransporter_set_enabled("to_beaumont_cave", true)
+  sol.game.savegame_set_boolean(153, true)
+  sol.game.set_item("level_4_way", 0)
+  sol.main.timer_start(1000, "edelweiss_end", false)
+end
+
+function edelweiss_end()
+  sol.main.play_sound("secret")
+  sol.map.hero_unfreeze()
 end
 

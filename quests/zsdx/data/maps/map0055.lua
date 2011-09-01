389421349089a3cb240de53b93f9865d7cf372b8
Christopho <christopho@solarus-engine.org>
2011-09-02 00:11:45 +0200

    Improving dungeon 6

--- quests/zsdx/data/maps/map0055.lua
+++ quests/zsdx/data/maps/map0055.lua
@@ -2,9 +2,12 @@
 
 function event_hero_on_sensor(sensor_name)
 
+  -- fix the position of the hero when he lands on barriers after the feather jump
   if sensor_name == "above_barrier_sensor" then
     x, y = sol.map.hero_get_position()
-    if y < 653 then
+    if y > 629 and y < 641 then
+      sol.map.hero_set_position(x, 629, 0)
+    elseif y >= 641 and y < 653 then
       sol.map.hero_set_position(x, 653, 0)
     end
   end

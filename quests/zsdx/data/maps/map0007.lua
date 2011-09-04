ab8eb117a2e07173ae7abbeba6920a25c02d4fdc
Christopho <christopho@solarus-engine.org>
2011-09-04 12:19:38 +0200

    Starting the waterfall cave

--- quests/zsdx/data/maps/map0007.lua
+++ quests/zsdx/data/maps/map0007.lua
@@ -6,12 +6,24 @@ function event_map_started(destination_point_name)
    if sol.game.savegame_get_boolean(193) then
      remove_iron_lock()
    end
+
+   -- remove the wooden lock if open
+   if sol.game.savegame_get_boolean(194) then
+     remove_wooden_lock()
+   end
+
 end
 
 function remove_iron_lock()
    sol.map.interactive_entity_remove("iron_lock")
    sol.map.tile_set_group_enabled("iron_lock_tile", false)
 end
+
+function remove_wooden_lock()
+   sol.map.interactive_entity_remove("wooden_lock")
+   sol.map.tile_set_group_enabled("wooden_lock_tile", false)
+end
+
 
 function event_hero_interaction(entity_name)
 
@@ -25,6 +37,17 @@ function event_hero_interaction(entity_name)
       remove_iron_lock()
     else
       sol.map.dialog_start("outside_world.iron_key_required")
+    end
+  elseif entity_name == "wooden_lock" then
+
+    -- open the door if the player has the wooden key
+    if sol.game.has_item("wooden_key") then
+      sol.main.play_sound("door_open")
+      sol.main.play_sound("secret")
+      sol.game.savegame_set_boolean(194, true)
+      remove_wooden_lock()
+    else
+      sol.map.dialog_start("outside_world.wooden_key_required")
     end
   end
 end
---
+++ quests/zsdx/data/maps/map0080.lua
@@ -0,0 +1,7 @@
+-- Waterfall cave B1
+
+function event_map_started(destination_point_name)
+
+  sol.map.light_set(0)
+end
+

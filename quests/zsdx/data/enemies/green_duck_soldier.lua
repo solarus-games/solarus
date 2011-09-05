0548a9d02548e32c67a14b846afce909de0d922d
Christopho <christopho@solarus-engine.org>
2011-09-05 17:01:39 +0200

    New enemies: green, blue and red duck soldiers

---
+++ quests/zsdx/data/enemies/green_duck_soldier.lua
@@ -0,0 +1,11 @@
+-- Blue pig soldier
+
+sol.main.include("enemies/generic_soldier")
+set_properties({
+  main_sprite = "enemies/green_duck_soldier",
+  sword_sprite = "enemies/green_duck_soldier_sword",
+  life = 4,
+  damage = 3,
+  hurt_sound_style = "monster"
+})
+
---
+++ quests/zsdx/data/enemies/red_duck_soldier.lua
@@ -0,0 +1,11 @@
+-- Blue pig soldier
+
+sol.main.include("enemies/generic_soldier")
+set_properties({
+  main_sprite = "enemies/red_duck_soldier",
+  sword_sprite = "enemies/red_duck_soldier_sword",
+  life = 6,
+  damage = 3,
+  hurt_sound_style = "monster"
+})
+

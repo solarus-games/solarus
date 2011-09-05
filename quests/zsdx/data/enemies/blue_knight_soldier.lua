a5097d295f6667abdbda7014601455870fed9aff
Christopho <christopho@solarus-engine.org>
2011-09-05 15:59:22 +0200

    Add blue and red knight soldiers

---
+++ quests/zsdx/data/enemies/blue_knight_soldier.lua
@@ -0,0 +1,11 @@
+-- Blue knight soldier
+
+sol.main.include("enemies/generic_soldier")
+set_properties({
+  main_sprite = "enemies/blue_knight_soldier",
+  sword_sprite = "enemies/blue_knight_soldier_sword",
+  life = 3,
+  damage = 2,
+  play_hero_seen_sound = true
+})
+

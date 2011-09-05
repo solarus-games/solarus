3486a5446eb3131420bc6ba4c53fa60d13583943
Christopho <christopho@solarus-engine.org>
2011-09-05 14:23:43 +0200

    New enemy: green knight soldier

---
+++ quests/zsdx/data/enemies/green_knight_soldier.lua
@@ -0,0 +1,11 @@
+-- Green knight soldier
+
+sol.main.include("enemies/generic_soldier")
+set_properties({
+  main_sprite = "enemies/green_knight_soldier",
+  sword_sprite = "enemies/green_knight_soldier_sword",
+  life = 3,
+  damage = 2,
+  play_hero_seen_sound = true
+})
+

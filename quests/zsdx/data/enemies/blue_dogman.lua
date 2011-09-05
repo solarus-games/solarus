956617f9e6ba53c002b2ebc2146356e7831c765f
Christopho <christopho@solarus-engine.org>
2011-09-05 17:59:49 +0200

    New enemies: blue and red dogman

---
+++ quests/zsdx/data/enemies/blue_dogman.lua
@@ -0,0 +1,11 @@
+-- Blue Dogman
+
+sol.main.include("enemies/generic_towards_hero")
+set_properties({
+  sprite = "enemies/blue_dogman",
+  life = 6,
+  damage = 4,
+  normal_speed = 48,
+  faster_speed = 48
+})
+
---
+++ quests/zsdx/data/enemies/red_dogman.lua
@@ -0,0 +1,11 @@
+-- Red Dogman
+
+sol.main.include("enemies/generic_towards_hero")
+set_properties({
+  sprite = "enemies/red_dogman",
+  life = 8,
+  damage = 4,
+  normal_speed = 48,
+  faster_speed = 48
+})
+

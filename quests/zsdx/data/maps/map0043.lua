469accfcb37d4ac1ed859118c1f76e40cc975393
Christopho <christopho@solarus-engine.org>
2011-09-08 00:02:15 +0200

    Dungeon 3 final room

--- quests/zsdx/data/maps/map0043.lua
+++ quests/zsdx/data/maps/map0043.lua
@@ -3,9 +3,15 @@
 function event_map_started(destination_point_name)
 
   sol.map.door_set_open("miniboss_door", true)
+  sol.map.door_set_open("boss_door", true)
+  if destination_point_name == "from_5f_c"
+      or sol.game.savegame_get_boolean(903) then
+    sol.map.door_set_open("final_room_door", true)
+  end
 end
 
 fighting_miniboss = false
+fighting_boss = false
 
 function event_hero_on_sensor(sensor_name)
 
@@ -21,10 +27,15 @@ function event_hero_on_sensor(sensor_name)
     sol.map.hero_freeze()
     sol.main.timer_start(1000, "miniboss_timer", false)
     fighting_miniboss = true
+  elseif sensor_name == "start_boss_sensor"
+      and not sol.game.savegame_get_boolean(902)
+      and not fighting_boss then
+    start_boss()
   end
 end
 
 function miniboss_timer()
+
   sol.main.play_music("boss.spc")
   sol.map.enemy_set_enabled("miniboss", true)
   sol.map.tile_set_group_enabled("miniboss_prickles", true)
@@ -38,5 +49,31 @@ function event_enemy_dead(enemy_name)
     sol.map.door_open("miniboss_door")
     sol.map.tile_set_group_enabled("miniboss_prickles", false)
   end
+end
+
+function start_boss()
+
+  fighting_boss = true
+  sol.map.enemy_set_enabled("boss", true)
+  sol.map.door_close("boss_door")
+  sol.main.play_music("boss.spc")
+  sol.main.play_sound("door_closed")
+end
+
+function event_treasure_obtained(item_name, variant, savegame_variable)
+
+  if item_name == "heart_container" then
+    sol.main.timer_start(9000, "open_final_room", false)
+    sol.main.play_music("victory.spc")
+    sol.map.hero_freeze()
+    sol.map.hero_set_direction(3)
+  end
+end
+
+function open_final_room()
+
+  sol.map.door_open("final_room_door")
+  sol.main.play_sound("secret")
+  sol.map.hero_unfreeze()
 end
 
---
+++ quests/zsdx/data/maps/map0085.lua
@@ -0,0 +1,41 @@
+-- Dungeon 3 final room
+
+function event_map_opening_transition_finished(destination_point_name)
+  solarus_child_sprite = sol.map.npc_get_sprite("solarus_child")
+  sol.map.npc_set_position("solarus_child", 160, 165)
+  sol.main.sprite_set_animation(solarus_child_sprite, "stopped")
+  sol.main.sprite_set_animation_ignore_suspend(solarus_child_sprite, true)
+end
+
+function event_npc_dialog(npc_name)
+
+  if npc_name == "solarus_child" then
+    if sol.game.is_dungeon_finished(3) then
+      -- dialog already done
+      sol.main.play_sound("warp")
+      sol.map.hero_set_map(3, "from_dungeon_3", 1)
+    else
+      -- start the final sequence
+      sol.map.camera_move(160, 120, 100)
+    end
+  end
+end
+
+function event_camera_reached_target()
+  sol.map.dialog_start("dungeon_3.solarus_child")
+  sol.map.dialog_set_variable("dungeon_3.solarus_child", sol.game.savegame_get_name());
+end
+
+function event_dialog_finished(first_message_id, answer)
+
+  if first_message_id == "dungeon_3.solarus_child" then
+    sol.map.hero_start_victory_sequence()
+  end
+
+end
+
+function event_hero_victory_sequence_finished()
+  sol.game.set_dungeon_finished(3)
+  sol.map.hero_set_map(3, "from_dungeon_3", 1)
+end
+

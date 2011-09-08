e253302a064a08505ad7ba807191cf1dc394db9a
Christopho <christopho@solarus-engine.org>
2011-09-08 18:12:58 +0200

    Dialogs of the telepathic booth

--- quests/zsdx/data/maps/map0032.lua
+++ quests/zsdx/data/maps/map0032.lua
@@ -6,13 +6,19 @@ function event_hero_interaction(entity_name)
 
   if entity_name == "hint_stone" then
 
-    if not sol.game.savegame_get_boolean(103) then
+    if not sol.game.is_dungeon_finished(2) then
       sol.map.dialog_start("telepathic_booth.not_working")
-    else
+    elseif not sol.game.is_dungeon_finished(3) then
       sol.map.dialog_start("telepathic_booth.go_master_arbror")
       sol.map.dialog_set_variable("telepathic_booth.go_master_arbror", sol.game.savegame_get_name())
+    elseif not sol.game.is_dungeon_finished(4) then
+      sol.map.dialog_start("telepathic_booth.go_billy")
+      sol.map.dialog_set_variable("telepathic_booth.go_billy", sol.game.savegame_get_name())
+    elseif not sol.game.is_dungeon_finished(5) then
+      sol.map.dialog_start("telepathic_booth.hahaha")
+      sol.map.dialog_set_variable("telepathic_booth.hahaha", sol.game.savegame_get_name())
     end
-    -- TODO after dungeon 3
+    -- TODO after dungeon 5
   end
 end
 

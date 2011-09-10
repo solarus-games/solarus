01c9ea2ea3f216cce2a97cce712ad6ba334514c2
Christopho <christopho@solarus-engine.org>
2011-09-10 12:11:20 +0200

    Dungeon 4 final cutscene

--- quests/zsdx/data/maps/map0087.lua
+++ quests/zsdx/data/maps/map0087.lua
@@ -1,6 +1,7 @@
 -- Dungeon 4 final room
 
 function event_map_opening_transition_finished(destination_point_name)
+
   solarus_child_sprite = sol.map.npc_get_sprite("solarus_child")
   sol.map.npc_set_position("solarus_child", 160, 165)
   sol.main.sprite_set_animation(solarus_child_sprite, "stopped")
@@ -30,13 +31,58 @@ function event_dialog_finished(first_message_id, answer)
 
   if first_message_id == "dungeon_4.solarus_child" then
     sol.map.hero_start_victory_sequence()
+  elseif first_message_id == "dungeon_4.agahnim" then
+    agahnim_sequence_5()
   end
 end
 
 function event_hero_victory_sequence_finished()
-  -- TODO
-  --sol.game.set_dungeon_finished(4)
-  --sol.map.hero_set_map(66, "from_dungeon_4", 1)
+
+  sol.main.timer_start(2000, "agahnim_sequence_1", false)
 end
 
+function agahnim_sequence_1()
+
+  sol.map.hero_unfreeze()
+  sol.map.hero_set_direction(1)
+  sol.main.timer_start(1000, "agahnim_sequence_2", false)
+end
+
+function agahnim_sequence_2()
+
+  sprite = sol.map.npc_get_sprite("solarus_child")
+  sol.main.sprite_fade(sprite, 1)
+  sol.main.play_music("none")
+  sol.main.play_sound("warp")
+  sol.main.timer_start(3000, "agahnim_sequence_3", false)
+end
+
+function agahnim_sequence_3()
+
+  sol.map.npc_set_position("agahnim", 160, 141)
+  sol.main.play_music("agahnim.spc")
+  sol.main.timer_start(1000, "agahnim_sequence_4", false)
+end
+
+function agahnim_sequence_4()
+
+  sol.map.dialog_start("dungeon_4.agahnim")
+end
+
+function agahnim_sequence_5()
+
+-- FIXME not working
+--  s = sol.map.npc_get_sprite("agahnim")
+--  sol.main.sprite_fade(s, 1)
+--  sol.main.play_sound("warp")
+  sol.main.timer_start(2000, "agahnim_sequence_6", false)
+end
+
+function agahnim_sequence_6()
+
+  sol.game.set_dungeon_finished(4)
+  sol.game.savegame_set_boolean(905, true) -- enable the dark world
+  sol.game.savegame_set_boolean(155, true) -- break the rupee house
+  sol.map.hero_set_map(66, "from_dungeon_4", 1)
+end
 

c1f6dcc7700b7465cee381133bca11abd9c75915
Christophe Thiéry <christopho128@gmail.com>
2011-09-20 11:06:19 +0200

    Continuing Master Arbror

--- quests/zsdx/data/enemies/arbror_root.lua
+++ quests/zsdx/data/enemies/arbror_root.lua
@@ -4,7 +4,7 @@ function event_appear()
 
   sol.enemy.set_life(1)
   sol.enemy.set_damage(2)
-  sol.enemy.create_sprite("enemies/master_arbror")
+  sol.enemy.create_sprite("enemies/arbror_root")
   sol.enemy.set_size(64, 96)
   sol.enemy.set_origin(28, 86)
   sol.enemy.set_invincible()

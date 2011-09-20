ae2a21b45559a2f9dc7a5a182299afc5477c242e
Christophe Thiéry <christopho128@gmail.com>
2011-09-20 10:54:53 +0200

    Master Arbror's roots (work in progress)

---
+++ quests/zsdx/data/enemies/arbror_root.lua
@@ -0,0 +1,13 @@
+-- A root of Master Arbror
+
+function event_appear()
+
+  sol.enemy.set_life(1)
+  sol.enemy.set_damage(2)
+  sol.enemy.create_sprite("enemies/master_arbror")
+  sol.enemy.set_size(64, 96)
+  sol.enemy.set_origin(28, 86)
+  sol.enemy.set_invincible()
+  sol.enemy.set_attack_consequence("hookshot", "immobilized")
+end
+

2800e87f51dbcde9bacf9da04655af442615333c
Christopho <christopho@solarus-engine.org>
2011-09-25 23:15:14 +0200

    Adding Inferno

--- quests/zsdx/data/maps/map0007.lua
+++ quests/zsdx/data/maps/map0007.lua
@@ -17,6 +17,14 @@ function event_map_started(destination_point_name)
   if sol.game.savegame_get_boolean(194) then
     remove_wooden_lock()
   end
+
+  -- dungeon 6 entrance
+  if sol.game.savegame_get_boolean(914) then
+    inferno_set_open()
+  else
+    sol.map.teletransporter_set_enabled("to_dungeon_6", false)
+  end
+  sol.map.sensor_set_enabled("inferno_sensor", false)
 end
 
 function remove_iron_lock()
@@ -55,5 +63,45 @@ function event_hero_interaction(entity_name)
       sol.map.dialog_start("outside_world.wooden_key_required")
     end
   end
+end
+
+function event_npc_dialog(npc_name)
+
+  if npc_name == "inferno" then
+    inferno_open()
+  end
+end
+
+function inferno_open()
+
+  sol.map.sensor_set_enabled("inferno_sensor", true)
+  sol.map.hero_walk("66", false, false)
+end
+
+function event_hero_on_sensor(sensor_name)
+
+  if sensor_name == "inferno_sensor" then
+    local sprite = sol.map.npc_get_sprite("inferno")
+    sol.main.sprite_set_animation(sprite, "opening")
+    sol.main.timer_start(1200, "inferno_open_finish")
+    sol.map.hero_freeze()
+    sol.map.hero_set_direction(1)
+    sol.map.sensor_set_enabled("inferno_sensor", false)
+  end
+end
+
+function inferno_open_finish()
+
+  sol.main.play_sound("secret")
+  sol.map.hero_unfreeze()
+  sol.game.savegame_set_boolean(914, true)
+  inferno_set_open()
+end
+
+function inferno_set_open()
+
+  local sprite = sol.map.npc_get_sprite("inferno")
+  sol.main.sprite_set_animation(sprite, "open")
+  sol.map.teletransporter_set_enabled("to_dungeon_6", true)
 end
 

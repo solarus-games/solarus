5c61ec6291800603eedef57e7a36c2e059520481
Christopho <christopho@solarus-engine.org>
2011-10-07 15:52:51 +0200

    New enemy: hardhat beetle

---
+++ quests/zsdx/data/enemies/blue_hardhat_beetle.lua
@@ -0,0 +1,14 @@
+-- Blue Hardhat Beetle
+
+sol.main.include("enemies/generic_towards_hero")
+set_properties({
+  sprite = "enemies/blue_hardhat_beetle",
+  life = 5,
+  damage = 2,
+  normal_speed = 32,
+  faster_speed = 48,
+  hurt_sound_style = "monster",
+  push_hero_on_sword = true,
+  movement_create = sol.main.random_movement_create
+})
+
---
+++ quests/zsdx/data/enemies/red_hardhat_beetle.lua
@@ -0,0 +1,14 @@
+-- Red Hardhat Beetle
+
+sol.main.include("enemies/generic_towards_hero")
+set_properties({
+  sprite = "enemies/red_hardhat_beetle",
+  life = 8,
+  damage = 3,
+  normal_speed = 32,
+  faster_speed = 48,
+  hurt_sound_style = "monster",
+  push_hero_on_sword = true,
+  movement_create = sol.main.random_movement_create
+})
+

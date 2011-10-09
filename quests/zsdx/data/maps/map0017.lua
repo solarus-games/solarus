8d056fb4e2a91a20b889852b253c773ad5f7d842
Binbin <binbin1984@hotmail.fr>
2011-10-09 15:13:14 +0200

    Binbin dungeon

--- quests/zsdx/data/maps/map0017.lua
+++ quests/zsdx/data/maps/map0017.lua
@@ -5,7 +5,17 @@ function event_map_started(destination_point_name)
 	-- Init blocks
 	-- Init chest
 	-- Init stairs	
+  	if sol.game.savegame_get_boolean(201) then
+   		lock_torches()
+	end
 
+end
+
+function event_door_open(door_name)
+
+	if (door_name=="weak_door") then
+		sol.main.play_sound("secret")		
+	end
 end
 
 function event_map_opening_transition_finished(destination_point_name)
@@ -16,11 +26,10 @@ function event_map_opening_transition_finished(destination_point_name)
 
 end
 
-
 function event_enemy_dead(enemy_name)
 
-	if sol.map.enemy_is_group_dead("enemy_group1") and not sol.game.savegame_get_boolean(202) then
-		sol.map.pickable_item_create("small_key", 1, "202", 608,544, 1)
+	if sol.map.enemy_is_group_dead("enemy_group1") and not sol.game.savegame_get_boolean(200) then
+		sol.map.pickable_item_create("small_key", 1, "200", 608,544, 1)
 		sol.main.play_sound("chest_appears")
 	end
 
@@ -36,15 +45,32 @@ function event_switch_activated(switch_name)
 
 end
 
-function event_hero_on_sensor(sensor_name)
+function are_all_torches_on()
 
-
+  return sol.map.interactive_entity_exists("torch1")
+      and sol.main.sprite_get_animation(sol.map.interactive_entity_get_sprite("torch1")) == "lit"
+      and sol.main.sprite_get_animation(sol.map.interactive_entity_get_sprite("torch2")) == "lit"
+      and sol.main.sprite_get_animation(sol.map.interactive_entity_get_sprite("torch3")) == "lit"
+      and sol.main.sprite_get_animation(sol.map.interactive_entity_get_sprite("torch4")) == "lit"
 
 end
 
-function event_camera_reached_target()
+function lock_torches()
 
-	sol.map.door_open("seroom_door_close")
-	sol.map.camera_restore()
+  sol.map.interactive_entity_remove("torch1")
+  sol.map.interactive_entity_remove("torch2")
+  sol.map.interactive_entity_remove("torch3")
+  sol.map.interactive_entity_remove("torch4")
 
+end
+
+function event_update()
+
+	if not sol.game.savegame_get_boolean(201) and are_all_torches_on() 
+	 then
+		sol.main.play_sound("chest_appears")
+		sol.map.pickable_item_create("small_key", 1, "202", 232,496, 0)
+		sol.game.savegame_set_boolean(201, true)
+   		lock_torches()
+	end
 end

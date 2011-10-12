f5663126c372f3917b4e32eb86271eeb0638d8d5
Christopho <christopho@solarus-engine.org>
2011-10-12 19:25:57 +0200

    Add new musics

--- quests/zsdx/data/maps/map0010.lua
+++ quests/zsdx/data/maps/map0010.lua
@@ -4,7 +4,6 @@ function event_map_started(destination_point_name)
 
   -- enable dark world
   if sol.game.savegame_get_boolean(905) then
-    sol.main.play_music("dark_mountain.spc")
     sol.map.tileset_set(13)
   end
 end

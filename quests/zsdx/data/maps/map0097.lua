1756c942cf03bb48a4ace0ae0e9a6bf0417e4651
Binbin <binbin1984@hotmail.fr>
2011-10-16 17:03:41 +0200

    Binbin dungeon

--- quests/zsdx/data/maps/map0097.lua
+++ quests/zsdx/data/maps/map0097.lua
@@ -45,7 +45,60 @@ function lock_torches_group2()
 
 end
 
+function event_switch_activated(switch_name)
 
+	if not sol.game.savegame_get_boolean(228) then
+		if switch_name == "switch1_1"  then
+			error = false
+			if sol.map.switch_is_activated("switch1_2") 
+			or sol.map.switch_is_activated("switch1_3") 
+			or sol.map.switch_is_activated("switch1_4") then
+				error = true
+			end
+		end
+
+		if switch_name == "switch1_2" then
+			if sol.map.switch_is_activated("switch1_1") == false  then
+				error = true
+			end
+		end
+
+		if switch_name == "switch1_3" then
+			if  sol.map.switch_is_activated("switch1_2") == false then
+				error = true			
+			end
+		end
+
+		if switch_name == "switch1_4" then
+			if  sol.map.switch_is_activated("switch1_3") == false then
+				error = true			
+			end
+		end
+
+		if error==true then
+			switch1_error()
+		end
+
+		if sol.map.switch_is_activated("switch1_1") 
+			and sol.map.switch_is_activated("switch1_2") 
+			and sol.map.switch_is_activated("switch1_3")
+			and sol.map.switch_is_activated("switch1_4") then
+			sol.main.play_sound("secret")
+			sol.map.pickable_item_create("small_key", 1, "227", 144,504, 0)
+			sol.game.savegame_set_boolean(228, true)
+		end
+
+	end
+end
+
+function switch1_error ()
+	sol.main.play_sound("wrong")
+	sol.map.switch_set_activated("switch1_1",false)
+	sol.map.switch_set_activated("switch1_2",false)
+	sol.map.switch_set_activated("switch1_3",false)
+	sol.map.switch_set_activated("switch1_4",false)
+	
+end
 
 function event_enemy_dead(enemy_name)
 

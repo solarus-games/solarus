735d0cd860c3856e27964ee81cbae7d68ed71b27
Christopho <christopho@solarus-engine.org>
2011-10-16 15:32:15 +0200

    East mountains

--- quests/zsdx/data/maps/map0010.lua
+++ quests/zsdx/data/maps/map0010.lua
@@ -6,6 +6,53 @@ function event_map_started(destination_point_name)
   if sol.game.savegame_get_boolean(905) then
     sol.map.tileset_set(13)
   end
+
+  if sol.game.savegame_get_boolean(921) then
+    -- show the vine
+    sol.map.interactive_entity_remove("vine_start")
+    sol.map.interactive_entity_remove("vine")
+    sol.map.tile_set_group_enabled("vine", false)
+  else
+    local sprite = sol.map.interactive_entity_get_sprite("vine")
+    sol.main.sprite_set_paused(sprite, true)
+  end
 end
 
+-- Function called when the player presses the action key on the vine bottom
+function event_hero_interaction(entity_name)
+
+  if entity_name == "vine_start" then
+    sol.map.dialog_start("outside_world.vine_start")
+  end
+end
+
+-- Function called when the player uses an item on the vine bottom
+function event_hero_interaction_item(entity_name, item_name, variant)
+
+  if entity_name == "vine_start" and
+      string.find(item_name, "^bottle") and variant == 2 then
+
+    -- using water on the vine bottom
+    sol.map.hero_freeze()
+    sol.game.set_item(item_name, 1) -- make the bottle empty
+    sol.main.play_sound("item_in_water")
+    sol.main.timer_start(show_vine, 1000)
+    return true
+  end
+
+  return false
+end
+
+function show_vine()
+
+  sol.map.camera_move(608, 136, 136, function()
+    sol.main.play_sound("secret")
+    sol.map.hero_unfreeze()
+    sol.game.savegame_set_boolean(921, true)
+  end)
+
+  local sprite = sol.map.interactive_entity_get_sprite("vine")
+  sol.main.sprite_set_animation_ignore_suspend(sprite, true)
+  sol.main.sprite_set_paused(sprite, false)
+end
 

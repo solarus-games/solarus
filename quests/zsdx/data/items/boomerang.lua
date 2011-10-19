778216912b82fab4ff017a361d7a108872ce3af3
Christopho <christopho@solarus-engine.org>
2011-10-19 18:41:10 +0200

    Add the red boomerang

--- quests/zsdx/data/items/boomerang.lua
+++ quests/zsdx/data/items/boomerang.lua
@@ -1,5 +1,11 @@
 function event_use()
-  sol.map.hero_start_boomerang(96, 160, "entities/boomerang")
+
+  if sol.item.get_variant() == 1 then
+    sol.map.hero_start_boomerang(96, 160, "entities/boomerang1")
+  else
+    -- boomerang 2: longer and faster movement
+    sol.map.hero_start_boomerang(192, 320, "entities/boomerang2")
+  end
   sol.item.set_finished()
 end
 

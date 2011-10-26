4c6951d984808901e499437878868d243859b34f
Christopho <christopho@solarus-engine.org>
2011-10-26 23:25:18 +0200

    Finish the Cane of Somaria

--- quests/zsdx/data/items/cane_of_somaria.lua
+++ quests/zsdx/data/items/cane_of_somaria.lua
@@ -1,40 +1,63 @@
+created = false -- indicates a Somaria block has been created on the current map
+
 function event_use()
 
   local magic_needed = 7
   if sol.game.get_magic() >= magic_needed then
-    sol.main.play_sound("bomb")
+
+    sol.main.play_sound("cane")
+    sol.main.play_sound("magic_bar")
     sol.game.remove_magic(magic_needed)
 
-    -- TODO: remove previous somaria blocks
     local x, y, layer = get_block_position_from_hero()
-    sol.map.block_create(x, y, layer, "somaria_block",
-    {
-      sprite_name = "entities/somaria_block",
-      can_be_pushed = true,
-      can_be_pulled = true,
-      maximum_moves = 2 -- infinite moves
-    })
+
+    if not created then
+
+      -- create the Somaria block
+      sol.map.block_create(x, y, layer, "somaria_block",
+      {
+	sprite_name = "entities/somaria_block",
+	can_be_pushed = true,
+	can_be_pulled = true,
+	maximum_moves = 2 -- infinite moves
+      })
+      created = true
+    else
+      -- reuse the old one
+      sol.map.block_set_position("somaria_block", x, y, layer)
+    end
   else
     sol.main.play_sound("wrong")
   end
   sol.item.set_finished()
 end
 
+-- Called when the current map changes
+function event_map_changed()
+
+  -- no more Somaria block on the new map
+  created = false
+end
+
+
 function get_block_position_from_hero()
 
-  -- TODO snap to the grid
-
+  -- compute a position
   local x, y, layer = sol.map.hero_get_position()
   local direction = sol.map.hero_get_direction()
   if direction == 0 then
-    x = x + 16
+    x = x + 20
   elseif direction == 1 then
-    y = y - 16
+    y = y - 20
   elseif direction == 2 then
-    x = x - 16
+    x = x - 20
   elseif direction == 3 then
-    y = y + 16
+    y = y + 20
   end
+
+  -- snap the center of the block to the 8*8 grid
+  x = (x + 4) - (x + 4) % 8
+  y = (y - 1) - (y - 1) % 8 + 5
 
   return x, y, layer
 end

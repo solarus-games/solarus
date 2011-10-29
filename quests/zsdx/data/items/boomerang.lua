edaf92f601d4e2d65b7215dc500159524dba3c44
Christopho <christopho@solarus-engine.org>
2011-10-29 17:28:45 +0200

    Show the appropriate boomerang animation on the tunic sprite

--- quests/zsdx/data/items/boomerang.lua
+++ quests/zsdx/data/items/boomerang.lua
@@ -1,10 +1,10 @@
 function event_use()
 
   if sol.item.get_variant() == 1 then
-    sol.map.hero_start_boomerang(96, 160, "entities/boomerang1")
+    sol.map.hero_start_boomerang(96, 160, "boomerang1", "entities/boomerang1")
   else
     -- boomerang 2: longer and faster movement
-    sol.map.hero_start_boomerang(192, 320, "entities/boomerang2")
+    sol.map.hero_start_boomerang(192, 320, "boomerang2", "entities/boomerang2")
   end
   sol.item.set_finished()
 end

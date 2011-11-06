d05fe5ee3247e0cfd12f31e5cfeaaaba5ef8109b
Christopho <christopho@solarus-engine.org>
2011-11-06 12:27:15 +0100

    Finish the boss of dungeon 4

--- quests/zsdx/data/enemies/snap_dragon.lua
+++ quests/zsdx/data/enemies/snap_dragon.lua
@@ -2,7 +2,7 @@
 
 function event_appear()
 
-  sol.enemy.set_life(4)
+  sol.enemy.set_life(3)
   sol.enemy.set_damage(4)
   sol.enemy.create_sprite("enemies/snap_dragon")
   sol.enemy.set_hurt_style("normal")
@@ -25,6 +25,11 @@ function event_obstacle_reached()
   go_random()
 end
 
+function event_hurt(attack, life_lost)
+
+  sol.main.timer_stop_all()
+end
+
 function go_random()
 
   -- random diagonal direction
@@ -38,6 +43,7 @@ function go_random()
   local sprite = sol.enemy.get_sprite()
   sol.main.sprite_set_direction(sprite, rand4 - 1)
 
+  sol.main.timer_stop_all()
   sol.main.timer_start(function()
     sol.main.sprite_set_animation(sprite, "bite")
   end, 300 + math.random(1500))

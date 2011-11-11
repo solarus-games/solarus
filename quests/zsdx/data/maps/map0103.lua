ed29e9ae5c1f8b4bf5c7544634223398d11ca2ef
Christopho <christopho@solarus-engine.org>
2011-11-11 23:05:50 +0100

    Dungeon 7: evil timed doors

--- quests/zsdx/data/maps/map0103.lua
+++ quests/zsdx/data/maps/map0103.lua
@@ -3,6 +3,16 @@
 just_removed_special_torch = false
 special_torch_allow_close = true
 
+doors = { -- properties of the 5 timed doors
+  door_a = { x = 920, y = 640, delay = 12000 },
+  door_b = { x = 864, y = 808, delay = 15000 },
+  door_c = { x = 1024, y = 840, delay = 12000 },
+  door_d = { x = 832, y = 936, delay = 4000 },
+  door_e = { x = 976, y = 952, delay = 4000 }
+}
+current_door_name = nil -- current door during a camera movement
+door_timers = {} -- doors that currently have a timer running
+
 function event_map_started(destination_point_name)
 
   -- block fallen into the hole
@@ -13,6 +23,11 @@ function event_map_started(destination_point_name)
   -- NW door
   if sol.game.savegame_get_boolean(624) then
     sol.map.door_set_open("ne_door", true)
+  end
+
+  -- timed doors
+  if destination_point_name == "from_2f_11" then
+    sol.map.door_set_open("door_a", false)
   end
 end
 
@@ -72,13 +87,23 @@ function event_switch_activated(switch_name)
       sol.map.tile_set_enabled("special_torch", false)
       just_removed_special_torch = true
     end)
+
+  -- timed doors
+  else
+    current_door_name = switch_name:match("^(door_[a-e])_switch$")
+    if current_door_name ~= nil then
+      door = doors[current_door_name]
+      sol.map.camera_move(door.x, door.y, 250, function()
+	sol.map.door_open(current_door_name)
+      end)
+    end
   end
 end
 
 function event_camera_back()
 
+  -- set up a timer when the camera movement is finished
   if just_removed_special_torch then
-    -- set up a timer when the camera movement is finished
     just_removed_special_torch = false
     sol.main.timer_start(function()
       sol.map.switch_set_activated("special_torch_switch", false)
@@ -87,6 +112,19 @@ function event_camera_back()
         sol.map.tile_set_enabled("special_torch", true)
       end
     end, 8000, true)
+
+  elseif current_door_name ~= nil then
+    local door_name = current_door_name
+    sol.main.timer_start(function()
+      if door_timers[door_name] ~= nil then
+	sol.map.door_close(door_name)
+	sol.map.switch_set_activated(door_name .. "_switch", false)
+	door_timers[door_name] = nil
+      end
+    end, doors[door_name].delay, true)
+    door_timers[door_name] = true
+    current_door_name = nil
+
   end
 end
 
@@ -109,6 +147,27 @@ function event_hero_on_sensor(sensor_name)
     local state = sol.map.tile_is_enabled("w_room_tile_1")
     sol.map.tile_set_enabled("w_room_tile_1", not state)
     sol.map.tile_set_enabled("w_room_tile_2", state)
+
+  else
+    -- pass a timed door
+    local door_name = sensor_name:match("^(door_[a-e])_done_sensor$")
+    if door_name ~= nil then
+      door_timers[door_name] = nil -- disable the timer
+      if door_name == "door_a" then
+	-- just passed the last one
+	sol.main.play_sound("secret")
+      end
+    else
+
+      -- close a timed door previously passed (i.e. it has no current timer)
+      door_name = sensor_name:match("^(door_[a-e])_close_sensor$")
+      if door_name ~= nil then
+        if door_timers[door_name] == nil and sol.map.door_is_open(door_name) then
+	  sol.map.door_close(door_name)
+	  sol.map.switch_set_activated(door_name .. "_switch", false)
+	end
+      end
+    end
   end
 end
 

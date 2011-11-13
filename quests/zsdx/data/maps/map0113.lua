36cddcdca352a74b84b06d6a3bbf4e6db5fe4d9f
Christopho <christopho@solarus-engine.org>
2011-11-13 20:12:36 +0100

    Finish the east mountains cave

---
+++ quests/zsdx/data/maps/map0113.lua
@@ -0,0 +1,7 @@
+-- East mountains cave 3F
+
+function event_map_started(destination_point_name)
+
+  sol.map.light_set(0)
+end
+
---
+++ quests/zsdx/data/maps/map0114.lua
@@ -0,0 +1,8 @@
+-- East mountains cave 4F
+
+function event_map_started(destination_point_name)
+
+  sol.map.light_set(0)
+end
+
+

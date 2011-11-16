55fa9b4acae165e8d0f538b3cb997f496591ed4e
Christopho <christopho@solarus-engine.org>
2011-11-16 20:22:21 +0100

    Creating entrance of dungeon 10

---
+++ quests/zsdx/data/maps/map0119.lua
@@ -0,0 +1,11 @@
+-- Outside world C3
+
+function event_map_started(destination_point_name)
+
+  -- enable dark world
+  if sol.game.savegame_get_boolean(905) then
+    sol.main.play_music("dark_world.spc")
+    sol.map.tileset_set(13)
+  end
+end
+

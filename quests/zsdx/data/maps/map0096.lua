aee43aa1882e8b840a40386da2f2c2b580688a3b
Christopho <christopho@solarus-engine.org>
2011-11-21 03:47:09 +0100

    Dungeon 10 is now okay, I have fixed the maps and scripts.

--- quests/zsdx/data/maps/map0096.lua
+++ quests/zsdx/data/maps/map0096.lua
@@ -1,13 +1,24 @@
+-- Dungeon 10 3F
+
 door3 = false
+
 function event_map_started(destination_point_name)
-	sol.map.door_open("door1")
- 	sol.map.door_open("door2")
-	init_evil_tiles()
+
+  sol.map.door_set_open("door1", true)
+  sol.map.door_set_open("door2", true)
+  init_evil_tiles()
+
+  if destination_point_name == "entrance_F_1" then
+    sol.map.sensor_set_enabled("sensor1", false)
+    sol.map.door_set_open("door3", true)
+    sol.map.switch_set_activated("switch1_1", true)
+  end
 end
 
 -- Initializes evil tiles (this function should be called
 -- from event_map_started)
 function init_evil_tiles()
+
   sol.map.enemy_set_group_enabled("evil", false)
   sol.map.tile_set_group_enabled("evil_after", false)
 end
@@ -21,13 +32,13 @@ function start_evil_tiles()
   local spawn_delay = 1500 -- delay between two tiles
 
   sol.map.enemy_set_group_enabled("evil", false)
- sol.map.tile_set_group_enabled("evil_after", false)
+  sol.map.tile_set_group_enabled("evil_after", false)
 
   -- spawns a tile and schedules the next one
   function repeat_spawn()
 
-    sol.map.enemy_set_enabled("evil_"..next, true)
-    sol.map.tile_set_enabled("evil_after_"..next, true)
+    sol.map.enemy_set_enabled("evil_" .. next, true)
+    sol.map.tile_set_enabled("evil_after_" .. next, true)
     next = next + 1
     if next <= total then
       sol.main.timer_start(repeat_spawn, spawn_delay)
@@ -45,14 +56,14 @@ function start_evil_tiles()
     if remaining > 1 then
       again = true
     elseif remaining == 1 then
-      local sprite = sol.map.enemy_get_sprite("evil_"..total)
+      local sprite = sol.map.enemy_get_sprite("evil_" .. total)
       again = sol.main.sprite_get_animation(sprite) ~= "destroy"
     end
 
     if again then
       sol.main.timer_start(repeat_sound, 150)
     else 
-     finish_evil_tiles()
+      finish_evil_tiles()
     end
   end
 
@@ -62,49 +73,39 @@ end
 
 function event_hero_on_sensor(sensor_name)
 
-	if sensor_name=="sensor1" then
-		sol.map.door_close("door1")
-		sol.map.door_close("door2")
-		sol.map.sensor_set_enabled("sensor1",false)
-		sol.main.timer_start(start_evil_tiles, 2000)
-	end
+  if sensor_name == "sensor1"
+      and sol.map.door_is_open("door1_1") then
+    sol.map.door_close("door1")
+    sol.map.door_close("door2")
+    sol.map.sensor_set_enabled("sensor1", false)
+    sol.main.timer_start(start_evil_tiles, 2000)
+  end
 
-
-	if sensor_name=="sensor2" then
-		door3 = true
-	end
+  if sensor_name == "sensor2" then
+    door3 = true
+  end
 end
 
 function finish_evil_tiles()
 
-	sol.map.door_open("door1")
-	sol.map.door_open("door2")
-
+  sol.map.door_open("door1")
+  sol.map.door_open("door2")
 end
 
 function event_switch_activated(switch_name)
 
-	if switch_name=="switch1_1" then
-		if door3==false then
-			sol.map.switch_set_activated("switch2_1",true)
-			sol.map.door_open("door3")
-			sol.main.play_sound("door_open")
- 			sol.main.timer_start(function()
-				if door3==false then
-					sol.map.switch_set_activated("switch1_1",false)
-					sol.map.door_close("door3")
-					sol.main.play_sound("door_closed")
-					sol.map.switch_set_activated("switch2_1",false)
-				end	
-			end,5000)
-		end
-	end
-
-	if switch_name=="switch2_1" then
-		sol.map.door_open("door3")
-		sol.main.play_sound("door_open")
-		sol.map.switch_set_activated("switch1_1",true)
-	end
-
-
+  if switch_name == "switch1_1" then
+    if not door3 then
+      sol.map.door_open("door3")
+      sol.main.play_sound("door_open")
+      sol.main.timer_start(function()
+        if not door3 then
+          sol.map.switch_set_activated("switch1_1", false)
+          sol.map.door_close("door3")
+          sol.main.play_sound("door_closed")
+        end
+      end, 5000, true)
+    end
+  end
 end
+

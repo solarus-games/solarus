47dd89a5a914723c44bcd0a43be11c39ce65085d
Christopho <christopho@solarus-engine.org>
2011-11-28 00:25:36 +0100

    Finish the hidden palace

---
+++ quests/zsdx/data/maps/map0127.lua
@@ -0,0 +1,41 @@
+-- Hidden palace end
+
+function event_map_started(destination_point_name)
+
+  sol.map.light_set(0)
+end
+
+function event_chest_empty(chest_name)
+
+  if chest_name == "sword_chest" then
+    local variant
+    if sol.game.savegame_get_boolean(298) then
+      -- already got sword 2
+      variant = 3
+    else
+      variant = 2
+    end
+  end
+  sol.map.treasure_give("sword", variant, -1)
+end
+
+function event_treasure_obtaining(item_name, variant, savegame_variable)
+
+  if item_name == "sword" then
+    sol.main.play_music("excalibur.spc")
+  end
+end
+
+function event_treasure_obtained(item_name, variant, savegame_variable)
+
+  if item_name == "sword" then
+    sol.map.hero_start_victory_sequence()
+  end
+end
+
+function event_hero_victory_sequence_finished()
+
+  sol.map.hero_unfreeze()
+  sol.main.play_music("light_world_dungeon.spc")
+end
+

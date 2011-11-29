325f884f195ab82d690ba218f5fd48d7948e794a
Christopho <christopho@solarus-engine.org>
2011-11-29 03:09:37 +0100

    Dungeon 9: 3F and 4F finished

---
+++ quests/zsdx/data/maps/map0107.lua
@@ -0,0 +1,80 @@
+-- Dungeon 9 3F
+
+fighting_miniboss = false
+
+function event_map_started(destination_point_name)
+
+  -- miniboss
+  sol.map.door_set_open("miniboss_door", true)
+  sol.map.enemy_set_group_enabled("miniboss", false)
+end
+
+function event_enemy_dead(enemy_name)
+
+  -- door A
+  if enemy_name:match("^door_a_enemy") then
+    if not sol.map.door_is_open("door_a") then
+      sol.map.camera_move(2248, 648, function()
+	sol.main.play_sound("secret")
+	sol.map.door_open("door_a")
+      end)
+    end
+
+  -- miniboss
+  elseif string.find(enemy_name, "^miniboss")
+      and sol.map.enemy_is_group_dead("miniboss") then
+
+    sol.main.play_music("southern_shrine.ogg")
+    sol.main.play_sound("secret")
+    sol.map.door_open("miniboss_door")
+    sol.game.savegame_set_boolean(866, true)
+  end
+end
+
+function event_npc_interaction(npc_name)
+
+  -- door B hint stones
+  if npc_name:find("^door_b_hint") then
+    if not sol.map.door_is_open("door_b") then
+      local door_b_next = sol.game.savegame_get_integer(1202) + 1
+      local index = tonumber(npc_name:match("^door_b_hint_([1-8])$"))
+      if index == door_b_next then
+	-- correct
+	if index < 8 then
+	  local directions = { 0, 0, 0, 3, 2, 2, 2 }
+	  sol.map.dialog_start("dungeon_9.3f_door_b_hint_" .. directions[index])
+	  sol.game.savegame_set_integer(1202, index)
+	else
+	  sol.map.camera_move(1928, 1928, 500, function()
+	    sol.main.play_sound("secret")
+	    sol.map.door_open("door_b")
+	  end)
+	end
+      else
+	-- wrong
+	sol.main.play_sound("wrong")
+	sol.game.savegame_set_integer(1202, 0)
+      end
+    end
+  end
+end
+
+function event_hero_on_sensor(sensor_name)
+
+  -- miniboss
+  if sensor_name == "start_miniboss_sensor"
+      and not sol.game.savegame_get_boolean(866)
+      and not fighting_miniboss then
+
+    sol.map.hero_freeze()
+    sol.map.door_close("miniboss_door")
+    fighting_miniboss = true
+    sol.main.timer_start(function()
+      sol.main.play_music("boss.spc")
+      sol.map.enemy_set_group_enabled("miniboss", true)
+      sol.map.hero_unfreeze()
+    end,
+    1000)
+  end
+end
+
---
+++ quests/zsdx/data/maps/map0108.lua
@@ -0,0 +1,75 @@
+-- Dungeon 9 4F
+
+function event_map_started(destination_point_name)
+
+  -- chest A
+  if sol.game.savegame_get_boolean(872) then
+    sol.map.switch_set_activated("chest_a_switch", true)
+  else
+    sol.map.chest_set_enabled("chest_a", false)
+  end
+
+  -- chest C
+  if not sol.game.savegame_get_boolean(876) then
+    sol.map.chest_set_enabled("chest_c", false)
+  end
+end
+
+function event_switch_activated(switch_name)
+
+  -- open doors A and B (I love you, anonymous functions)
+  if switch_name == "door_ab_switch" then
+    sol.map.camera_move(1288, 680, 250, function()
+      -- open door A, wait 1 second and go to door B
+      sol.main.play_sound("secret")
+      sol.map.door_open("door_a")
+      sol.main.timer_start(function()
+	sol.map.camera_move(1608, 360, 250, function()
+	  -- open door B
+	  sol.main.play_sound("secret")
+	  sol.map.door_open("door_b")
+	end) -- camera to door B
+      end, 1000) -- timer before going from door A to door B
+    end, 1000, 5000) -- camera to door A (don't go back too soon)
+
+  -- chest A
+  elseif switch_name == "chest_a_switch" 
+      and not sol.map.chest_is_enabled("chest_a") then
+    sol.map.camera_move(1336, 520, 250, function()
+      sol.main.play_sound("chest_appears")
+      sol.map.chest_set_enabled("chest_a", true)
+    end)
+  end
+end
+
+function event_camera_back()
+
+  -- chest A
+  sol.main.timer_start(function()
+    if not sol.map.chest_is_open("chest_a") then
+      sol.main.play_sound("door_closed")
+      sol.map.chest_set_enabled("chest_a", false)
+      sol.map.switch_set_activated("chest_a_switch", false)
+    end
+  end, 5000, true)
+end
+
+function event_treasure_obtaining(item_name, variant, savegame_variable)
+
+  -- chest A
+  if savegame_variable == 872 then
+    sol.main.timer_stop_all()
+  end
+end
+
+function event_enemy_dead(enemy_name)
+
+  if enemy_name:find("^chest_c_enemy_") then
+    if sol.map.enemy_is_group_dead("chest_c_enemy")
+        and not sol.map.chest_is_enabled("chest_c") then
+      sol.main.play_sound("chest_appears")
+      sol.map.chest_set_enabled("chest_c", true)
+    end
+  end
+end
+

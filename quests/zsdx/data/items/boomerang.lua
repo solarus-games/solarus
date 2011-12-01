e5ccb3b4a725f4e31143b9ab5105ba2edcaec016
Christopho <christopho@solarus-engine.org>
2011-12-01 13:53:36 +0100

    Increase the distance of the blue boomerang.
    The Lyriann's cave was impossible to finish since this distance was
    reduced.

--- quests/zsdx/data/items/boomerang.lua
+++ quests/zsdx/data/items/boomerang.lua
@@ -1,7 +1,7 @@
 function event_use()
 
   if sol.item.get_variant() == 1 then
-    sol.map.hero_start_boomerang(96, 160, "boomerang1", "entities/boomerang1")
+    sol.map.hero_start_boomerang(128, 160, "boomerang1", "entities/boomerang1")
   else
     -- boomerang 2: longer and faster movement
     sol.map.hero_start_boomerang(192, 320, "boomerang2", "entities/boomerang2")

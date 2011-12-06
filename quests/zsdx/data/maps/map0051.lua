3141d41807906f236a8cd20c9557e85a84eb6f86
Christopho <christopho@solarus-engine.org>
2011-12-06 13:47:48 +0100

    Fix some issues in dungeon 8

--- quests/zsdx/data/maps/map0051.lua
+++ quests/zsdx/data/maps/map0051.lua
@@ -1,6 +1,4 @@
------------------------------
--- Dungeon 8 B4 script     --
------------------------------
+-- Dungeon 8 B4
 
 -- Legend
 -- CB: Chest Button
@@ -18,35 +16,35 @@ DB03_status = false
 DB04_status = false
 
 function event_map_started()
-	sol.map.light_set(0)
-	if not sol.game.savegame_get_boolean(725) then
-		sol.map.block_set_enabled("STT5", false)
-	end
+  sol.map.light_set(0)
+  if not sol.game.savegame_get_boolean(725) then
+    sol.map.block_set_enabled("STT5", false)
+    sol.map.tile_set_enabled("from_hole_tile", false)
+  end
 end
 
 function event_switch_activated(switch_name)
-	if switch_name == "DB03" then
-		DB03_status = true
-	elseif switch_name == "DB04" then
-		DB04_status = true
-	end
-	if DB03_status == true and DB04_status == true then
-		sol.main.play_sound("secret")		
-		if not sol.map.door_is_open("LD06") then
-			sol.map.door_open("LD06")
-		end
-	end
+  if switch_name == "DB03" then
+    DB03_status = true
+  elseif switch_name == "DB04" then
+    DB04_status = true
+  end
+  if DB03_status == true and DB04_status == true then
+    if not sol.map.door_is_open("LD06") then
+      sol.map.door_open("LD06")
+    end
+  end
 end
 
 function event_switch_inactivated(switch_name)
-	if switch_name == "DB03" then
-		DB03_status = false
-	elseif switch_name == "DB04" then
-		DB04_status = false
-	end
-	if DB03_status == false or DB04_status == false then
-		if sol.map.door_is_open("LD06") then		
-			sol.map.door_close("LD06")
-		end
-	end
+  if switch_name == "DB03" then
+    DB03_status = false
+  elseif switch_name == "DB04" then
+    DB04_status = false
+  end
+  if DB03_status == false or DB04_status == false then
+    if sol.map.door_is_open("LD06") then
+      sol.map.door_close("LD06")
+    end
+  end
 end

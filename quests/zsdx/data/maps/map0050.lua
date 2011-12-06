7e41c9cd6e0305cea81e8ed73994c504327f25de
Christopho <christopho@solarus-engine.org>
2011-12-06 13:47:48 +0100

    Fix some issues in dungeon 8

--- quests/zsdx/data/maps/map0050.lua
+++ quests/zsdx/data/maps/map0050.lua
@@ -1,6 +1,4 @@
------------------------------
--- Dungeon 8 B3 script     --
------------------------------
+-- Dungeon 8 B3
 
 -- Legend
 -- RC: Rupee Chest
@@ -14,57 +12,60 @@
 -- DS: Door Sensor
 
 function event_map_started(destination_point_name)
-	sol.map.door_set_open("LD12", true)
-	if sol.game.savegame_get_boolean(725) then
-		sol.map.block_set_enabled("STT5", false)
-	end
-	if sol.game.savegame_get_boolean(720) then
-		sol.map.switch_set_activated("DB1", true)
-	end
-	if sol.game.savegame_get_boolean(721) then
-		sol.map.switch_set_activated("DB2", true)
-	end
-	if sol.game.savegame_get_boolean(720) and sol.game.savegame_get_boolean(721) then
-		sol.map.door_set_open("LD14", true)
-	end
+  sol.map.door_set_open("LD12", true)
+  if sol.game.savegame_get_boolean(725) then
+    sol.map.block_set_enabled("STT5", false)
+  end
+  if sol.game.savegame_get_boolean(720) then
+    sol.map.switch_set_activated("DB1", true)
+  end
+  if sol.game.savegame_get_boolean(721) then
+    sol.map.switch_set_activated("DB2", true)
+  end
+  if sol.game.savegame_get_boolean(720) and sol.game.savegame_get_boolean(721) then
+    sol.map.door_set_open("LD14", true)
+  end
 end
 
 function event_hero_on_sensor(sensor_name)
-	if sensor_name == "DS12" then
-		sol.map.door_close("LD12")
-		sol.map.sensor_set_enabled("DS12", false)
-	end
+  if sensor_name == "DS12" then
+    sol.map.door_close("LD12")
+    sol.map.sensor_set_enabled("DS12", false)
+  end
 end
 
 function event_block_moved(block_name)
-	x, y = sol.map.block_get_position("STT5")
-	if x >= 1088 and x <= 1168 then
-		sol.map.block_set_enabled("STT5", false)
-		sol.main.play_sound("jump")
-		sol.game.savegame_set_boolean(725, true)
-	end
+  x, y = sol.map.block_get_position("STT5")
+  if x >= 1088 and x <= 1168 then
+    sol.map.block_set_enabled("STT5", false)
+    sol.main.play_sound("jump")
+    sol.game.savegame_set_boolean(725, true)
+    sol.main.timer_start(function()
+      sol.main.play_sound("bomb")
+    end, 500)
+  end
 end
 
 function event_switch_activated(switch_name)
-	if switch_name == "DB1" then
-		sol.game.savegame_set_boolean(720, true)
-		if sol.game.savegame_get_boolean(721) then
-			sol.map.door_open("LD14")
-			sol.main.play_sound("secret")
-		end
-	elseif switch_name == "DB2" then
-		sol.game.savegame_set_boolean(721, true)
-		if sol.game.savegame_get_boolean(720) then
-			sol.map.door_open("LD14")
-			sol.main.play_sound("secret")
-		end
-	elseif switch_name == "DB3" then
-		sol.map.door_open("LD12")
-		sol.map.door_open("LD13")
-		sol.main.play_sound("secret")
-	end
-	if DB1_status == true and DB2_status == true then
-		sol.map.door_open("LD14")
-		sol.main.play_sound("secret")
-	end
+  if switch_name == "DB1" then
+    sol.game.savegame_set_boolean(720, true)
+    if sol.game.savegame_get_boolean(721) then
+      sol.map.door_open("LD14")
+      sol.main.play_sound("secret")
+    end
+  elseif switch_name == "DB2" then
+    sol.game.savegame_set_boolean(721, true)
+    if sol.game.savegame_get_boolean(720) then
+      sol.map.door_open("LD14")
+      sol.main.play_sound("secret")
+    end
+  elseif switch_name == "DB3" then
+    sol.map.door_open("LD12")
+    sol.map.door_open("LD13")
+    sol.main.play_sound("secret")
+  end
+  if DB1_status == true and DB2_status == true then
+    sol.map.door_open("LD14")
+    sol.main.play_sound("secret")
+  end
 end

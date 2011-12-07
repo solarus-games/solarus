5de1a1e82d85be3c37292ebe9d6f113a4218e2b2
Christopho <christopho@solarus-engine.org>
2011-12-07 23:38:20 +0100

    Dungeon 8 details

--- quests/zsdx/data/maps/map0048.lua
+++ quests/zsdx/data/maps/map0048.lua
@@ -11,94 +11,102 @@
 -- BB: Barrier Button
 -- DS: Door Sensor
 
-LD7_all_enemies_dead = false
-
 function event_map_started(destination_point_name)
-	sol.map.door_set_open("LD9", true)
+  sol.map.door_set_open("LD9", true)
 
-	-- Link has mirror shield: no laser obstacles
-	if sol.game.get_ability("shield") >= 3 then
-		sol.map.obstacle_set_enabled("LO4", false)
-	end
+  -- Link has mirror shield: no laser obstacles
+  if sol.game.get_ability("shield") >= 3 then
+    sol.map.obstacle_set_enabled("LO4", false)
+  end
 
-	if not sol.game.savegame_get_boolean(706) then
-		sol.map.chest_set_enabled("KC03", false)
-	end
-	if not sol.game.savegame_get_boolean(707) then
-		sol.map.chest_set_enabled("KC04", false)
-	end
+  if not sol.game.savegame_get_boolean(706) then
+    sol.map.chest_set_enabled("KC03", false)
+  end
+  if not sol.game.savegame_get_boolean(707) then
+    sol.map.chest_set_enabled("KC04", false)
+  end
 
+  if destination_point_name == "from_1F_A" then
+    sol.map.door_set_open("LD8", true)
+  end
 end
 
 function event_hero_on_sensor(sensor_name)
-	if sensor_name == "DS12" then
-	-- Push block room		
-		sol.main.play_sound("secret")
-		sol.map.door_open("LD12")
-		sol.map.sensor_set_enabled("DS12", false)
-	elseif sensor_name == "DS7" then
-	-- Globules monsters room		
-		if sol.map.door_is_open("LD7") and not LD7_all_enemies_dead then		
-			sol.map.door_close("LD7")
-		end
-	elseif sensor_name == "DS9" then
-	-- Hard hat beetles room		
-		if sol.map.door_is_open("LD9") then
-			sol.map.door_close("LD9")
-			sol.map.sensor_set_enabled("DS9", false)
-		end
-	end
+  if sensor_name == "DS12" then
+    -- Push block room		
+    sol.main.play_sound("secret")
+    sol.map.door_open("LD12")
+    sol.map.sensor_set_enabled("DS12", false)
+  elseif sensor_name == "DS7" then
+    -- Globules monsters room		
+    if sol.map.door_is_open("LD7")
+        and not sol.map.enemy_is_group_dead("LD7_enemy") then		
+      sol.map.door_close("LD7")
+    end
+  elseif sensor_name == "DS9" then
+    -- Hard hat beetles room		
+    if sol.map.door_is_open("LD9")
+        and not sol.map.enemy_is_group_dead("LD9_enemy") then		
+      sol.map.door_close("LD9")
+      sol.map.sensor_set_enabled("DS9", false)
+    end
+  end
 end
 
 function event_switch_activated(switch_name)
-	if switch_name == "CB03" then
-		if not sol.game.savegame_get_boolean(706) then
-			sol.map.hero_freeze()
-			sol.main.timer_start(CB03_camera_move, 750)
-		end
-	elseif switch_name == "CB04" then
-		sol.map.chest_set_enabled("KC04", true)
-		sol.main.play_sound("chest_appears")
-	elseif string.match(switch_name, "^DB08") then
-		if not sol.map.door_is_open("LD8") then
-			sol.map.door_open("LD8")
-		end
-		if not sol.map.door_is_open("LD7") then
-			sol.map.door_open("LD7")
-		end
-	end
-end
-
-function CB03_camera_move()
-	sol.map.camera_move(1488, 1152, 250, CB03_chest_appears)
+  if switch_name == "CB03" then
+    if not sol.game.savegame_get_boolean(706) then
+      sol.map.camera_move(1488, 1152, 250, CB03_chest_appears)
+    end
+  elseif switch_name == "CB04" then
+    sol.map.chest_set_enabled("KC04", true)
+    sol.main.play_sound("chest_appears")
+  elseif string.match(switch_name, "^DB08") then
+    sol.main.play_sound("secret")
+    sol.map.door_open("LD8")
+    sol.map.door_open("LD7")
+  end
 end
 
 function CB03_chest_appears()
-	sol.map.chest_set_enabled("KC03", true)
-	sol.main.play_sound("chest_appears")
+  sol.map.chest_set_enabled("KC03", true)
+  sol.main.play_sound("chest_appears")
 end
 
 function CB03_time_out()
-	sol.map.chest_set_enabled("KC03", false)
-	sol.map.switch_set_activated("CB03", false)
+  if not sol.map.chest_is_open("KC03") then
+    sol.main.play_sound("door_closed")
+    sol.map.chest_set_enabled("KC03", false)
+    sol.map.switch_set_activated("CB03", false)
+  end
 end
 
 function event_camera_back()
-	sol.map.hero_unfreeze()
-	sol.main.timer_start(CB03_time_out, 10000, true)
+  sol.main.timer_start(CB03_time_out, 8000, true)
 end
 
+function event_treasure_obtaining(item_name, variant, savegame_variable)
+
+  if savegame_variable == 706 then
+    sol.main.timer_stop_all()
+  end
+end
+
+
 function event_enemy_dead(enemy_name)
-	if string.match(enemy_name, "^LD7_enemy") and sol.map.enemy_is_group_dead("LD7_enemy") then	
-	-- LD7 room: kill all enemies will open the door LD7
-		sol.map.door_open("LD7")
-		sol.main.play_sound("secret")
-		LD7_all_enemies_dead = true
-	elseif string.match(enemy_name, "^LD9_enemy") and sol.map.enemy_is_group_dead("LD9_enemy") then	
-	-- LD7 room: kill all enemies will open the door LD7
-		sol.map.door_open("LD9")
-		sol.main.play_sound("secret")
-	end
+  if string.match(enemy_name, "^LD7_enemy") and sol.map.enemy_is_group_dead("LD7_enemy") then	
+    -- LD7 room: kill all enemies will open the door LD7
+    if not sol.map.door_is_open("LD7") then
+      sol.map.door_open("LD7")
+      sol.main.play_sound("secret")
+    end
+  elseif string.match(enemy_name, "^LD9_enemy") and sol.map.enemy_is_group_dead("LD9_enemy") then	
+    -- LD9 room: kill all enemies will open the door LD9
+    if not sol.map.door_is_open("LD9") then
+      sol.map.door_open("LD9")
+      sol.main.play_sound("secret")
+    end
+  end
 end
 
 function event_door_open(door_name)
--- quests/zsdx/data/maps/map0049.lua
+++ quests/zsdx/data/maps/map0049.lua
@@ -1,6 +1,4 @@
------------------------------
--- Dungeon 8 B2 script     --
------------------------------
+-- Dungeon 8 B2
 
 -- Legend
 -- RC: Rupee Chest
@@ -14,139 +12,143 @@
 -- DS: Door Sensor
 
 function event_map_started(destination_point_name)
-	sol.map.door_set_open("LD11", true)
-	if not sol.game.savegame_get_boolean(711)	then
-		sol.map.chest_set_enabled("RC100", false)
-	end
-	if destination_point_name == "from_B3_C" then
-		sol.map.door_set_open("LD15", true)
-		for i = 1, 9 do
-			sol.map.switch_set_activated("BRoom" .. i, true)
-		end
-	end
+  sol.map.door_set_open("LD11", true)
+  if not sol.game.savegame_get_boolean(711) then
+    sol.map.chest_set_enabled("RC100", false)
+  end
+  if destination_point_name == "from_B3_C" then
+    sol.map.door_set_open("LD15", true)
+    for i = 1, 9 do
+      sol.map.switch_set_activated("BRoom" .. i, true)
+    end
+  end
 end
 
 function button_room_test_combination()
-	if sol.map.switch_is_activated("BRoom1")
-	and sol.map.switch_is_activated("BRoom2")
-	and sol.map.switch_is_activated("BRoom3")
-	and sol.map.switch_is_activated("BRoom4")
-	and sol.map.switch_is_activated("BRoom5")
-	and sol.map.switch_is_activated("BRoom6")
-	and sol.map.switch_is_activated("BRoom7")
-	and sol.map.switch_is_activated("BRoom8")
-	and sol.map.switch_is_activated("BRoom9") then
-		if not sol.map.door_is_open("LD15") then
-			sol.main.play_sound("secret")
-			sol.map.door_open("LD15")
-		end
-	end
+  if sol.map.switch_is_activated("BRoom1")
+    and sol.map.switch_is_activated("BRoom2")
+    and sol.map.switch_is_activated("BRoom3")
+    and sol.map.switch_is_activated("BRoom4")
+    and sol.map.switch_is_activated("BRoom5")
+    and sol.map.switch_is_activated("BRoom6")
+    and sol.map.switch_is_activated("BRoom7")
+    and sol.map.switch_is_activated("BRoom8")
+    and sol.map.switch_is_activated("BRoom9") then
+    if not sol.map.door_is_open("LD15") then
+      sol.main.play_sound("secret")
+      sol.map.door_open("LD15")
+      sol.map.switch_set_activated("BRoomReset", true)
+    end
+  end
 end
 
 function event_switch_activated(switch_name)
-	if switch_name == "BRoom1" then
-		sol.map.switch_set_activated("BRoom2", not sol.map.switch_is_activated("BRoom2"))
-		sol.map.switch_set_activated("BRoom4", not sol.map.switch_is_activated("BRoom4"))
-		sol.map.switch_set_activated("BRoomReset", false)
-		button_room_test_combination()
-	elseif switch_name == "BRoom2" then
-		sol.map.switch_set_activated("BRoom1", not sol.map.switch_is_activated("BRoom1"))
-		sol.map.switch_set_activated("BRoom3", not sol.map.switch_is_activated("BRoom3"))
-		sol.map.switch_set_activated("BRoom5", not sol.map.switch_is_activated("BRoom5"))
-		sol.map.switch_set_activated("BRoomReset", false)
-		button_room_test_combination()
-	elseif switch_name == "BRoom3" then
-		sol.map.switch_set_activated("BRoom2", not sol.map.switch_is_activated("BRoom2"))
-		sol.map.switch_set_activated("BRoom6", not sol.map.switch_is_activated("BRoom6"))
-		sol.map.switch_set_activated("BRoomReset", false)
-		button_room_test_combination()
-	elseif switch_name == "BRoom4" then
-		sol.map.switch_set_activated("BRoom1", not sol.map.switch_is_activated("BRoom1"))
-		sol.map.switch_set_activated("BRoom5", not sol.map.switch_is_activated("BRoom5"))
-		sol.map.switch_set_activated("BRoom7", not sol.map.switch_is_activated("BRoom7"))
-		sol.map.switch_set_activated("BRoomReset", false)
-		button_room_test_combination()
-	elseif switch_name == "BRoom5" then
-		sol.map.switch_set_activated("BRoom2", not sol.map.switch_is_activated("BRoom2"))
-		sol.map.switch_set_activated("BRoom4", not sol.map.switch_is_activated("BRoom4"))
-		sol.map.switch_set_activated("BRoom6", not sol.map.switch_is_activated("BRoom6"))
-		sol.map.switch_set_activated("BRoom8", not sol.map.switch_is_activated("BRoom8"))
-		sol.map.switch_set_activated("BRoomReset", false)
-		button_room_test_combination()
-	elseif switch_name == "BRoom6" then
-		sol.map.switch_set_activated("BRoom3", not sol.map.switch_is_activated("BRoom3"))
-		sol.map.switch_set_activated("BRoom5", not sol.map.switch_is_activated("BRoom5"))
-		sol.map.switch_set_activated("BRoom9", not sol.map.switch_is_activated("BRoom9"))
-		sol.map.switch_set_activated("BRoomReset", false)
-		button_room_test_combination()
-	elseif switch_name == "BRoom7" then
-		sol.map.switch_set_activated("BRoom4", not sol.map.switch_is_activated("BRoom4"))
-		sol.map.switch_set_activated("BRoom8", not sol.map.switch_is_activated("BRoom8"))
-		sol.map.switch_set_activated("BRoomReset", false)
-		button_room_test_combination()
-	elseif switch_name == "BRoom8" then
-		sol.map.switch_set_activated("BRoom5", not sol.map.switch_is_activated("BRoom5"))
-		sol.map.switch_set_activated("BRoom7", not sol.map.switch_is_activated("BRoom7"))
-		sol.map.switch_set_activated("BRoom9", not sol.map.switch_is_activated("BRoom9"))
-		sol.map.switch_set_activated("BRoomReset", false)
-		button_room_test_combination()
-	elseif switch_name == "BRoom9" then
-		sol.map.switch_set_activated("BRoom6", not sol.map.switch_is_activated("BRoom6"))
-		sol.map.switch_set_activated("BRoom8", not sol.map.switch_is_activated("BRoom8"))
-		sol.map.switch_set_activated("BRoomReset", false)
-		button_room_test_combination()
-	elseif switch_name == "BRoomReset" then
-		sol.map.switch_set_activated("BRoom1", false)
-		sol.map.switch_set_activated("BRoom2", false)
-		sol.map.switch_set_activated("BRoom3", false)
-		sol.map.switch_set_activated("BRoom4", false)
-		sol.map.switch_set_activated("BRoom5", false)
-		sol.map.switch_set_activated("BRoom6", false)
-		sol.map.switch_set_activated("BRoom7", false)
-		sol.map.switch_set_activated("BRoom8", false)
-		sol.map.switch_set_activated("BRoom9", false)
-	elseif switch_name == "DB16" then
-		sol.map.hero_freeze()
-		sol.main.timer_start(DB16_camera_move, 750)
-	end
-end
-
-function DB16_camera_move()
-	sol.map.camera_move(808, 1056, 250, DB16_open_door)
+  if switch_name == "BRoom1" then
+    sol.map.switch_set_activated("BRoom2", not sol.map.switch_is_activated("BRoom2"))
+    sol.map.switch_set_activated("BRoom4", not sol.map.switch_is_activated("BRoom4"))
+    sol.map.switch_set_activated("BRoomReset", false)
+    button_room_test_combination()
+  elseif switch_name == "BRoom2" then
+    sol.map.switch_set_activated("BRoom1", not sol.map.switch_is_activated("BRoom1"))
+    sol.map.switch_set_activated("BRoom3", not sol.map.switch_is_activated("BRoom3"))
+    sol.map.switch_set_activated("BRoom5", not sol.map.switch_is_activated("BRoom5"))
+    sol.map.switch_set_activated("BRoomReset", false)
+    button_room_test_combination()
+  elseif switch_name == "BRoom3" then
+    sol.map.switch_set_activated("BRoom2", not sol.map.switch_is_activated("BRoom2"))
+    sol.map.switch_set_activated("BRoom6", not sol.map.switch_is_activated("BRoom6"))
+    sol.map.switch_set_activated("BRoomReset", false)
+    button_room_test_combination()
+  elseif switch_name == "BRoom4" then
+    sol.map.switch_set_activated("BRoom1", not sol.map.switch_is_activated("BRoom1"))
+    sol.map.switch_set_activated("BRoom5", not sol.map.switch_is_activated("BRoom5"))
+    sol.map.switch_set_activated("BRoom7", not sol.map.switch_is_activated("BRoom7"))
+    sol.map.switch_set_activated("BRoomReset", false)
+    button_room_test_combination()
+  elseif switch_name == "BRoom5" then
+    sol.map.switch_set_activated("BRoom2", not sol.map.switch_is_activated("BRoom2"))
+    sol.map.switch_set_activated("BRoom4", not sol.map.switch_is_activated("BRoom4"))
+    sol.map.switch_set_activated("BRoom6", not sol.map.switch_is_activated("BRoom6"))
+    sol.map.switch_set_activated("BRoom8", not sol.map.switch_is_activated("BRoom8"))
+    sol.map.switch_set_activated("BRoomReset", false)
+    button_room_test_combination()
+  elseif switch_name == "BRoom6" then
+    sol.map.switch_set_activated("BRoom3", not sol.map.switch_is_activated("BRoom3"))
+    sol.map.switch_set_activated("BRoom5", not sol.map.switch_is_activated("BRoom5"))
+    sol.map.switch_set_activated("BRoom9", not sol.map.switch_is_activated("BRoom9"))
+    sol.map.switch_set_activated("BRoomReset", false)
+    button_room_test_combination()
+  elseif switch_name == "BRoom7" then
+    sol.map.switch_set_activated("BRoom4", not sol.map.switch_is_activated("BRoom4"))
+    sol.map.switch_set_activated("BRoom8", not sol.map.switch_is_activated("BRoom8"))
+    sol.map.switch_set_activated("BRoomReset", false)
+    button_room_test_combination()
+  elseif switch_name == "BRoom8" then
+    sol.map.switch_set_activated("BRoom5", not sol.map.switch_is_activated("BRoom5"))
+    sol.map.switch_set_activated("BRoom7", not sol.map.switch_is_activated("BRoom7"))
+    sol.map.switch_set_activated("BRoom9", not sol.map.switch_is_activated("BRoom9"))
+    sol.map.switch_set_activated("BRoomReset", false)
+    button_room_test_combination()
+  elseif switch_name == "BRoom9" then
+    sol.map.switch_set_activated("BRoom6", not sol.map.switch_is_activated("BRoom6"))
+    sol.map.switch_set_activated("BRoom8", not sol.map.switch_is_activated("BRoom8"))
+    sol.map.switch_set_activated("BRoomReset", false)
+    button_room_test_combination()
+  elseif switch_name == "BRoomReset" then
+    sol.map.switch_set_activated("BRoom1", false)
+    sol.map.switch_set_activated("BRoom2", false)
+    sol.map.switch_set_activated("BRoom3", false)
+    sol.map.switch_set_activated("BRoom4", false)
+    sol.map.switch_set_activated("BRoom5", false)
+    sol.map.switch_set_activated("BRoom6", false)
+    sol.map.switch_set_activated("BRoom7", false)
+    sol.map.switch_set_activated("BRoom8", false)
+    sol.map.switch_set_activated("BRoom9", false)
+  elseif switch_name == "DB16" then
+    sol.map.camera_move(808, 1056, 250, DB16_open_door)
+  end
 end
 
 function DB16_open_door()
-	sol.map.door_open("LD16")
+  sol.map.door_open("LD16")
 end
 
 function DB16_time_out()
-	sol.map.door_close("LD16")
-	sol.map.switch_set_activated("DB16", false)
+  sol.map.door_close("LD16")
+  sol.map.switch_set_activated("DB16", false)
 end
 
 function event_camera_back()
-	sol.map.hero_unfreeze()
-	sol.main.timer_start(DB16_time_out, 10000, true)
+  sol.main.timer_start(DB16_time_out, 10000, true)
 end
 
 function event_npc_interaction(npc_name)
-	if npc_name == "TD01" then
-		sol.main.play_sound("warp")
-		hero_x, hero_y = sol.map.hero_get_position()
-		sol.map.hero_set_position(hero_x - 320, hero_y)
-	elseif npc_name == "TD02" then
-		sol.main.play_sound("warp")
-		hero_x, hero_y = sol.map.hero_get_position()
-		sol.map.hero_set_position(hero_x + 320, hero_y)
-	end
+  if npc_name == "TD01" then
+    sol.main.play_sound("warp")
+    hero_x, hero_y = sol.map.hero_get_position()
+    sol.map.hero_set_position(hero_x - 320, hero_y)
+  elseif npc_name == "TD02" then
+    sol.main.play_sound("warp")
+    hero_x, hero_y = sol.map.hero_get_position()
+    sol.map.hero_set_position(hero_x + 320, hero_y)
+  end
 end
 
 function event_enemy_dead(enemy_name)
-	if string.match(enemy_name, "^rupee_enemy") and sol.map.enemy_is_group_dead("rupee_enemy") then	
-	-- 100 rupee room: kill all enemies
-		if not sol.map.door_is_open("LD15") then
-			sol.main.play_sound("chest_appears")
-			sol.map.chest_set_enabled("RC100", true)
-		end
-	end
+  if string.find(enemy_name, "^rupee_enemy") and sol.map.enemy_is_group_dead("rupee_enemy") then	
+    -- 100 rupee room: kill all enemies
+    if not sol.map.chest_is_enabled("RC100") then
+      sol.main.play_sound("chest_appears")
+      sol.map.chest_set_enabled("RC100", true)
+    end
+  end
 end
+
+function event_hero_on_sensor(sensor_name)
+
+  if sensor_name == "dont_close_LD16_sensor" then
+    sol.main.timer_stop_all()
+    sol.map.sensor_set_enabled(sensor_name, false)
+  end
+end
+

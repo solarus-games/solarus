solarus-games/solarus

commit bc4c98c4e256865a29efd09b13b408f6f4a1a80c

Trigger conveyor belts only if the hero can completely leave them

quests/zsdx/data/maps/map0046.dat
@@ -29,9 +29,20 @@
 0	1	184	40	48	48	407
 0	1	208	168	8	24	71
 0	1	176	168	8	24	71
+0	1	136	136	16	16	607
+0	1	56	120	8	16	69
+0	1	64	112	16	8	69
+0	1	80	120	32	8	69
 1	1	160	213	destination_point	1	0
 2	1	152	224	16	16	teletransporter	0	1	8	from_west_mountain_cave_1f_w
 2	1	32	0	16	16	teletransporter_16	0	1	46	destination_point
+17	1	120	149	0
+17	1	144	173	2
+17	1	168	149	4
+17	1	144	125	6
+17	1	48	149	0
+9	1	64	149	block	-1	entities/block	1	1	2
+7	1	104	149	enemy	3	blue_flame	0	-1	_random	1	-1
 0	2	112	216	96	8	357
 0	2	144	216	32	8	188
 0	2	0	0	320	16	170

src/entities/Hero.cpp
@@ -1470,9 +1470,16 @@ void Hero::notify_collision_with_conveyor_belt(ConveyorBelt &conveyor_belt, int 
 
     if (conveyor_belt.overlaps(center)) {
 
-      // check that the hero will be able to get out
-      Rectangle collision_box = conveyor_belt.get_bounding_box();
-      collision_box.add_xy(dx, dy);
+      // check that the hero will be able to completely get out
+      Rectangle collision_box(0, 0, 16, 16);
+      if (dx != 0) { // horizontal conveyor belt
+        collision_box.set_xy(get_top_left_x() + dx,
+            conveyor_belt.get_top_left_y() + dy);
+      }
+      else { // vertical conveyor belt
+        collision_box.set_xy(conveyor_belt.get_top_left_x() + dx,
+            get_top_left_y() + dy);
+      }
 
       if (!get_map().test_collision_with_obstacles(get_layer(), collision_box, *this)) {
 

commit 003b05fca63422dccda150adef27dcc0177b8d69

The hero could not take blocked conveyor belts the reverse way.
This bug was introduced in commit bc4c98c4.

quests/zsdx/data/maps/map0046.dat
@@ -26,6 +26,10 @@
 1	1	160	213	destination_point	1	0
 2	1	152	224	16	16	teletransporter	0	1	8	from_west_mountain_cave_1f_w
 2	1	32	0	16	16	teletransporter_16	0	1	46	destination_point
+17	1	128	133	0
+17	1	128	149	0
+9	1	144	133	block	-1	entities/block	1	0	1
+9	1	152	149	block_2	-1	entities/block	1	0	1
 0	2	112	216	96	8	357
 0	2	144	216	32	8	188
 0	2	0	0	320	16	170

src/entities/Hero.cpp
@@ -1470,21 +1470,31 @@ void Hero::notify_collision_with_conveyor_belt(ConveyorBelt &conveyor_belt, int 
 
     if (conveyor_belt.overlaps(center)) {
 
-      // check that the hero will be able to completely get out
+      // check that the hero can go in the conveyor belt's direction
+      // (otherwise the hero would be trapped forever if there
+      // is an obstacle 8 pixels after the conveyor belt)
       Rectangle collision_box(0, 0, 16, 16);
       if (dx != 0) { // horizontal conveyor belt
         collision_box.set_xy(get_top_left_x() + dx,
-            conveyor_belt.get_top_left_y() + dy);
+            conveyor_belt.get_top_left_y());
       }
       else { // vertical conveyor belt
-        collision_box.set_xy(conveyor_belt.get_top_left_x() + dx,
+        collision_box.set_xy(conveyor_belt.get_top_left_x(),
             get_top_left_y() + dy);
       }
 
       if (!get_map().test_collision_with_obstacles(get_layer(), collision_box, *this)) {
 
-        // move the hero
-        set_state(new ConveyorBeltState(*this, conveyor_belt));
+        // check that the conveyor belt's exit is clear
+        // (otherwise the hero could not take a blocked conveyor belt the reverse way)
+        collision_box.set_xy(conveyor_belt.get_bounding_box());
+        collision_box.add_xy(dx, dy);
+
+        if (!get_map().test_collision_with_obstacles(get_layer(), collision_box, *this)) {
+
+          // move the hero
+          set_state(new ConveyorBeltState(*this, conveyor_belt));
+        }
       }
     }
   }
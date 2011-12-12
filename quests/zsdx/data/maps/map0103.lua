ef3cbdfda01df9ae26167a4c343aa376216358b7
Christopho <christopho@solarus-engine.org>
2011-12-12 11:28:46 +0100

    Dungeon 7: the hero could be stucked after the special torch

--- quests/zsdx/data/maps/map0103.lua
+++ quests/zsdx/data/maps/map0103.lua
@@ -2,7 +2,6 @@
 
 fighting_boss = false
 just_removed_special_torch = false
-special_torch_allow_close = true
 
 doors = { -- properties of the 5 timed doors
   door_a = { x = 920, y = 640, delay = 12000 },
@@ -33,6 +32,11 @@ function event_map_started(destination_point_name)
 
   -- boss door
   sol.map.door_set_open("boss_door", true)
+
+  -- special torch door
+  if sol.game.savegame_get_boolean(624) then
+    sol.map.switch_set_activated("ne_switch", true)
+  end
 end
 
 function event_block_moved(block_name)
@@ -110,11 +114,9 @@ function event_camera_back()
   if just_removed_special_torch then
     just_removed_special_torch = false
     sol.main.timer_start(function()
+      sol.main.play_sound("door_closed")
+      sol.map.tile_set_enabled("special_torch", true)
       sol.map.switch_set_activated("special_torch_switch", false)
-      if special_torch_allow_close then
-        sol.main.play_sound("door_closed")
-        sol.map.tile_set_enabled("special_torch", true)
-      end
     end, 8000, true)
 
   elseif current_door_name ~= nil then
@@ -136,14 +138,7 @@ function event_hero_on_sensor(sensor_name)
 
   -- special torch
   if sensor_name == "special_torch_dont_close_sensor" then
-    special_torch_allow_close = false
-  elseif sensor_name == "special_torch_close_sensor" then
-    special_torch_allow_close = true
-    if not sol.map.switch_is_activated("special_torch_switch") then
-      -- the timer has expired in the meantime
-      sol.main.play_sound("door_closed")
-      sol.map.tile_set_enabled("special_torch", true)
-    end
+    sol.main.timer_stop_all()
 
   -- boss door
   elseif sensor_name == "close_boss_door_sensor"

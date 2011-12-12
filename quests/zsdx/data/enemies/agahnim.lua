1ebda628d84265d6623f1aae85251ba9811c6e14
Christopho <christopho@solarus-engine.org>
2011-12-12 18:40:18 +0100

    Improve the boss of dungeon 5

--- quests/zsdx/data/enemies/agahnim.lua
+++ quests/zsdx/data/enemies/agahnim.lua
@@ -1,26 +1,28 @@
 -- Agahnim (Boss of dungeon 5)
 
 -- possible positions where he appears
-positions = {
+local positions = {
   {x = 192, y = 277, direction4 = 3},
   {x = 400, y = 277, direction4 = 3},
   {x = 296, y = 373, direction4 = 1}
 }
 
-nb_sons_created = 0
-initial_life = 8
-finished = false
+local nb_sons_created = 0
+local initial_life = 10
+local finished = false
+local blue_fireball_proba = 33 -- percent
 
 function event_appear()
 
   sol.enemy.set_life(initial_life)
-  sol.enemy.set_damage(2)
+  sol.enemy.set_damage(12)
   sol.enemy.create_sprite("enemies/agahnim")
   sol.enemy.set_size(16, 16)
   sol.enemy.set_origin(8, 13)
   sol.enemy.set_invincible()
   sol.enemy.set_attack_consequence("sword", "protected")
   sol.enemy.set_pushed_back_when_hurt(false)
+  sol.enemy.set_push_hero_on_sword(true)
 
   local sprite = sol.enemy.get_sprite()
   sol.main.sprite_set_animation(sprite, "stopped")
@@ -34,7 +36,7 @@ function event_restart()
 
   if not finished then
     sol.main.sprite_fade(sprite, 1)
-    sol.main.timer_start(hide, 500)
+    sol.main.timer_start(hide, 700)
   else
     sol.main.sprite_set_animation(sprite, "hurt")
     sol.map.hero_freeze()
@@ -71,7 +73,11 @@ end
 function fire_step_2()
 
   local sprite = sol.enemy.get_sprite()
-  sol.main.sprite_set_animation(sprite, "preparing_fire")
+  if math.random(100) <= blue_fireball_proba then
+    sol.main.sprite_set_animation(sprite, "preparing_blue_fireball")
+  else
+    sol.main.sprite_set_animation(sprite, "preparing_red_fireball")
+  end
   sol.main.play_sound("boss_charge")
   sol.main.timer_start(fire_step_3, 1500)
 end
@@ -79,23 +85,33 @@ end
 function fire_step_3()
 
   local sprite = sol.enemy.get_sprite()
+
+  local sound, breed
+  if sol.main.sprite_get_animation(sprite) == "preparing_blue_fireball" then
+    sound = "cane"
+    breed = "blue_fireball_triple"
+  else
+    sound = "boss_fireball"
+    breed = "red_fireball_triple"
+  end
   sol.main.sprite_set_animation(sprite, "stopped")
-  sol.main.play_sound("boss_fireball")
+  sol.main.play_sound(sound)
+
+  vulnerable = true
+  sol.main.timer_start(sol.enemy.restart, 700)
+
+  function throw_fire()
+
+    nb_sons_created = nb_sons_created + 1
+    sol.enemy.create_son("agahnim_fireball_" .. nb_sons_created,
+    breed, 0, -21)
+  end
 
   throw_fire()
   if sol.enemy.get_life() <= initial_life / 2 then
     sol.main.timer_start(throw_fire, 200)
     sol.main.timer_start(throw_fire, 400)
   end
-
-  vulnerable = true
-  sol.main.timer_start(sol.enemy.restart, 700)
-end
-
-function throw_fire()
-
-  nb_sons_created = nb_sons_created + 1
-  sol.enemy.create_son("agahnim_fireball_"..nb_sons_created, "fireball_triple", 0, -21)
 end
 
 function event_message_received(src_enemy, message)
@@ -111,11 +127,14 @@ end
 
 function event_hurt(attack, life_lost)
 
-  if sol.enemy.get_life() <= 0 then
+  local life = sol.enemy.get_life()
+  if life <= 0 then
 
     sol.map.enemy_remove_group("agahnim_fireball")
     sol.enemy.set_life(1)
     finished = true
+  elseif life <= initial_life / 3 then
+    blue_fireball_proba = 50
   end
 end
 

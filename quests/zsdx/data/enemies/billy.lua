c99b318034f36bcfb2e73466a24a5df6115b81f5
Christopho <christopho@solarus-engine.org>
2011-12-13 12:10:51 +0100

    Dungeon 8: make the first boss more difficult

--- quests/zsdx/data/enemies/billy.lua
+++ quests/zsdx/data/enemies/billy.lua
@@ -4,9 +4,10 @@ sol.main.include("enemies/generic_towards_hero")
 set_properties({
   sprite = "enemies/billy",
   life = 30,
-  damage = 8,
+  damage = 12,
   normal_speed = 48,
   faster_speed = 72,
-  hurt_style = "boss"
+  hurt_style = "boss",
+  push_hero_on_sword = true
 })
 

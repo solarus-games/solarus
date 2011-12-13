b2692404313cf504efb1197362a969f18a15fd12
Christopho <christopho@solarus-engine.org>
2011-12-13 05:06:05 +0100

    Update dialogs of the telepathic booth

--- quests/zsdx/data/maps/map0032.lua
+++ quests/zsdx/data/maps/map0032.lua
@@ -4,8 +4,14 @@ function event_npc_interaction(npc_name)
 
   if npc_name == "hint_stone" then
 
-    if not sol.game.is_dungeon_finished(2) then
+    if not sol.game.is_dungeon_finished(1) then
       sol.map.dialog_start("telepathic_booth.not_working")
+    elseif not sol.game.has_item("bow") then
+      sol.map.dialog_start("telepathic_booth.go_sahasrahla")
+      sol.map.dialog_set_variable("telepathic_booth.go_sahasrahla", sol.game.savegame_get_name())
+    elseif not sol.game.is_dungeon_finished(2) then
+      sol.map.dialog_start("telepathic_booth.go_twin_caves")
+      sol.map.dialog_set_variable("telepathic_booth.go_twin_caves", sol.game.savegame_get_name())
     elseif sol.game.get_item("rupee_bag") < 2 then
       sol.map.dialog_start("telepathic_booth.dungeon_2_not_really_finished")
       sol.map.dialog_set_variable("telepathic_booth.dungeon_2_not_really_finished", sol.game.savegame_get_name())
@@ -15,9 +21,6 @@ function event_npc_interaction(npc_name)
     elseif not sol.game.is_dungeon_finished(4) then
       sol.map.dialog_start("telepathic_booth.go_billy")
       sol.map.dialog_set_variable("telepathic_booth.go_billy", sol.game.savegame_get_name())
-    elseif not sol.game.is_dungeon_finished(5) then
-      sol.map.dialog_start("telepathic_booth.hahaha")
-      sol.map.dialog_set_variable("telepathic_booth.hahaha", sol.game.savegame_get_name())
     else
       sol.map.dialog_start("telepathic_booth.shop")
       sol.map.dialog_set_variable("telepathic_booth.shop", sol.game.savegame_get_name())

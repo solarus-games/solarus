4b60738f87b0a72005c45885298d5527ccb7f55b
Christopho <christopho@solarus-engine.org>
2011-12-14 13:30:22 +0100

    Working on the final boss

--- quests/zsdx/data/maps/map0130.lua
+++ quests/zsdx/data/maps/map0130.lua
@@ -1,5 +1,10 @@
 -- Dungeon 9 boss
 
+local torches_error = false
+local torches_next = nil
+local torches_nb_on = 0
+local torches_delay = 15000
+
 function event_map_started(destination_point_name)
 
   if not sol.game.savegame_get_boolean(881) then
@@ -8,6 +13,8 @@ function event_map_started(destination_point_name)
     sol.map.npc_set_enabled("zelda", false)
     sol.map.npc_set_group_enabled("child", false)
   end
+
+  sol.map.switch_set_group_enabled("switch", false)
 end
 
 function event_map_opening_transition_finished(destination_point_name)
@@ -83,5 +90,100 @@ function start_zelda_sequence()
     sol.map.dialog_start("dungeon_9.zelda")
     sol.map.dialog_set_variable("dungeon_9.zelda", sol.game.savegame_get_name())
   end, 3000)
+end
+
+-- Torches on this map interact with the map script
+-- because we don't want usual behavior from items/lamp.lua:
+-- we want a longer delay and special Ganon interaction 
+function event_npc_interaction(npc_name)
+
+  if string.find(npc_name, "^torch") then
+    sol.map.dialog_start("torch.need_lamp")
+  end
+end
+
+-- Called when fire touches an NPC linked to this map
+function event_npc_collision_fire(npc_name)
+
+  if string.find(npc_name, "^torch") then
+    
+    local torch_sprite = sol.map.npc_get_sprite(npc_name)
+    if sol.main.sprite_get_animation(torch_sprite) == "unlit" then
+      -- temporarily light the torch up
+      sol.main.sprite_set_animation(torch_sprite, "lit")
+      check_torches()
+      sol.main.timer_start(function()
+        sol.main.sprite_set_animation(torch_sprite, "unlit")
+        check_torches()
+      end, torches_delay)
+    end
+  end
+end
+
+function check_torches()
+  
+  local states = {
+    sol.main.sprite_get_animation(sol.map.npc_get_sprite("torch_1")) == "lit",
+    sol.main.sprite_get_animation(sol.map.npc_get_sprite("torch_2")) == "lit",
+    sol.main.sprite_get_animation(sol.map.npc_get_sprite("torch_3")) == "lit",
+    sol.main.sprite_get_animation(sol.map.npc_get_sprite("torch_4")) == "lit"
+  }
+  local on = {}
+
+  for i = 1, #states do
+    if states[i] then
+      on[#on + 1] = i
+    end
+  end
+
+  if #on == torches_nb_on then
+    -- no change
+    return
+  end
+
+  --print("torches on:", #on)
+
+  if #on == #states then
+   -- all torches are on
+    if torches_error then
+      -- TODO
+      sol.main.play_sound("wrong")
+      --print("wrong")
+    else
+      -- TODO
+      sol.main.play_sound("secret")
+      --print("correct")
+      torches_next = on[1] % #states + 1
+    end
+
+  elseif #on == 0 then
+    -- no torch is on
+    torches_error = false
+    torches_next = nil
+    --print("no torch is on")
+
+  elseif #on == 1 then
+    --print("a first torch is on: ", on[1])
+    torches_error = false
+    torches_next = on[1] % #states + 1
+    --print("next should be ", torches_next)
+      
+  elseif not torches_error then
+
+    if #on == torches_nb_on + 1 then
+      -- a torch was just turned on
+      if states[torches_next] then
+        -- it's the correct one
+        torches_next = torches_next % #states + 1
+        --print("another torch is on, it's the correct one")
+	--print("next should be ", torches_next)
+      else
+	torches_error = true
+        --print("another torch is on, it's a wrong one")
+      end
+    end
+  end
+
+  torches_nb_on = #on
 end
 

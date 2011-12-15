solarus-games/solarus

commit 0e6000f41416c4d93fad0b96ede8840e368c22e5

Set the map of sprites created by the map script

src/entities/MapEntity.cpp
@@ -288,6 +288,7 @@ bool MapEntity::is_on_map() {
 void MapEntity::set_map(Map &map) {
 
   this->map = &map;
+  set_sprites_map(map);
 }
 
 /**
@@ -295,12 +296,21 @@ void MapEntity::set_map(Map &map) {
  */
 void MapEntity::notify_map_started() {
 
-  // notify the sprites (useful for tileset-dependent sprites such as doors and blocks)
+  set_sprites_map(*map);
+}
+
+/**
+ * @brief Notifies the sprites of this entity that they belong to a map.
+ *
+ * This is useful for tileset-dependent sprites such as doors and blocks.
+ */
+void MapEntity::set_sprites_map(Map& map) {
+
   std::list<Sprite*>::iterator it;
   for (it = sprites.begin(); it != sprites.end(); it++) {
 
     Sprite& sprite = *(*it);
-    sprite.set_map(*map);
+    sprite.set_map(map);
   }
 }
 

include/entities/MapEntity.h
@@ -120,6 +120,10 @@ class MapEntity {
                                                  * (if not, it will not be displayed and collisions will not be notified) */
     bool waiting_enabled;                       /**< indicates that the entity will be enabled as soon as the hero stops overlapping it */
 
+  private:
+
+    void set_sprites_map(Map& map);
+
   protected:
 
     bool suspended;                             /**< indicates that the animation and movement of this entity are suspended */

src/SpriteAnimation.cpp
@@ -148,7 +148,9 @@ int SpriteAnimation::get_next_frame(int current_direction, int current_frame) co
 void SpriteAnimation::display(Surface *destination, int x, int y,
     int current_direction, int current_frame) {
 
-  directions[current_direction]->display(destination, x, y, current_frame, src_image);
+  if (src_image != NULL) {
+    directions[current_direction]->display(destination, x, y, current_frame, src_image);
+  }
 }
 
 /**

commit efade0fc1114896e067aad320de477e375a9c6ad

Don't change tileset-dependent sprites before the new map is active
Otherwise, if the tileset changes, the old map also reflect these
changes.
This bug was introduced by commit 0e6000f4.

src/entities/MapEntity.cpp
@@ -288,7 +288,9 @@ bool MapEntity::is_on_map() {
 void MapEntity::set_map(Map &map) {
 
   this->map = &map;
-  set_sprites_map(map);
+  if (&get_game().get_current_map() == &map) {
+    set_sprites_map(map);
+  }
 }
 
 /**
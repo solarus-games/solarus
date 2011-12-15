solarus-games/solarus

commit dbdc68e3f3cc1aace41d11d079615442cd9fdf28

Fix throwing the carried item when the state changes

src/hero/JumpingState.cpp
@@ -92,6 +92,8 @@ void Hero::JumpingState::stop(State *next_state) {
 
   if (carried_item != NULL) {
 
+    get_sprites().set_lifted_item(NULL);
+
     switch (next_state->get_previous_carried_item_behavior(*carried_item)) {
 
     case CarriedItem::BEHAVIOR_THROW:
@@ -103,7 +105,6 @@ void Hero::JumpingState::stop(State *next_state) {
     case CarriedItem::BEHAVIOR_DESTROY:
       delete carried_item;
       carried_item = NULL;
-      get_sprites().set_lifted_item(NULL);
       break;
 
     case CarriedItem::BEHAVIOR_KEEP:

src/hero/StairsState.cpp
@@ -120,6 +120,8 @@ void Hero::StairsState::stop(State* next_state) {
 
   if (carried_item != NULL) {
 
+    get_sprites().set_lifted_item(NULL);
+
     switch (next_state->get_previous_carried_item_behavior(*carried_item)) {
 
     case CarriedItem::BEHAVIOR_THROW:
@@ -131,7 +133,6 @@ void Hero::StairsState::stop(State* next_state) {
     case CarriedItem::BEHAVIOR_DESTROY:
       delete carried_item;
       carried_item = NULL;
-      get_sprites().set_lifted_item(NULL);
       break;
 
     case CarriedItem::BEHAVIOR_KEEP:

include/hero/PlungingState.h
@@ -26,14 +26,13 @@ class Hero::PlungingState: public Hero::State {
 
   public:
 
-    PlungingState(Hero &hero);
+    PlungingState(Hero& hero);
     ~PlungingState();
 
-    void start(State *previous_state);
+    void start(State* previous_state);
     void update();
     bool can_start_gameover_sequence();
     bool is_touching_ground();
-    CarriedItem::Behavior get_previous_carried_item_behavior(CarriedItem& carried_item);
 };
 
 #endif

src/hero/CarryingState.cpp
@@ -80,7 +80,6 @@ void Hero::CarryingState::stop(State *next_state) {
     case CarriedItem::BEHAVIOR_DESTROY:
       delete carried_item;
       carried_item = NULL;
-      get_sprites().set_lifted_item(NULL);
       break;
 
     case CarriedItem::BEHAVIOR_KEEP:

src/hero/LiftingState.cpp
@@ -69,9 +69,9 @@ void Hero::LiftingState::stop(State *next_state) {
 
   State::stop(next_state);
 
-  get_sprites().set_lifted_item(NULL);
-
   if (lifted_item != NULL) {
+
+    get_sprites().set_lifted_item(NULL);
 
     // the lifted item is still managed by this state
     switch (next_state->get_previous_carried_item_behavior(*lifted_item)) {
@@ -83,7 +83,6 @@ void Hero::LiftingState::stop(State *next_state) {
     case CarriedItem::BEHAVIOR_DESTROY:
       delete lifted_item;
       lifted_item = NULL;
-      get_sprites().set_lifted_item(NULL);
       break;
 
     case CarriedItem::BEHAVIOR_KEEP:

src/hero/PlungingState.cpp
@@ -27,7 +27,7 @@
  * @brief Constructor.
  * @param hero the hero controlled by this state
  */
-Hero::PlungingState::PlungingState(Hero &hero):
+Hero::PlungingState::PlungingState(Hero& hero):
   State(hero) {
 
 }
@@ -43,7 +43,7 @@ Hero::PlungingState::~PlungingState() {
  * @brief Starts this state.
  * @param previous_state the previous state
  */
-void Hero::PlungingState::start(State *previous_state) {
+void Hero::PlungingState::start(State* previous_state) {
 
   State::start(previous_state);
 
@@ -99,15 +99,5 @@ bool Hero::PlungingState::can_start_gameover_sequence() {
  */
 bool Hero::PlungingState::is_touching_ground() {
   return false;
-}
-
-/**
- * @brief Returns the action to do with an item previously carried by the hero when this state starts.
- * @param carried_item the item carried in the previous state
- * @return the action to do with a previous carried item when this state starts
- */
-CarriedItem::Behavior Hero::PlungingState::get_previous_carried_item_behavior(CarriedItem& carried_item) {
-
-  return CarriedItem::BEHAVIOR_DESTROY;
 }
 

src/hero/SwimmingState.cpp
@@ -168,3 +168,4 @@ bool Hero::SwimmingState::is_stairs_obstacle(Stairs& stairs) {
 bool Hero::SwimmingState::can_take_jump_sensor() {
   return true;
 }
+

commit 8ad46273548f27065877924aed08bf017f8e8452

The carried item took animation "walking" after stairs of a jump.
This fixes a bug introduced by commit dbdc68e3.

src/hero/JumpingState.cpp
@@ -92,19 +92,19 @@ void Hero::JumpingState::stop(State *next_state) {
 
   if (carried_item != NULL) {
 
-    get_sprites().set_lifted_item(NULL);
-
     switch (next_state->get_previous_carried_item_behavior(*carried_item)) {
 
     case CarriedItem::BEHAVIOR_THROW:
       carried_item->throw_item(get_sprites().get_animation_direction());
       get_entities().add_entity(carried_item);
       carried_item = NULL;
+      get_sprites().set_lifted_item(NULL);
       break;
 
     case CarriedItem::BEHAVIOR_DESTROY:
       delete carried_item;
       carried_item = NULL;
+      get_sprites().set_lifted_item(NULL);
       break;
 
     case CarriedItem::BEHAVIOR_KEEP:

src/hero/StairsState.cpp
@@ -120,19 +120,19 @@ void Hero::StairsState::stop(State* next_state) {
 
   if (carried_item != NULL) {
 
-    get_sprites().set_lifted_item(NULL);
-
     switch (next_state->get_previous_carried_item_behavior(*carried_item)) {
 
     case CarriedItem::BEHAVIOR_THROW:
       carried_item->throw_item(get_sprites().get_animation_direction());
       get_entities().add_entity(carried_item);
       carried_item = NULL;
+      get_sprites().set_lifted_item(NULL);
       break;
 
     case CarriedItem::BEHAVIOR_DESTROY:
       delete carried_item;
       carried_item = NULL;
+      get_sprites().set_lifted_item(NULL);
       break;
 
     case CarriedItem::BEHAVIOR_KEEP:
bded58bf3077b760fa91f87470c8579ba098ab05
Christopho <christopho@solarus-engine.org>
2011-12-16 05:34:08 +0100

    Increase the damage of duck soldiers

--- quests/zsdx/data/enemies/red_duck_soldier.lua
+++ quests/zsdx/data/enemies/red_duck_soldier.lua
@@ -5,7 +5,7 @@ set_properties({
   main_sprite = "enemies/red_duck_soldier",
   sword_sprite = "enemies/red_duck_soldier_sword",
   life = 8,
-  damage = 3,
+  damage = 12,
   hurt_style = "monster",
   play_hero_seen_sound = true
 })

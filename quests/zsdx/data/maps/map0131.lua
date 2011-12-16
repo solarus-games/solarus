b860982bac74d03773f8b81643f3ace2a34727da
Christopho <christopho@solarus-engine.org>
2011-12-16 10:13:43 +0100

    Ending sequence: slow down the camera

--- quests/zsdx/data/maps/map0131.lua
+++ quests/zsdx/data/maps/map0131.lua
@@ -7,7 +7,7 @@ function event_map_started(destination_point_name)
     sol.map.hero_set_visible(false)
     sol.map.hud_set_enabled(false)
     sol.map.hud_set_pause_enabled(false)
-    sol.main.timer_start(sol.game.reset, 10000)
+    sol.main.timer_start(sol.game.reset, 25000)
   end
 end
 

ff015a4ddfd7680af1cce507846dde59d84d732b
Christopho <christopho@solarus-engine.org>
2011-12-19 11:55:54 +0100

    Update copyright dates

--- include/hero/HeroSprites.h
+++ include/hero/HeroSprites.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2009 Christopho, Solarus - http://www.solarus-engine.org
+ * Copyright (C) 2009-2011 Christopho, Solarus - http://www.solarus-engine.org
  * 
  * Solarus is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

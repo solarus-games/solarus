solarus-games/solarus

commit 3c19643f995ad8249368e6d2e4d493677b3bc714

Don't make enemies create their movement in event_appear().
The preferred way is to create movements in event_restart(), which is
/bin/bash: q : commande introuvable
event_appear() is only called at the creation of the enemy and should be
used to set its properties.

quests/zsdx/data/enemies/minillosaur_egg_thrown.lua
@@ -21,10 +21,6 @@ function event_appear()
   local x, y = sol.enemy.get_position()
   local hero_x, hero_y = sol.map.hero_get_position()
   local angle = sol.main.get_angle(x, y, hero_x, hero_y)
-  local m = sol.main.straight_movement_create(120, angle)
-  sol.main.movement_set_property(m, "max_distance", 180)
-  sol.main.movement_set_property(m, "smooth", false)
-  sol.enemy.start_movement(m)
 end
 
 -- The enemy was stopped for some reason and should restart
@@ -33,6 +29,10 @@ function event_restart()
   if in_egg then
     local sprite = sol.enemy.get_sprite()
     sol.main.sprite_set_animation(sprite, "egg")
+    local m = sol.main.straight_movement_create(120, angle)
+    sol.main.movement_set_property(m, "max_distance", 180)
+    sol.main.movement_set_property(m, "smooth", false)
+    sol.enemy.start_movement(m)
   end
 end
 

quests/zsdx/data/enemies/bubble.lua
@@ -14,8 +14,6 @@ function event_appear()
   sol.enemy.set_origin(6, 6)
   sol.enemy.set_can_hurt_hero_running(true)
   sol.enemy.set_invincible()
-  local m = sol.main.straight_movement_create(80, 0)
-  sol.enemy.start_movement(m)
 end
 
 -- The enemy was stopped for some reason and should restart
@@ -62,7 +60,9 @@ end
 -- Makes the Bubble go towards a diagonal direction (1, 3, 5 or 7)
 function go(direction8)
 
-  local m = sol.enemy.get_movement()
+  local m = sol.main.straight_movement_create(80, 0)
+  sol.enemy.start_movement(m)
+
   sol.main.movement_set_property(m, "speed", 80)
   sol.main.movement_set_property(m, "angle", direction8 * math.pi / 4)
   last_direction8 = direction8

quests/zsdx/data/enemies/chain_and_ball.lua
@@ -76,7 +76,7 @@ function event_position_changed(x, y)
   end
 end
 
-function event_enabled()
+function event_restart()
 
   local m = sol.main.circle_movement_create(7, father_name, 56)
   sol.main.movement_set_property(m, "center_dx", center_xy.x)

quests/zsdx/data/enemies/khotor.lua
@@ -17,16 +17,16 @@ function event_appear(creator_name)
   sol.enemy.set_attack_consequence("thrown_item", 1)
   sol.enemy.set_attack_consequence("arrow", 1)
 
-  -- set the movement
-  local m = sol.main.random_path_movement_create(40)
-  sol.enemy.start_movement(m)
-
   -- create the chain and ball
   chain_name = sol.enemy.get_name().."_chain"
   sol.enemy.create_son(chain_name, "chain_and_ball", -16, -33)
 end
 
 function event_restart()
+
+  -- set the movement
+  local m = sol.main.random_path_movement_create(40)
+  sol.enemy.start_movement(m)
 
   sol.map.enemy_set_enabled(chain_name, true)
 end

quests/zsdx/data/enemies/mini_mowatulla.lua
@@ -17,9 +17,6 @@ function event_appear()
   sol.main.sprite_set_animation(sprite, "shell")
   in_shell = true
   sol.main.timer_start(break_shell, 1000)
-
-  local m = sol.main.target_movement_create(64)
-  sol.enemy.start_movement(m)
 end
 
 -- The enemy was stopped for some reason and should restart
@@ -28,6 +25,8 @@ function event_restart()
   if in_shell then
     local sprite = sol.enemy.get_sprite()
     sol.main.sprite_set_animation(sprite, "shell")
+    local m = sol.main.target_movement_create(64)
+    sol.enemy.start_movement(m)
   end
 end
 

quests/zsdx/data/enemies/simple_green_soldier.lua
@@ -10,13 +10,13 @@ function event_appear()
   sol.enemy.create_sprite("enemies/simple_green_soldier")
   sol.enemy.set_size(16, 16)
   sol.enemy.set_origin(8, 13)
-
-  local m = sol.main.straight_movement_create(0, 0)
-  sol.enemy.start_movement(m)
 end
 
 -- The enemy was stopped for some reason and should restart
 function event_restart()
+
+  local m = sol.main.straight_movement_create(0, 0)
+  sol.enemy.start_movement(m)
   local direction4 = math.random(4) - 1
   go(direction4)
 end

quests/zsdx/data/maps/map0046.dat
@@ -26,10 +26,8 @@
 1	1	160	213	destination_point	1	0
 2	1	152	224	16	16	teletransporter	0	1	8	from_west_mountain_cave_1f_w
 2	1	32	0	16	16	teletransporter_16	0	1	46	destination_point
-17	1	128	133	0
-17	1	128	149	0
-9	1	144	133	block	-1	entities/block	1	0	1
-9	1	152	149	block_2	-1	entities/block	1	0	1
+7	1	156	133	enemy	3	gibdo	0	-1	_random	1	-1
+12	1	152	120	16	16	custom_obstacle	0	1	0	0
 0	2	112	216	96	8	357
 0	2	144	216	32	8	188
 0	2	0	0	320	16	170

commit 5982bdc6faf014afec07875737130586852e7b4d

The movement of minillosaur_egg_thrown was broken since commit 3c19643f

quests/zsdx/data/enemies/minillosaur_egg_thrown.lua
@@ -18,9 +18,6 @@ function event_appear()
   local sprite = sol.enemy.get_sprite()
   sol.main.sprite_set_animation(sprite, "egg")
   in_egg = true
-  local x, y = sol.enemy.get_position()
-  local hero_x, hero_y = sol.map.hero_get_position()
-  local angle = sol.main.get_angle(x, y, hero_x, hero_y)
 end
 
 -- The enemy was stopped for some reason and should restart
@@ -29,6 +26,9 @@ function event_restart()
   if in_egg then
     local sprite = sol.enemy.get_sprite()
     sol.main.sprite_set_animation(sprite, "egg")
+    local x, y = sol.enemy.get_position()
+    local hero_x, hero_y = sol.map.hero_get_position()
+    local angle = sol.main.get_angle(x, y, hero_x, hero_y)
     local m = sol.main.straight_movement_create(120, angle)
     sol.main.movement_set_property(m, "max_distance", 180)
     sol.main.movement_set_property(m, "smooth", false)
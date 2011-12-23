solarus-games/solarus

commit 1d01331bf0c1614a8aec850524c221b956b31636

Display the grass or water sprite after falling into a hole

src/entities/Hero.cpp
@@ -1146,16 +1146,11 @@ void Hero::notify_ground_changed() {
     break;
 
   case GROUND_SHALLOW_WATER:
+    start_shallow_water();
+    break;
+
   case GROUND_GRASS:
-    {
-      // display a special sprite below the hero
-      sprites->create_ground(ground);
-
-      uint32_t now = System::now();
-      next_ground_date = std::max(next_ground_date, now);
-
-      set_walking_speed(normal_walking_speed * 4 / 5);
-    }
+    start_grass();
     break;
 
   case GROUND_LADDER:
@@ -1876,6 +1871,35 @@ void Hero::hurt(const Rectangle& source_xy, int life_points, int magic_points) {
 }
 
 /**
+ * @brief Displays a grass sprite below the hero and makes him walk slower.
+ */
+void Hero::start_grass() {
+
+  // display a special sprite below the hero
+  sprites->create_ground(GROUND_GRASS);
+
+  uint32_t now = System::now();
+  next_ground_date = std::max(next_ground_date, now);
+
+  set_walking_speed(normal_walking_speed * 4 / 5);
+}
+
+/**
+ * @brief Displays a shallow water sprite below the hero and makes him walk
+ * slower.
+ */
+void Hero::start_shallow_water() {
+
+  // display a special sprite below the hero
+  sprites->create_ground(GROUND_SHALLOW_WATER);
+
+  uint32_t now = System::now();
+  next_ground_date = std::max(next_ground_date, now);
+
+  set_walking_speed(normal_walking_speed * 4 / 5);
+}
+
+/**
  * @brief This function is called when the hero was dead but saved by a fairy.
  */
 void Hero::get_back_from_death() {
@@ -2217,10 +2241,26 @@ void Hero::start_state_from_ground() {
     start_prickle(0);
     break;
 
-  case GROUND_NORMAL:
   case GROUND_SHALLOW_WATER:
+    start_shallow_water();
+    if (state->is_carrying_item()) {
+      set_state(new CarryingState(*this, state->get_carried_item()));
+    }
+    else {
+      set_state(new FreeState(*this));
+    }
+
   case GROUND_GRASS:
+    start_grass();
+    if (state->is_carrying_item()) {
+      set_state(new CarryingState(*this, state->get_carried_item()));
+    }
+    else {
+      set_state(new FreeState(*this));
+    }
+
   case GROUND_LADDER:
+  case GROUND_NORMAL:
   case GROUND_EMPTY:
     if (state->is_carrying_item()) {
       set_state(new CarryingState(*this, state->get_carried_item()));

src/hero/BackToSolidGroundState.cpp
@@ -104,7 +104,7 @@ void Hero::BackToSolidGroundState::update() {
     }
 
     if (now >= end_date) {
-      hero.set_state(new FreeState(hero));
+      hero.start_state_from_ground();
     }
   }
 }

include/entities/Hero.h
@@ -294,6 +294,8 @@ class Hero: public MapEntity {
     bool is_moving_grabbed_entity();
     void notify_grabbed_entity_collision();
 
+    void start_grass();
+    void start_shallow_water();
     void start_deep_water();
     void start_hole();
     void start_lava();

commit e7925d361919ba4a4d451ee6211c20e9e87f82e5

Detectors were not ignored while coming back to solid ground
This bug was introduced in commit 1d01331b.

src/entities/Hero.cpp
@@ -1192,7 +1192,9 @@ Ground Hero::get_ground() {
  */
 void Hero::set_ground(Ground ground) {
 
-  if (ground != this->ground && ground != GROUND_EMPTY) {
+  if (ground != this->ground
+      && ground != GROUND_EMPTY
+      && !state->are_collisions_ignored()) {
     this->ground = ground;
   }
   // we don't call notify_ground_changed() from here because set_ground()

src/hero/BackToSolidGroundState.cpp
@@ -176,10 +176,10 @@ bool Hero::BackToSolidGroundState::is_touching_ground() {
 
 /**
  * @brief Returns whether this state ignores the collisions with the detectors and the ground.
- * @return true if the collision are ignored
+ * @return true if the collisions are ignored
  */
 bool Hero::BackToSolidGroundState::are_collisions_ignored() {
-  return System::now() < end_date;
+  return true;
 }
 
 /**

src/hero/State.cpp
@@ -544,7 +544,7 @@ void Hero::State::notify_ground_changed() {
 
 /**
  * @brief Returns whether this state ignores the collisions with the detectors and the ground.
- * @return true if the collision are ignored
+ * @return true if the collisions are ignored
  */
 bool Hero::State::are_collisions_ignored() {
   return false;
14ba00d3f63c7e6d071f786beffd8f1d65c952bd
Christopho <christopho@solarus-engine.org>
2011-12-28 11:37:33 +0100

    Dungeon 8 B4: the two switches could remain activated when leaving them

--- quests/zsdx/data/maps/map0051.lua
+++ quests/zsdx/data/maps/map0051.lua
@@ -12,10 +12,8 @@
 -- BB: Barrier Button
 -- DS: Door Sensor
 
-DB03_status = false
-DB04_status = false
-
 function event_map_started()
+
   sol.map.light_set(0)
   if not sol.game.savegame_get_boolean(725) then
     sol.map.block_set_enabled("STT5", false)
@@ -24,27 +22,18 @@ function event_map_started()
 end
 
 function event_switch_activated(switch_name)
-  if switch_name == "DB03" then
-    DB03_status = true
-  elseif switch_name == "DB04" then
-    DB04_status = true
-  end
-  if DB03_status == true and DB04_status == true then
-    if not sol.map.door_is_open("LD06") then
-      sol.map.door_open("LD06")
-    end
+
+  if sol.map.switch_is_activated("DB03")
+      and sol.map.switch_is_activated("DB04") then
+    sol.map.door_open("LD06")
   end
 end
 
 function event_switch_inactivated(switch_name)
-  if switch_name == "DB03" then
-    DB03_status = false
-  elseif switch_name == "DB04" then
-    DB04_status = false
-  end
-  if DB03_status == false or DB04_status == false then
-    if sol.map.door_is_open("LD06") then
-      sol.map.door_close("LD06")
-    end
+
+  if not sol.map.switch_is_activated("DB03")
+      or not sol.map.switch_is_activated("DB04") then
+    sol.map.door_close("LD06")
   end
 end
+

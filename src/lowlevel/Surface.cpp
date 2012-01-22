e68493bf27e0f31423ecdf423b3986225a7d6c64
vlag <valentin.soudier@gmail.com>
2012-01-22 05:28:01 +0100

    Remove OSX temporary code

--- src/lowlevel/Surface.cpp
+++ src/lowlevel/Surface.cpp
@@ -29,11 +29,9 @@
  */
 Surface::Surface(int width, int height):
   internal_surface_created(true) {
-#ifndef __APPLE__
-  this->internal_surface = SDL_CreateRGBSurface(SDL_HWSURFACE, width, height, 32, 0, 0, 0, 0);
-#else
-  this->internal_surface = SDL_CreateRGBSurface(SDL_HWSURFACE, width, height, 32, 0x0000ff00, 0x00ff0000, 0xff000000, 0x00000000);
-#endif
+
+  this->internal_surface = SDL_CreateRGBSurface(SDL_HWSURFACE, width, height,
+      32, 0, 0, 0, 0);
 }
 
 /**

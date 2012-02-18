9cf2daa5918acfc59fdd0b38464aeff32415fe5c
Christopho <christopho@solarus-engine.org>
2012-02-18 12:57:24 +0100

    Enable English again

--- quests/zsdx/data/languages/languages.lua
+++ quests/zsdx/data/languages/languages.lua
@@ -1,16 +1,15 @@
 language{
   code = "fr",
   name = "Français",
-  default = true
 }
 
---[[
 language{
   code = "en",
   name = "English",
   default = true
 }
 
+--[[
 language{
   code = "de",
   name = "Deutsch"

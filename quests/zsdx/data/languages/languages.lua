1732d69c2cae34e65c0cd36c0ef7211204a4f121
Christopho <christopho@solarus-engine.org>
2012-02-18 10:54:31 +0100

    Disable English temporarily

--- quests/zsdx/data/languages/languages.lua
+++ quests/zsdx/data/languages/languages.lua
@@ -1,15 +1,16 @@
 language{
   code = "fr",
   name = "Français",
+  default = true
 }
 
+--[[
 language{
   code = "en",
   name = "English",
   default = true
 }
 
---[[
 language{
   code = "de",
   name = "Deutsch"

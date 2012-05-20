91fd76f567cbb8d3313fe614617b2536585e32e7
Christophe <christopho128@gmail.com>
2012-05-20 14:51:35 +0200

    My sed forgot this line.

--- quests/zsdx/data/maps/map0131.lua
+++ quests/zsdx/data/maps/map0131.lua
@@ -7,7 +7,9 @@ function event_map_started(destination_point_name)
     sol.map.hero_set_visible(false)
     sol.map.hud_set_enabled(false)
     sol.map.hud_set_pause_enabled(false)
-    sol.main:start_timer(25000, sol.game.reset)
+    sol.main:start_timer(25000, function()
+      sol.map.get_game().reset()
+    end)
   end
 end
 

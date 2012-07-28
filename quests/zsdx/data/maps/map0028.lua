d394ed9664dd3a1f09cf8f0d15cc847344ea39cc
Christopho <christopho@solarus-games.org>
2012-07-28 17:36:21 +0200

    Enable Link's house script with the intermediate API.

--- quests/zsdx/data/maps/map0028.lua
+++ quests/zsdx/data/maps/map0028.lua
@@ -1,53 +1,65 @@
 -- Link's house
 
-bed_sprite = nil
+-- This is an intermediate version:
+-- - The map is now a proper userdata and this file is a function that manages it.
+-- - The map API is still the old one (entities are not userdata yet).
 
-function event_map_started(destination_point_name)
+local map = ...
 
-  bed_sprite = sol.map.npc_get_sprite("bed")
+function map:on_started(destination_point_name)
+
   if destination_point_name == "from_intro" then
     -- the intro scene is playing
-    sol.map.hud_set_enabled(true)
-    sol.map.hud_set_pause_enabled(false)
-    sol.map.dialog_set_style(0)
-    local snores_sprite = sol.map.npc_get_sprite("snores")
+    self:hud_set_enabled(true)
+    self:hud_set_pause_enabled(false)
+    self:dialog_set_style(0)
+    local snores_sprite = self:npc_get_sprite("snores")
+    local bed_sprite = self:npc_get_sprite("bed")
     snores_sprite:set_ignore_suspend(true)
     bed_sprite:set_animation("hero_sleeping")
-    sol.map.hero_freeze()
-    sol.map.hero_set_visible(false)
-    sol.timer.start(2000, sahasrahla_dream)
+    self:hero_freeze()
+    self:hero_set_visible(false)
+    sol.timer.start(2000, function()
+      self:sahasrahla_dream()
+    end)
   else
-    sol.map.npc_remove("snores")
+    self:npc_remove("snores")
   end
 end
 
-function sahasrahla_dream()
-  sol.map.dialog_start("link_house.dream")
-  sol.map.dialog_set_variable("link_house.dream", sol.map.get_game():get_player_name())
+function map:sahasrahla_dream()
+  self:dialog_start("link_house.dream")
+  self:dialog_set_variable("link_house.dream", self:get_game():get_player_name())
 end
 
-function event_dialog_finished(dialog_id)
+function map:on_dialog_finished(dialog_id)
   
   if dialog_id == "link_house.dream" then
-    sol.timer.start(1000, wake_up)
+    sol.timer.start(1000, function()
+      self:wake_up()
+    end)
   end
 end
 
-function wake_up()
-  sol.map.npc_remove("snores")
+function map:wake_up()
+  self:npc_remove("snores")
+  local bed_sprite = self:npc_get_sprite("bed")
   bed_sprite:set_animation("hero_waking")
-  sol.timer.start(500, jump_from_bed)
+  sol.timer.start(500, function()
+    self:jump_from_bed()
+  end)
 end
 
-function jump_from_bed()
-  sol.map.hero_set_visible(true)
-  sol.map.hero_start_jumping(4, 24, true)
-  sol.map.hud_set_pause_enabled(true)
+function map:jump_from_bed()
+  self:hero_set_visible(true)
+  self:hero_start_jumping(4, 24, true)
+  self:hud_set_pause_enabled(true)
+  local bed_sprite = self:npc_get_sprite("bed")
   bed_sprite:set_animation("empty_open")
-  sol.audio.play_sound("hero_lands");
+  sol.audio.play_sound("hero_lands")
 end
 
-function event_door_open(door_name)
+function map:on_door_open(door_name)
 
   if door_name == "weak_wall" then
     sol.audio.play_sound("secret")

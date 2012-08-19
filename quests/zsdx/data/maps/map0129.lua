330e283821131b1c32373f794923c78b5ae53219
Christopho <christopho@solarus-games.org>
2012-08-19 12:08:42 +0200

    Replace sensors of subtype "change layer" by usual ones.

--- quests/zsdx/data/maps/map0129.lua
+++ quests/zsdx/data/maps/map0129.lua
@@ -1,1 +1,11 @@
 local map = ...
+
+function map:on_hero_on_sensor(sensor_name)
+
+  local layer = sensor_name:match("^to_layer_([0-1])_sensor")
+  if layer ~= nil then
+    local x, y = map:hero_get_position()
+    map:hero_set_position(x, y, layer)
+  end
+end
+

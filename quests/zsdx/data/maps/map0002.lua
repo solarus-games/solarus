1b56d4b6767ed342ca8f61c1ba5cec924112506b
Christopho <christopho@solarus-games.org>
2012-08-23 17:16:45 +0200

    Update the rupee house script to the new entity functions.

--- quests/zsdx/data/maps/map0002.lua
+++ quests/zsdx/data/maps/map0002.lua
@@ -1,132 +1,135 @@
 local map = ...
--- Rupee house
 
--- Initializations made when the map has just been loaded
+-- Rupee house.
+
+-- Initializations made when the map has just been loaded.
+local hero = map:get_hero()
 local playing_game_1 = false
 local playing_game_2 = false
 local playing_game_3 = false
 local already_played_game_1 = false
-local game_1_rewards = {5, 20, 50} -- possible rupee rewards in game 1
+local game_1_rewards = {5, 20, 50}  -- Possible rupee rewards in game 1.
 local game_2_bet = 0
 local game_2_reward = 0
 local game_2_man_sprite = nil
 local game_2_timer
 local game_2_slots = {
-  slot_machine_left =   {initial_frame = 6, initial_delay = 70, current_delay = 0, symbol = -1, sprite = nil},
-  slot_machine_middle = {initial_frame = 15, initial_delay = 90, current_delay = 0, symbol = -1, sprite = nil},
-  slot_machine_right =  {initial_frame = 9, initial_delay = 60, current_delay = 0, symbol = -1, sprite = nil}
-} -- the key is also the entity name
+  [slot_machine_left] =   {initial_frame = 6, initial_delay = 70, current_delay = 0, symbol = -1},
+  [slot_machine_middle] = {initial_frame = 15, initial_delay = 90, current_delay = 0, symbol = -1},
+  [slot_machine_right] =  {initial_frame = 9, initial_delay = 60, current_delay = 0, symbol = -1}
+}  -- The key is also the entity.
 local game_3_timer
 
--- Function called when the map starts
-function map:on_started(destination_point_name)
+-- Function called when the map starts.
+function map:on_started(destination)
 
-  for k, v in pairs(game_2_slots) do
-    v.sprite = map:npc_get_sprite(k)
-    v.sprite:set_frame(v.initial_frame)
+  chest_1:on_empty = open_game_1_chest
+  chest_2:on_empty = open_game_1_chest
+  chest_3:on_empty = open_game_1_chest
+  for npc, v in pairs(game_2_slots) do
+    npc:get_sprite():set_frame(v.initial_frame)
+    npc:on_interaction = function()
+      activate_slot_machine(npc)
+    end
   end
   game_2_man_sprite = map:npc_get_sprite("game_2_man")
 end
 
--- Function called when the player wants to talk to a non-playing character
-function map:on_npc_interaction(npc_name)
+-- Function called when the player wants to talk to this non-playing character.
+function game_1_man:on_interaction()
+  -- game 1 dialog
 
-  if npc_name == "game_1_man" then
-    -- game 1 dialog
+  if playing_game_1 then
+    -- the player is already playing: tell him to choose a chest
+    map:start_dialog("rupee_house.game_1.choose_chest")
+  else
 
-    if playing_game_1 then
-      -- the player is already playing: tell him to choose a chest
-      map:start_dialog("rupee_house.game_1.choose_chest")
-    else
+    -- see if the player can still play
+    local unauthorized = map:get_game():get_boolean(16)
 
-      -- see if the player can still play
-      local unauthorized = map:get_game():get_boolean(16)
-
-      if unauthorized then
-	-- the player already won much money
-	map:start_dialog("rupee_house.game_1.not_allowed_to_play")
-      else 
-	if not already_played_game_1 then
-	  -- first time: long dialog with the game rules
-	  map:start_dialog("rupee_house.game_1.intro")
-	else
-	  -- quick dialog to play again
-	  map:start_dialog("rupee_house.game_1.play_again_question")
-	end
-      end
-    end
-
-  elseif npc_name == "game_2_man" then
-    -- game 2 dialog
-
-    if playing_game_2 then
-      -- the player is already playing: tell him to stop the reels
-      map:start_dialog("rupee_house.game_2.playing")
-    else
-      -- dialog with the game rules
-      map:start_dialog("rupee_house.game_2.intro")
-    end
-
-  elseif npc_name == "game_3_man" then
-    -- game 3 dialog
-
-    if playing_game_3 then
-      -- the player is already playing: let him restart the game
-      map:start_dialog("rupee_house.game_3.restart_question")
-    else
-      -- see if the player can still play
-      unauthorized = map:get_game():get_boolean(17)
-
-      if unauthorized then
-	-- the player already won this game
-	map:start_dialog("rupee_house.game_3.not_allowed_to_play")
+    if unauthorized then
+      -- the player already won much money
+      map:start_dialog("rupee_house.game_1.not_allowed_to_play")
+    else 
+      if not already_played_game_1 then
+        -- first time: long dialog with the game rules
+        map:start_dialog("rupee_house.game_1.intro", game_1_question_dialog_finished)
       else
-	-- game rules
-	map:start_dialog("rupee_house.game_3.intro")
+        -- quick dialog to play again
+        map:start_dialog("rupee_house.game_1.play_again_question", game_1_question_dialog_finished)
       end
-    end
-
-  elseif string.find(npc_name, "^slot_machine_") then
-    
-    if playing_game_2 then
-
-      game_2_man_sprite:set_direction(0)
-
-      if game_2_slots[npc_name].symbol == -1 then
-	-- stop this reel
-
-	local sprite = game_2_slots[npc_name].sprite
-	local current_symbol = math.floor(sprite:get_frame() / 3)
-	game_2_slots[npc_name].symbol = (current_symbol + math.random(2)) % 7
-	game_2_slots[npc_name].current_delay = game_2_slots[npc_name].current_delay + 100
-	sprite:set_frame_delay(game_2_slots[npc_name].current_delay)
-
-	-- test code (temporary code to win every game)
-	--	 for k, v in pairs(game_2_slots) do
-	--	    v.symbol = game_2_slots[npc_name].symbol
-	--	    v.current_delay = game_2_slots[npc_name].current_delay + 100
-	--	    v.sprite:set_frame_delay(v.current_delay)
-	--	 end
-	-----------
-
-	sol.audio.play_sound("switch")
-	map:get_hero():freeze()
-      end
-    else
-      sol.audio.play_sound("wrong")
-      map:start_dialog("rupee_house.pay_first")
     end
   end
 end
 
--- Function called when the dialog box is being closed
--- dialog_id: name of the first message of the sequence that has just finished
--- answer: the answer of the question (0 or 1) or -1 if there was no question
-function map:on_dialog_finished(dialog_id, answer)
+function game_2_man:on_interaction()
+
+  -- game 2 dialog
+
+  if playing_game_2 then
+    -- the player is already playing: tell him to stop the reels
+    map:start_dialog("rupee_house.game_2.playing")
+  else
+    -- dialog with the game rules
+    map:start_dialog("rupee_house.game_2.intro", game_2_question_dialog_finished)
+  end
+end
+
+function game_3_man:on_interaction()
+  -- game 3 dialog
+
+  if playing_game_3 then
+    -- the player is already playing: let him restart the game
+    map:start_dialog("rupee_house.game_3.restart_question", game_3_question_dialog_finished)
+  else
+    -- see if the player can still play
+    local unauthorized = map:get_game():get_boolean(17)
+
+    if unauthorized then
+      -- the player already won this game
+      map:start_dialog("rupee_house.game_3.not_allowed_to_play")
+    else
+      -- game rules
+      map:start_dialog("rupee_house.game_3.intro", game_3_question_dialog_finished)
+    end
+  end
+end
+
+local function activate_slot_machine(npc)
+
+  if playing_game_2 then
+
+    game_2_man_sprite:set_direction(0)
+    local slot = game_2_slots[npc]
+    if slot.symbol == -1 then
+      -- stop this reel
+
+      local sprite = slot.sprite
+      local current_symbol = math.floor(sprite:get_frame() / 3)
+      slot.symbol = (current_symbol + math.random(2)) % 7
+      slot.current_delay = slot.current_delay + 100
+      sprite:set_frame_delay(slot.current_delay)
+
+      -- test code to win every game:
+      -- for k, v in pairs(game_2_slots) do
+      --    v.symbol = slot.symbol
+      --    v.current_delay = slot.current_delay + 100
+      --    v.sprite:set_frame_delay(v.current_delay)
+      -- end
+
+      sol.audio.play_sound("switch")
+      map:get_hero():freeze()
+    end
+  else
+    sol.audio.play_sound("wrong")
+    map:start_dialog("rupee_house.pay_first")
+  end
+end
+
+local function game_1_question_dialog_finished(answer)
 
   if dialog_id == "rupee_house.game_1.intro" or 
     dialog_id == "rupee_house.game_1.play_again_question" then
-    -- if the dialog was the game 1 question
 
     if answer == 1 then
       -- the player does not want to play the game
@@ -141,117 +144,118 @@ function map:on_dialog_finished(dialog_id, answer)
 
       else
 	-- enough money: reset the 3 chests, pay and start the game
-	map:chest_set_open("chest_1", false)
-	map:chest_set_open("chest_2", false)
-	map:chest_set_open("chest_3", false)
+        chest_1:set_open(false)
+        chest_2:set_open(false)
+        chest_3:set_open(false)
 
 	map:get_game():remove_money(20)
 	map:start_dialog("rupee_house.game_1.good_luck")
 	playing_game_1 = true
       end
     end
+  end
+end
 
-  elseif dialog_id == "rupee_house.game_2.intro"  or
-    dialog_id == "rupee_house.game_2.reward.none" then
+local function game_2_question_dialog_finished(answer)
 
-    if answer == 1 then
-      -- don't want to play the game
-      map:start_dialog("rupee_house.game_2.not_playing")
-    else
-      -- wants to play game 2
-      map:start_dialog("rupee_house.game_2.choose_bet")
+  if answer == 1 then
+    -- don't want to play the game
+    map:start_dialog("rupee_house.game_2.not_playing")
+  else
+    -- wants to play game 2
+    map:start_dialog("rupee_house.game_2.choose_bet", game_2_choose_bet_dialog_finished)
+  end
+end
+
+local function game_2_choose_bet_dialog_finished(answer)
+
+  if answer == 0 then
+    -- bet 5 rupees
+    game_2_bet = 5
+  else
+    -- bet 20 rupees
+    game_2_bet = 20
+  end
+
+  if map:get_game():get_money() < game_2_bet then
+    -- not enough money
+    sol.audio.play_sound("wrong")
+    map:start_dialog("rupee_house.not_enough_money")
+  else
+    -- enough money: pay and start the game
+    map:get_game():remove_money(game_2_bet)
+    map:start_dialog("rupee_house.game_2.just_paid")
+    playing_game_2 = true
+
+    -- start the slot machine animations
+    for k, v in pairs(game_2_slots) do
+      v.symbol = -1
+      v.current_delay = v.initial_delay
+      v.sprite:set_animation("started")
+      v.sprite:set_frame_delay(v.current_delay)
+      v.sprite:set_frame(v.initial_frame)
+      v.sprite:set_paused(false)
     end
+  end
+end
 
-  elseif dialog_id == "rupee_house.game_2.choose_bet" then
+-- Called after the game 3 dialog "do you want to play?" or "do you want to try again?".
+local function game_3_question_dialog_finished(answer)
 
-    if answer == 0 then
-      -- bet 5 rupees
-      game_2_bet = 5
-    else
-      -- bet 20 rupees
-      game_2_bet = 20
-    end
+  if answer == 1 then
+    -- don't want to play the game
+    map:start_dialog("rupee_house.game_3.not_playing")
+  else
+    -- wants to play game 3
 
-    if map:get_game():get_money() < game_2_bet then
+    if map:get_game():get_money() < 10 then
       -- not enough money
       sol.audio.play_sound("wrong")
       map:start_dialog("rupee_house.not_enough_money")
+
     else
-      -- enough money: pay and start the game
-      map:get_game():remove_money(game_2_bet)
-      map:start_dialog("rupee_house.game_2.just_paid")
-      playing_game_2 = true
-
-      -- start the slot machine animations
-      for k, v in pairs(game_2_slots) do
-	v.symbol = -1
-	v.current_delay = v.initial_delay
-	v.sprite:set_animation("started")
-	v.sprite:set_frame_delay(v.current_delay)
-	v.sprite:set_frame(v.initial_frame)
-	v.sprite:set_paused(false)
+      -- enough money: reset the game, pay and start the game
+      reset_blocks()
+      game_3_barrier_1:set_enabled(false)
+      game_3_barrier_2:set_enabled(false)
+      game_3_barrier_3:set_enabled(false)
+      game_3_middle_barrier:set_enabled(false)
+      if game_3_timer ~= nil then
+        game_3_timer:stop()
+        game_3_timer = nil
       end
+
+      map:get_game():remove_money(10)
+      map:start_dialog("rupee_house.game_3.go", function()
+        game_3_timer = sol.timer.start(8000, function()
+          sol.audio.play_sound("door_closed")
+          game_3_middle_barrier:set_enabled(true)
+        end)
+        game_3_timer:set_with_sound(true)
+        game_3_sensor:set_enabled(true)
+      end)
+      playing_game_3 = true
     end
-  elseif string.find(dialog_id, "^rupee_house.game_2.reward.") then
-    -- reward in game 2
-    map:get_game():add_money(game_2_reward)
+  end
+end
 
-  elseif dialog_id == "rupee_house.game_3.intro" or 
-    dialog_id == "rupee_house.game_3.restart_question" then
-    -- if the dialog was the game 3 question
-
-    if answer == 1 then
-      -- don't want to play the game
-      map:start_dialog("rupee_house.game_3.not_playing")
-    else
-      -- wants to play game 3
-
-      if map:get_game():get_money() < 10 then
-	-- not enough money
-	sol.audio.play_sound("wrong")
-	map:start_dialog("rupee_house.not_enough_money")
-
-      else
-	-- enough money: reset the game, pay and start the game
-
-	reset_blocks()
-	map:tile_set_enabled("game_3_barrier_1", false)
-	map:tile_set_enabled("game_3_barrier_2", false)
-	map:tile_set_enabled("game_3_barrier_3", false)
-	map:tile_set_enabled("game_3_middle_barrier", false)
-	if game_3_timer ~= nil then
-	  game_3_timer:stop()
-	  game_3_timer = nil
-	end
-
-	map:get_game():remove_money(10)
-	map:start_dialog("rupee_house.game_3.go")
-	playing_game_3 = true
-      end
-    end
-
-  elseif dialog_id == "rupee_house.game_3.go" then 
-    game_3_timer = sol.timer.start(8000, game_3_timeout)
-    game_3_timer:set_with_sound(true)
-    map:sensor_set_enabled("game_3_sensor", true)
-
-  -- stop game 3 when the player founds the piece of heart
-  elseif string.find(dialog_id, "^found_piece_of_heart") then
-    map:tile_set_enabled("game_3_final_barrier", false)
+function hero:on_obtained_treasure(item_name, variant, savegame_variable)
+  -- stop game 3 when the player finds the piece of heart
+  if item_name == "piece_of_heart" then
+    game_3_final_barrier:set_enabled(false)
     sol.audio.play_sound("secret")
     playing_game_3 = false
   end
 end
 
 -- Function called when the player opens an empty chest (i.e. a chest
--- whose feature is to call the script).
--- chest_name: name of the chest being open
-function map:on_chest_empty(chest_name)
+-- whose feature is to call the script) of game 1.
+local function open_game_1_chest(chest)
 
   if not playing_game_1 then
     -- trying to open a chest but not playing yet
     map:start_dialog("rupee_house.pay_first") -- the game man is angry
-    map:chest_set_open(chest_name, false) -- close the chest again
+    chest:set_open(false) -- close the chest again
     sol.audio.play_sound("wrong")
     map:get_hero():unfreeze() -- restore the control
   else
@@ -282,24 +286,16 @@ function map:on_chest_empty(chest_name)
   end
 end
 
--- Function called when the timer of game 3 ends.
-function game_3_timeout()
-  sol.audio.play_sound("door_closed")
-  map:tile_set_enabled("game_3_middle_barrier", true)
-end
-
 -- Function called when a sensor is activated
-function map:on_hero_on_sensor(sensor_name)
+function game_3_sensor:on_activated()
 
-  if sensor_name == "game_3_sensor" then
-    -- stop the timer when the player reaches this point
-    if game_3_timer ~= nil then
-      game_3_timer:stop()
-      game_3_timer = nil
-    end
-    sol.audio.play_sound("secret")
-    map:sensor_set_enabled("game_3_sensor", false)
+  -- stop the timer when the player reaches this point
+  if game_3_timer ~= nil then
+    game_3_timer:stop()
+    game_3_timer = nil
   end
+  sol.audio.play_sound("secret")
+  self:set_enabled(false)
 end
 
 -- Updates the slot machine
@@ -310,15 +306,15 @@ function map:on_update()
     -- stop the reels when necessary
     local nb_finished = 0
     for k, v in pairs(game_2_slots) do
-      if sol.main.sprite_is_paused(v.sprite) then
+      if v.sprite:is_paused() then
 	nb_finished = nb_finished + 1
       end
     end
 
     for k, v in pairs(game_2_slots) do
-      local frame = sol.main.sprite_get_frame(v.sprite)
+      local frame = v.sprite:get_frame()
 
-      if not sol.main.sprite_is_paused(v.sprite) and frame == v.symbol * 3 then
+      if not v.sprite:is_paused() and frame == v.symbol * 3 then
 	v.sprite:set_paused(true)
 	v.initial_frame = frame
 	nb_finished = nb_finished + 1
@@ -361,28 +357,28 @@ function game_2_timeout()
     -- three identical symbols
 
     if symbols[1] == 0 then -- 3 green rupees
-      map:start_dialog("rupee_house.game_2.reward.green_rupees")
+      map:start_dialog("rupee_house.game_2.reward.green_rupees", game_2_give_reward)
       game_2_reward = 5 * game_2_bet
     elseif symbols[1] == 2 then -- 3 blue rupees
-      map:start_dialog("rupee_house.game_2.reward.blue_rupees")
+      map:start_dialog("rupee_house.game_2.reward.blue_rupees", game_2_give_reward)
       game_2_reward = 7 * game_2_bet
     elseif symbols[1] == 4 then -- 3 red rupees
-      map:start_dialog("rupee_house.game_2.reward.red_rupees")
+      map:start_dialog("rupee_house.game_2.reward.red_rupees", game_2_give_reward)
       game_2_reward = 10 * game_2_bet
     elseif symbols[1] == 5 then -- 3 Yoshi
-      map:start_dialog("rupee_house.game_2.reward.yoshi")
+      map:start_dialog("rupee_house.game_2.reward.yoshi", game_2_give_reward)
       game_2_reward = 20 * game_2_bet
     else -- other symbol
-      map:start_dialog("rupee_house.game_2.reward.same_any")
+      map:start_dialog("rupee_house.game_2.reward.same_any", game_2_give_reward)
       game_2_reward = 4 * game_2_bet
     end
 
   elseif green_found and blue_found and red_found then
     -- three rupees with different colors
-    map:start_dialog("rupee_house.game_2.reward.different_rupees")
+    map:start_dialog("rupee_house.game_2.reward.different_rupees", game_2_give_reward)
     game_2_reward = 15 * game_2_bet
   else
-    map:start_dialog("rupee_house.game_2.reward.none")
+    map:start_dialog("rupee_house.game_2.reward.none", game_2_question_dialog_finished)
     game_2_reward = 0
   end
 
@@ -395,10 +391,10 @@ function game_2_timeout()
   map:get_hero():unfreeze()
 end
 
-function reset_blocks()
+local function reset_blocks()
 
   for i = 3, 16 do
-    map:block_reset("block_" .. i)
+    map:get_entity("block_" .. i):reset()
   end
 end
 

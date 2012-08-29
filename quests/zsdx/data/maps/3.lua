78e7dd011397d88764a825584e7a9a5a6b58129f
Christopho <christopho@solarus-games.org>
2012-08-29 12:13:14 +0200

    Fix a syntax error.

--- quests/zsdx/data/maps/3.lua
+++ quests/zsdx/data/maps/3.lua
@@ -68,14 +68,14 @@ function map:on_started(destination_point)
     local sensor = map:get_entity(entrance_name .. "_door_sensor")
     local tile = map:get_entity(entrance_name .. "_door")
 
-    sensor:on_activated_repeat = function()
-    if hero:get_direction() == 1
+    sensor.on_activated_repeat = function()
+      if hero:get_direction() == 1
         and tile:is_enabled() then
-      tile:set_enabled(false)
-      sol.audio.play_sound("door_open")
+        tile:set_enabled(false)
+        sol.audio.play_sound("door_open")
+      end
     end
   end
-end
 end
 
 function monkey:on_interaction()

a440d25f3500807582a31bc57464d5aaedea9a01
Christopho <christopho@solarus-games.org>
2012-12-10 21:34:31 +0100

    Make the quiver and bomb bag set correctly the maximum amount.

--- quests/zsdx/data/items/bomb_bag.lua
+++ quests/zsdx/data/items/bomb_bag.lua
@@ -5,24 +5,29 @@ function item:on_created()
   self:set_savegame_variable("i1032")
 end
 
+function item:on_started()
+
+  self:on_variant_changed(self:get_variant())
+end
+
 function item:on_variant_changed(variant)
 
-  if variant > 0 then
-    -- Obtaining a bomb bag changes the max amount of the bomb counter.
+  -- Obtaining a bomb bag changes the max amount of the bomb counter.
+  local bombs_counter = self:get_game():get_item("bombs_counter")
+  local bomb = self:get_game():get_item("bomb")
+  if variant == 0 then
+    bombs_counter:set_max_amount(0)
+    bomb:set_obtainable(false)
+  else
     local max_amounts = {10, 30, 99}
     local max_amount = max_amounts[variant]
-    if max_amount == nil then
-      error("Invalid variant '" .. variant .. "' for item 'bomb_bag'")
-    end
 
     -- Set the max value of the bomb counter.
-    local bombs_counter = self:get_game():get_item("bombs_counter")
     bombs_counter:set_variant(1)
     bombs_counter:set_max_amount(max_amount)
     bombs_counter:set_amount(max_amount)
 
     -- Unlock pickable bombs.
-    local bomb = self:get_game():get_item("bomb")
     bomb:set_obtainable(true)
   end
 end
--- quests/zsdx/data/items/quiver.lua
+++ quests/zsdx/data/items/quiver.lua
@@ -5,22 +5,29 @@ function item:on_created()
   self:set_savegame_variable("i1033")
 end
 
+function item:on_started()
+
+  self:on_variant_changed(self:get_variant())
+end
+
 function item:on_variant_changed(variant)
 
   -- Obtaining a quiver changes the max amount of the bow.
-  local max_amounts = {10, 30, 99}
-  local max_amount = max_amounts[variant]
-  if max_amount == nil then
-    error("Invalid variant '" .. variant .. "' for item 'quiver'")
-  end
-
-  -- Set the max value of the bow counter.
   local bow = self:get_game():get_item("bow")
-  bow:set_max_amount(max_amount)
-  bow:set_amount(max_amount)
-
-  -- Unlock pickable arrows.
   local arrow = self:get_game():get_item("arrow")
-  arrow:set_obtainable(true)
+  if variant == 0 then
+    bow:set_max_amount(0)
+    arrow:set_obtainable(false)
+  else
+    local max_amounts = {10, 30, 99}
+    local max_amount = max_amounts[variant]
+
+    -- Set the max value of the bow counter.
+    bow:set_max_amount(max_amount)
+    bow:set_amount(max_amount)
+
+    -- Unlock pickable arrows.
+    arrow:set_obtainable(true)
+  end
 end
 

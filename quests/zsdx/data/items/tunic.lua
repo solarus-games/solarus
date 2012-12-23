7ca3b288bbc84793c1f9d963b55d63f8d3a034cb
Christopho <christopho@solarus-games.org>
2012-12-23 16:29:19 +0100

    Keep the old tunic while brandishing the new one.

--- quests/zsdx/data/items/tunic.lua
+++ quests/zsdx/data/items/tunic.lua
@@ -5,8 +5,8 @@ function item:on_created()
   self:set_savegame_variable("i1128")
 end
 
-function item:on_variant_changed(variant)
-  -- The possession state of the tunic determines the built-in ability "tunic".
+function item:on_obtained(variant, savegame_variable)
+  -- Give the built-in ability "tunic", but only after the treasure sequence is done.
   self:get_game():set_ability("tunic", variant)
 end
 

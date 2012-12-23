935f2b11c283baf2d3ee27702260b2fdb61a6a75
Christopho <christopho@solarus-games.org>
2012-12-23 17:46:19 +0100

    Giving the tunic no longer gives the built-in ability.

--- quests/zsdx/data/screens/savegames.lua
+++ quests/zsdx/data/screens/savegames.lua
@@ -993,6 +993,7 @@ function savegame_menu:set_initial_values(savegame)
   savegame:set_max_life(12)
   savegame:set_life(savegame:get_max_life())
   savegame:get_item("tunic"):set_variant(1)
+  savegame:set_ability("tunic", 1)
   savegame:get_item("rupee_bag"):set_variant(1)
 end
 

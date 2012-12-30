ab21e0ac78b06029a3a79533e02ba1856014c1e4
is06 <thomas.noury@is06.com>
2012-12-30 15:29:27 +0100

    Lighthouse: all levels added

--- quests/zmc/data/maps/lighthouse_1f.lua
+++ quests/zmc/data/maps/lighthouse_1f.lua
@@ -3,16 +3,31 @@ local map = ...
 function map:on_started()
   local game = map:get_game()
 
+  -- Item initialization (for testing purposes)
   game:get_item("sword"):set_variant(1)
   game:get_item("shield"):set_variant(1)
-
   game:get_item("lamp"):set_variant(1)
   game:get_item("magic_bar"):set_variant(1)
-
   game:get_item("feather"):set_variant(1)
   game:get_item("glove"):set_variant(1)
-
   game:get_item("bomb_bag"):set_variant(1)
   game:get_item("bombs_counter"):set_variant(1)
   game:get_item("bombs_counter"):set_amount(10)
+
+  -- Torch puzzle
+  for _, torch in ipairs(map:get_entities("torch_A")) do
+    torch.on_activated = torch_A_activated
+  end
+end
+
+function map:on_update()
+  if torch_A1 ~= nil and
+  torch_A1:get_sprite():get_animation() == "lit" and
+  torch_A2:get_sprite():get_animation() == "lit" and
+  torch_A3:get_sprite():get_animation() == "lit" and
+  torch_A4:get_sprite():get_animation() == "lit" then
+    map:remove_entities("torch_A")    
+    sol.audio.play_sound("secret")
+    map:open_doors("torch_door")
+  end
 end
--- quests/zmc/data/maps/lighthouse_b1.lua
+++ quests/zmc/data/maps/lighthouse_b1.lua
@@ -12,27 +12,16 @@ end
 
 local function star_sensor_B_activated()
   sol.audio.play_sound("secret")
-  for _, sensor in ipairs(map:get_entities("star_sensor_A")) do
-    sensor:set_enabled(true)
-  end
-  for _, sensor in ipairs(map:get_entities("star_sensor_B")) do
-    sensor:set_enabled(false)
-  end
-  for _, tile in ipairs(map:get_entities("star_tile_A")) do
-    tile:set_enabled(true)
-  end
-  for _, tile in ipairs(map:get_entities("star_tile_B")) do
-    tile:set_enabled(false)
-  end
-  for _, floor in ipairs(map:get_entities("star_floor_A")) do
-    floor:set_enabled(true)
-  end
-  for _, floor in ipairs(map:get_entities("star_floor_B")) do
-    floor:set_enabled(false)
-  end
+  map:set_entities_enabled("star_sensor_A", true)
+  map:set_entities_enabled("star_sensor_B", false)
+  map:set_entities_enabled("star_tile_A", true)
+  map:set_entities_enabled("star_tile_B", false)
+  map:set_entities_enabled("star_floor_A", true)
+  map:set_entities_enabled("star_floot_B", false)
 end
 
 function map:on_started()
+  -- Star tiles puzzle
   for _, sensor in ipairs(map:get_entities("star_sensor_A")) do
     sensor.on_activated = star_sensor_A_activated
   end

313be9bd4ea12123fbe486efcd813b62620c78a1
Christopho <christopho@solarus-games.org>
2013-03-24 20:15:54 +0100

    Give the apple/croissant/etc. counter when obtaining some.

--- quests/zsdx/data/items/fire_stone.lua
+++ quests/zsdx/data/items/fire_stone.lua
@@ -2,6 +2,10 @@ local item = ...
 
 function item:on_obtaining(variant, savegame_variable)
 
-  self:get_game():get_item("fire_stones_counter"):add_amount(1)
+  local fire_stones_counter = self:get_game():get_item("fire_stones_counter")
+  if fire_stones_counter:get_variant() == 0 then
+    fire_stones_counter:set_variant(1)
+  end
+  fire_stones_counter:add_amount(1)
 end
 

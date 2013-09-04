5a7e6da5698033be6cb21cb48efa07715fe95e48
Christopho <christopho@solarus-games.org>
2013-09-04 17:49:53 +0200

    Register custom entity creation function in the map loader.

--- src/MapLoader.cpp
+++ src/MapLoader.cpp
@@ -171,6 +171,7 @@ int MapLoader::l_properties(lua_State* l) {
     { "door",             LuaContext::map_api_create_door },
     { "stairs",           LuaContext::map_api_create_stairs },
     { "separator",        LuaContext::map_api_create_separator },
+    { "custom",           LuaContext::map_api_create_custom_entity },
     { NULL, NULL }
   };
   const luaL_Reg* function = functions;

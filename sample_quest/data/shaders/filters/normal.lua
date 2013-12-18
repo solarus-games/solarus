f49412289d84c0e5452642c177f103e4c2f0f711
val <valentin.soudier@gmail.com>
2013-12-18 16:54:44 +0100

    Add comment to the demonstration shader of the sample quest

--- sample_quest/data/shaders/filters/normal.lua
+++ sample_quest/data/shaders/filters/normal.lua
@@ -1,15 +1,22 @@
-video_driver_name = ...
-shading_language_version = ...
-sampler_type = ...
+-- This is a normal shader. It will display the exact same display than
+-- the default non-shaded video mode, but with a custom shaded video mode.
 
+video_driver_name = ... -- Get the rendering driver name.
+shading_language_version = ... -- Get the shading language version.
+sampler_type = ... -- Get the type of samplers to use.
+
+-- Get the suffix of the sampler type to use.
 sampler_suffix = ""
 if (sampler_type == "sampler2DRect") then
 	sampler_suffix = "Rect"	
 end
 
 shader {
+	-- Name of the shader
 	name = "normal",
+	-- Scale of the window compared to the quest_size
 	logical_scale = 1,
+	-- Source of the vertex shader
 	vertex_source = [[
 		varying vec4 v_color;
 		varying vec2 v_texCoord;
@@ -21,10 +28,11 @@ shader {
     		v_texCoord = vec2(gl_MultiTexCoord0);
 		}
 	]],
+	-- Source of the fragment shader
 	fragment_source = [[
 		varying vec4 v_color;
 		varying vec2 v_texCoord;
-		uniform sampler2D]] .. sampler_suffix .. [[ solarus_sampler;
+		uniform ]] .. sampler_type .. [[ solarus_sampler;
 
 		void main()
 		{

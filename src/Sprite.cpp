0a8ed93334e12aab19070fb263d9fe882d001643
Christopho <christopho@solarus-games.org>
2014-08-26 14:10:50 +0200

    Fix sprite:on_animation_changed() called twice.
    The event was called twice when changing the animation or direction of a
    sprite.

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -359,7 +359,7 @@ void Sprite::set_current_frame(int current_frame, bool notify_script) {
     this->current_frame = current_frame;
     set_frame_changed(true);
 
-    if (lua_context != NULL) {
+    if (notify_script && lua_context != NULL) {
       lua_context->sprite_on_frame_changed(
           *this, current_animation_name, current_frame);
     }

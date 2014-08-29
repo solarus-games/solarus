481056dda8b1077ca3b74512d247f578014931ba
Christopho <christopho@solarus-games.org>
2014-08-29 16:31:41 +0200

    Fix misplaced break.

--- src/entities/MapEntities.cpp
+++ src/entities/MapEntities.cpp
@@ -654,9 +654,9 @@ void MapEntities::add_entity(MapEntity* entity) {
           Destination* destination = static_cast<Destination*>(entity);
           if (this->default_destination == NULL || destination->is_default()) {
             this->default_destination = destination;
-            break;
           }
         }
+        break;
 
       default:
       break;

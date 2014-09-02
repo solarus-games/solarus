4b89eb036debe0621eab47f3b79103c170886c77
Morwenn <morwenn29@hotmail.fr>
2014-09-03 00:36:53 +0200

    Added Point and Size classes to replace Rectangle when it is overkill.

---
+++ include/lowlevel/Point.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2006-2014 Christopho, Solarus - http://www.solarus-games.org
+ *
+ * Solarus is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Solarus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef SOLARUS_POINT_H
+#define SOLARUS_POINT_H
+
+#include "Common.h"
+#include <ostream>
+
+namespace solarus
+{
+
+/**
+ * \brief Represents a 2D point.
+ *
+ * This class represents a point with its 2
+ * coordinates, x and y.
+ */
+struct Point {
+
+  ////////////////////////////////////////////////////////////
+  // Constructors
+
+  Point();
+  Point(int x, int y);
+
+  ////////////////////////////////////////////////////////////
+  // Operators
+
+  Point& operator+=(const Point& other);
+  Point& operator-=(const Point& other);
+
+  Point& operator*=(int factor);
+  Point& operator/=(int divisor);
+
+  ////////////////////////////////////////////////////////////
+  // Member data
+
+  int x = 0;    /**< First coordinate */
+  int y = 0;    /**< Second coordinate */
+
+};
+
+////////////////////////////////////////////////////////////
+// Free functions
+
+// Comparison operators
+bool operator==(const Point& lhs, const Point& rhs);
+bool operator!=(const Point& lhs, const Point& rhs);
+
+// Relational operators
+bool operator<(const Point& lhs, const Point& rhs);
+bool operator<=(const Point& lhs, const Point& rhs);
+bool operator>(const Point& lhs, const Point& rhs);
+bool operator>=(const Point& lhs, const Point& rhs);
+
+// Unary arithmetic operators
+Point operator+(Point point);
+Point operator-(const Point& point);
+
+// Binary arithmetic operators
+Point operator+(Point lhs, const Point& rhs);
+Point operator-(Point lhs, const Point& rhs);
+
+Point operator*(Point point, int factor);
+Point operator*(int factor, Point point);
+Point operator/(Point point, int divisor);
+
+// Output to stream
+std::ostream& operator<<(std::ostream& stream, const Point& point);
+
+}
+
+#include "Point.inl"
+
+#endif
---
+++ include/lowlevel/Size.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2006-2014 Christopho, Solarus - http://www.solarus-games.org
+ *
+ * Solarus is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Solarus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef SOLARUS_SIZE_H
+#define SOLARUS_SIZE_H
+
+#include "Common.h"
+#include <ostream>
+
+namespace solarus
+{
+
+/**
+ * \brief Represents a size.
+ *
+ * This class represents a generic size with
+ * width and height properties.
+ */
+struct Size {
+
+  ////////////////////////////////////////////////////////////
+  // Constructors
+
+  Size();
+  Size(int width, int height);
+
+  ////////////////////////////////////////////////////////////
+  // Operators
+
+  Size& operator+=(const Size& other);
+  Size& operator-=(const Size& other);
+
+  Size& operator*=(int factor);
+  Size& operator/=(int divisor);
+
+  ////////////////////////////////////////////////////////////
+  // Miscellaneous functions
+
+  bool is_flat() const;
+
+  ////////////////////////////////////////////////////////////
+  // Member data
+
+  int width = 0;    /**< Width of an object */
+  int height = 0;   /**< Height of an object */
+
+};
+
+////////////////////////////////////////////////////////////
+// Free functions
+
+// Comparison operators
+bool operator==(const Size& lhs, const Size& rhs);
+bool operator!=(const Size& lhs, const Size& rhs);
+
+// Arithmetic operators
+Size operator+(Size lhs, const Size& rhs);
+Size operator-(Size lhs, const Size& rhs);
+
+Size operator*(Size size, int factor);
+Size operator*(int factor, Size size);
+Size operator/(Size size, int divisor);
+
+// Output to stream
+std::ostream& operator<<(std::ostream& stream, const Size& size);
+
+}
+
+#include "Size.inl"
+
+#endif

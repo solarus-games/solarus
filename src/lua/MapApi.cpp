f7ae7cd253cc8255288ae2bce557149483405934
Morwenn <morwenn29@hotmail.fr>
2014-09-03 21:47:49 +0200

    Fire now takes a Point instead of a Rectangle.

--- src/lua/MapApi.cpp
+++ src/lua/MapApi.cpp
@@ -1959,7 +1959,7 @@ int LuaContext::map_api_create_fire(lua_State* l) {
   MapEntity* entity = new Fire(
       name,
       layer,
-      Rectangle(x, y));
+      Point(x, y));
   map.get_entities().add_entity(entity);
 
   if (map.is_started()) {

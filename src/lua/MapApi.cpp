58b33970a0f06db11af9660237a683d2a1a5d40f
Christopho <christopho@solarus-games.org>
2015-01-13 11:25:47 +0100

    Fix field name for shop treasure.

--- src/lua/MapApi.cpp
+++ src/lua/MapApi.cpp
@@ -902,7 +902,7 @@ int LuaContext::l_create_shop_treasure(lua_State* l) {
             entity_creation_check_savegame_variable_optional(l, 1, data, "treasure_savegame_variable")
         ),
         data.get_integer("price"),
-        data.get_string("dialog_id")
+        data.get_string("dialog")
     );
     if (entity == nullptr) {
       lua_pushnil(l);

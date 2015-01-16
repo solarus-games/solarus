solarus-games/solarus

commit 069ba3829a05510d6521e895f54d43c93d1e470d

Load equipment items when loading the savegame.
Otherwise it is easy to forget. It was not done from unit tests.

src/Savegame.cpp
@@ -98,6 +98,8 @@ Savegame::Savegame(MainLoop& main_loop, const std::string& file_name):
     empty = false;
     load();
   }
+
+  get_equipment().load_items();
 }
 
 /**

src/lua/GameApi.cpp
@@ -209,8 +209,6 @@ int LuaContext::game_api_load(lua_State* l) {
         get_lua_context(l).get_main_loop(), file_name
     );
 
-    savegame->get_equipment().load_items();
-
     push_game(l, *savegame);
     return 1;
   });

commit fc5e7c1fca708fc7ca3518e731b6b681a5324e91

Fix assertion error when closing the program from the title screen.
The bug was introduced by commit 069ba38.

include/solarus/Savegame.h
@@ -84,6 +84,7 @@ class SOLARUS_API Savegame: public ExportableToLua {
 
     // file state
     bool is_empty() const;
+    void initialize();
     void save();
     const std::string& get_file_name() const;
 
@@ -128,12 +129,12 @@ class SOLARUS_API Savegame: public ExportableToLua {
     std::map<std::string, SavedValue> saved_values;
 
     bool empty;
-    std::string file_name;           /**< Savegame file name relative to the quest write directory. */
+    std::string file_name;   /**< Savegame file name relative to the quest write directory. */
     MainLoop& main_loop;
     Equipment equipment;
-    Game* game;             /**< nullptr if this savegame is not currently running */
+    Game* game;              /**< nullptr if this savegame is not currently running */
 
-    void load();
+    void import_from_file();
     static int l_newindex(lua_State* l);
 
     void set_initial_values();

src/Savegame.cpp
@@ -79,10 +79,25 @@ const std::string Savegame::KEY_ABILITY_GET_BACK_FROM_DEATH =
  */
 Savegame::Savegame(MainLoop& main_loop, const std::string& file_name):
   ExportableToLua(),
+  empty(true),
   file_name(file_name),
   main_loop(main_loop),
   equipment(*this),
   game(nullptr) {
+
+  // Don't call initialize() manually because the shared_ptr does not exist
+  // at this point, but is needed by initialize() when calling item scripts.
+}
+
+/**
+ * \brief Initializes the data from the file or from initial values if the file
+ * does not exist.
+ *
+ * This function should be called before using the object.
+ * The constructor does not call it because it involves Lua calls to initialize
+ * the equipment.
+ */
+void Savegame::initialize() {
 
   const std::string& quest_write_dir = QuestFiles::get_quest_write_dir();
   Debug::check_assertion(!quest_write_dir.empty(),
@@ -96,7 +111,7 @@ Savegame::Savegame(MainLoop& main_loop, const std::string& file_name):
   else {
     // A save already exists, let's load it.
     empty = false;
-    load();
+    import_from_file();
   }
 
   get_equipment().load_items();
@@ -175,9 +190,9 @@ void Savegame::set_default_joypad_controls() {
 }
 
 /**
- * \brief Reads the data from the savegame file.
+ * \brief Import the savegame data from the file.
  */
-void Savegame::load() {
+void Savegame::import_from_file() {
 
   // Try to parse as Lua first.
   lua_State* l = luaL_newstate();

src/lua/GameApi.cpp
@@ -208,6 +208,7 @@ int LuaContext::game_api_load(lua_State* l) {
     std::shared_ptr<Savegame> savegame = std::make_shared<Savegame>(
         get_lua_context(l).get_main_loop(), file_name
     );
+    savegame->initialize();
 
     push_game(l, *savegame);
     return 1;

tests/src/test_tools/TestEnvironment.cpp
@@ -62,6 +62,7 @@ Game& TestEnvironment::get_game() {
     std::shared_ptr<Savegame> savegame = std::make_shared<Savegame>(
         main_loop, "save_initial.dat"
     );
+    savegame->initialize();
     savegame->set_string(Savegame::KEY_STARTING_MAP, map_id);
     Game* game = new Game(main_loop, savegame);
     main_loop.set_game(game);
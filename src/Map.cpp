41e33eb715dbf456e21733fae8aee2ac3ec17696
Christopho <christopho@solarus-games.org>
2015-08-19 13:09:55 +0200

    Restore commit "Improve the performance of custom entity collisions""
    This reverts commit 65f35ad69313070280a4827e20209318cc0df874.

--- src/Map.cpp
+++ src/Map.cpp
@@ -1138,11 +1138,15 @@ void Map::check_collision_from_detector(Detector& detector) {
   detector.check_collision(get_entities().get_hero());
 
   // Check each entity with this detector.
+  // TODO use a grid or a quadtree to only check entities nearby.
   const std::list<MapEntityPtr>& all_entities = entities->get_entities();
   for (const MapEntityPtr& entity: all_entities) {
 
     if (entity->is_enabled()
-        && !entity->is_being_removed()) {
+        && !entity->is_suspended()
+        && !entity->is_being_removed()
+        && entity.get() != &detector
+    ) {
       detector.check_collision(*entity);
     }
   }
@@ -1171,6 +1175,7 @@ void Map::check_collision_with_detectors(MapEntity& entity, Sprite& sprite) {
   for (Detector* detector: detectors) {
 
     if (!detector->is_being_removed()
+        && !detector->is_suspended()
         && detector->is_enabled()) {
       detector->check_collision(entity, sprite);
     }

5c1658b0550f7cfa338e171ecd84efcefc5bf98b
CitizenKyle <kylekipperman@gmail.com>
2015-11-03 14:32:34 -0800

    Updated cmake/AddCompilationFlags.cmake to generate better build files for MSVC.
    Got rid of flags that MSVC doesn't understand (-Werror -Wextra -pedantic) or ignores (-std=c++0x) and added /W4 instead.

--- cmake/AddCompilationFlags.cmake
+++ cmake/AddCompilationFlags.cmake
@@ -4,6 +4,8 @@
 if(MINGW)
   # To avoid a compilation error in vorbisfile.h with fseeko64.
   set(CMAKE_CXX_FLAGS "-std=gnu++11 ${CMAKE_CXX_FLAGS}")
+elseif(MSVC)
+  # c++11 Enabled by default.
 elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
   set(CMAKE_CXX_COMPILER "/usr/bin/clang++")
   set(CMAKE_CXX_FLAGS "-std=c++11 -stdlib=libc++ ${CMAKE_CXX_FLAGS}")
@@ -21,20 +23,25 @@ endif()
 
 # Warnings and errors.
 
-# Be less pedantic in release builds for users.
-set(CMAKE_CXX_FLAGS_RELEASE "-Wno-error -Wno-all -Wno-unknown-pragmas -Wno-fatal-errors -Wno-extra ${CMAKE_CXX_FLAGS_RELEASE}")
+if (MSVC)
+  # Only show useful warnings in MSVC.
+  set(CMAKE_CXX_FLAGS_DEBUG "/W4 ${CMAKE_CXX_FLAGS_DEBUG}")
+else()
+  # Be less pedantic in release builds for users.
+  set(CMAKE_CXX_FLAGS_RELEASE "-Wno-error -Wno-all -Wno-unknown-pragmas -Wno-fatal-errors -Wno-extra ${CMAKE_CXX_FLAGS_RELEASE}")
 
-# Be more pedantic in debug mode for developers.
-set(CMAKE_CXX_FLAGS_DEBUG "-Wall -Werror -Wextra -pedantic ${CMAKE_CXX_FLAGS_DEBUG}")
+  # Be more pedantic in debug mode for developers.
+  set(CMAKE_CXX_FLAGS_DEBUG "-Wall -Werror -Wextra -pedantic ${CMAKE_CXX_FLAGS_DEBUG}")
+endif()
 
 # Platform-specific flags.
 if(WIN32)
-  # Windows: disable the console by default.
-  #if(MSVC)
-  #  set_target_properties(SOLARUS_ENGINE PROPERTIES LINK_FLAGS_RELEASE "/SUBSYSTEM:WINDOWS")
-  #elseif(CMAKE_COMPILER_IS_GNUCXX)
-  #  set(CMAKE_CXX_FLAGS "-mwindows ${CMAKE_CXX_FLAGS}")
-  #endif()
+  #windows: disable the console by default.
+  if(MSVC)
+   #set_target_properties(solarus_engine properties link_flags_release "/subsystem:windows")
+  elseif(cmake_compiler_is_gnucxx)
+   set(cmake_cxx_flags "-mwindows ${cmake_cxx_flags}")
+  endif()
 endif()
 
 if(GCWZERO)

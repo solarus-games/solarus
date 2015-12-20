1d5f500f732fbe29af563c277da9bb220fa4ad2f
Christopho <christopho@solarus-games.org>
2015-12-20 11:13:53 +0100

    Add checks to unit test

--- tests/testing_quest/data/maps/bugs/794_custom_entity_set_can_traverse_ground.lua
+++ tests/testing_quest/data/maps/bugs/794_custom_entity_set_can_traverse_ground.lua
@@ -39,9 +39,11 @@ function map:on_started()
     ground_entity:set_modified_ground(ground)
 
     tested_entity:set_can_traverse_ground(ground, true)
+    assert(tested_entity:can_traverse_ground(ground))
     assert(not tested_entity:test_obstacles(0, 0))
 
     tested_entity:set_can_traverse_ground(ground, false)
+    assert(not tested_entity:can_traverse_ground(ground))
     assert(tested_entity:test_obstacles(0, 0))
   end
 

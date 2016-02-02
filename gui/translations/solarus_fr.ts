6457146a53b3d7845ccb1145c1097493d77d012e
Christopho <christopho@solarus-games.org>
2016-02-02 15:20:15 +0100

    Add French translation

---
+++ gui/translations/solarus_fr.ts
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>SolarusGui::Console</name>
+    <message>
+        <location filename="../src/console.ui" line="42"/>
+        <source>Run Lua code to the quest</source>
+        <translation>Exécuter du code Lua dans la quête</translation>
+    </message>
+</context>
+<context>
+    <name>SolarusGui::MainWindow</name>
+    <message>
+        <location filename="../src/main_window.ui" line="14"/>
+        <source>Solarus</source>
+        <translation>Solarus</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.ui" line="25"/>
+        <source>Quests</source>
+        <translation>Quêtes</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.ui" line="38"/>
+        <source>Add quest</source>
+        <translation>Ajouter une quête</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.ui" line="55"/>
+        <location filename="../src/main_window.ui" line="222"/>
+        <source>Remove quest</source>
+        <translation>Retirer la quête</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.ui" line="116"/>
+        <location filename="../src/main_window.ui" line="210"/>
+        <source>Play</source>
+        <translation>Jouer</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.ui" line="143"/>
+        <source>Console</source>
+        <translation>Console</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.ui" line="166"/>
+        <source>File</source>
+        <translation>Fichier</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.ui" line="175"/>
+        <source>Quest</source>
+        <translation>Quête</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.ui" line="183"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.ui" line="196"/>
+        <source>Add quest...</source>
+        <translation>Ajouter une quête...</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.ui" line="201"/>
+        <source>Exit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.ui" line="213"/>
+        <source>F5</source>
+        <translation>F5</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.ui" line="231"/>
+        <source>Stop</source>
+        <translation>Arrêter</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.cpp" line="104"/>
+        <source>Solarus %1</source>
+        <translation>Solarus %1</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.cpp" line="138"/>
+        <source>A quest is playing</source>
+        <translation>Une quête est en cours</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.cpp" line="139"/>
+        <source>A quest is playing. Do you really want to exit Solarus?</source>
+        <translation>Une quête est en cours. Voulez-vous vraiment quitter Solarus ?</translation>
+    </message>
+    <message>
+        <location filename="../src/main_window.cpp" line="167"/>
+        <source>Select quest directory</source>
+        <translation>Sélectionnez le dossier de la quête</translation>
+    </message>
+</context>
+<context>
+    <name>SolarusGui::QuestRunner</name>
+    <message>
+        <location filename="../src/quest_runner.cpp" line="145"/>
+        <source>Failed to run quest</source>
+        <translation>Le lancement de la quête a échoué</translation>
+    </message>
+    <message>
+        <location filename="../src/quest_runner.cpp" line="146"/>
+        <source>Cannot start quest process: no program name</source>
+        <translation>Impossible de démarrer le processus de la quête : nom du programme manquant</translation>
+    </message>
+</context>
+</TS>

0b7ce6a019206bc55b63cf048e1c4af15b685896
Christopho <christopho@solarus-games.org>
2016-04-05 23:24:10 +0200

    Fix scrolling when the camera does not occupy the whole screen

--- src/Game.cpp
+++ src/Game.cpp
@@ -486,9 +486,9 @@ void Game::update_transitions() {
 
         // before closing the map, draw it on a backup surface for transition effects
         // that want to display both maps at the same time
-        if (needs_previous_surface) {
+        if (needs_previous_surface && current_map->get_camera() != nullptr) {
           previous_map_surface = Surface::create(
-              Video::get_quest_size()
+              current_map->get_camera()->get_size()
           );
           previous_map_surface->set_software_destination(false);
           current_map->draw();
@@ -566,6 +566,7 @@ void Game::draw(const SurfacePtr& dst_surface) {
 
   // Draw the map.
   if (current_map->is_loaded()) {
+    dst_surface->fill_with_color(current_map->get_tileset().get_background_color());
     current_map->draw();
     const CameraPtr& camera = current_map->get_camera();
     if (camera != nullptr) {
@@ -573,7 +574,7 @@ void Game::draw(const SurfacePtr& dst_surface) {
       if (transition != nullptr) {
         transition->draw(*camera_surface);
       }
-      camera_surface->draw(dst_surface);
+      camera_surface->draw(dst_surface, camera->get_position_on_screen());
     }
 
     // Draw the built-in dialog box if any.

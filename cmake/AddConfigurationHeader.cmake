f1555f0b2997587e7ac08f1d0e6b28b91a725af1
Tommy Savaria <Tommy.Savaria@USherbrooke.ca>
2016-04-20 07:56:13 -0400

    Modified according to christopho's suggestions

--- cmake/AddConfigurationHeader.cmake
+++ cmake/AddConfigurationHeader.cmake
@@ -2,15 +2,16 @@
 include(CheckFunctionExists)
 check_function_exists(mkstemp MKSTEMP_FUNCTION_EXIST)
 
-include(CheckCxxSymbolExists)
+include(CheckCXXSymbolExists)
 check_cxx_symbol_exists(mkstemp "cstdlib" MKSTEMP_SYMBOL_EXIST)
-
-include(CheckIncludeFiles)
-check_include_files(unistd.h HAVE_UNISTD_H)
 
 if(MKSTEMP_FUNCTION_EXIST AND MKSTEMP_SYMBOL_EXIST)
 	set(HAVE_MKSTEMP)
 endif()
+
+include(CheckIncludeFiles)
+check_include_files(unistd.h HAVE_UNISTD_H)
+
 
 configure_file("${CMAKE_SOURCE_DIR}/include/solarus/config.h.in" "${CMAKE_BINARY_DIR}/include/solarus/config.h")
 

b3406d4c17cbf193793af00776e9be1c247f8bfc
Christopho <christopho@solarus-games.org>
2016-06-28 16:11:26 +0200

    Continue teletransportation unit tests

---
+++ tests/testing_quest/data/maps/teletransportation_tests/main.lua
@@ -0,0 +1,40 @@
+local map = ...
+local game = map:get_game()
+local hero = map:get_hero()
+
+local test_maps = {
+  --"start_in_hole",
+  "start_in_deep_water",
+}
+
+function hero:assert_position_equal(other)
+
+  local x_1, y_1, layer_1 = hero:get_position()
+  local x_2, y_2, layer_2 = other:get_position()
+  assert(x_1 == x_2)
+  assert(y_1 == y_2)
+  assert(layer_1 == layer_2)
+end
+
+function hero:assert_state_ground_animation(state, ground, animation)
+
+  assert_equal(hero:get_state(), state)
+  assert_equal(hero:get_ground_below(), ground)
+  assert_equal(hero:get_sprite("tunic"):get_animation(), animation)
+end
+
+function map:on_started()
+
+  sol.timer.start(game, 5000, function()
+    if #test_maps == 0 then
+      -- Finished.
+      sol.main.exit()
+    else
+      -- Next map in the list.
+      local next_map_id = test_maps[1]
+      table.remove(test_maps, 1)
+      hero:teleport("teletransportation_tests/" .. next_map_id)
+      return true  -- Repeat the timer.
+    end
+  end)
+end
--- tests/testing_quest/data/maps/teletransportation_tests/start_in_hole.lua
+++ tests/testing_quest/data/maps/teletransportation_tests/start_in_hole.lua
@@ -6,9 +6,18 @@ local game = map:get_game()
 
 function map:on_started()
 
-  assert(hero:get_state() == "free")
+  hero:assert_position_equal(hero, destination_1)
+  hero:assert_state_ground_animation("free", "hole", "stopped")
 end
 
 function map:on_opening_transition_finished()
-  assert(hero:get_state() == "falling")
+
+  hero:assert_position_equal(hero, destination_1)
+  hero:assert_state_ground_animation("falling", "hole", "falling")
+
+  sol.timer.start(500, function()
+    
+    hero:assert_position_equal(hero, destination_2)
+    hero:assert_state_ground_animation("falling", "hole", "falling")
+  end)
 end

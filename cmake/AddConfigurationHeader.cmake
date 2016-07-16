f7f24d3ba97dfc42452616514ded8bcf557066a1
Christopho <christopho@solarus-games.org>
2016-07-16 12:50:24 +0200

    Remove debug messages

--- cmake/AddConfigurationHeader.cmake
+++ cmake/AddConfigurationHeader.cmake
@@ -1,11 +1,9 @@
 # Generate config.h with useful configuration info.
 include(CheckFunctionExists)
 check_function_exists(mkstemp MKSTEMP_FUNCTION_EXIST)
-message("function: ${MKSTEMP_FUNCTION_EXIST}")
 
 include(CheckCXXSymbolExists)
 check_cxx_symbol_exists(mkstemp "cstdlib" MKSTEMP_SYMBOL_EXIST)
-message("symbol: ${MKSTEMP_FUNCTION_EXIST}")
 
 if(MKSTEMP_FUNCTION_EXIST AND MKSTEMP_SYMBOL_EXIST)
   set(HAVE_MKSTEMP 1)

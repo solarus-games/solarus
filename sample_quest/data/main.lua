fe9621fd37184f3502ece5fcce2a66d490f9a2c6
Christopho <christopho@solarus-games.org>
2016-07-22 03:33:24 +0200

    Add keyboard shortcuts to change the video mode in the sample quest

--- sample_quest/data/main.lua
+++ sample_quest/data/main.lua
@@ -1,15 +1,19 @@
 -- This is the main Lua script of your project.
 -- You will probably make a title screen and then start a game.
--- See the Lua API! http://www.solarus-games.org/solarus/documentation/
+-- See the Lua API! http://www.solarus-games.org/doc/latest
 
 local game_manager = require("scripts/game_manager")
 
+-- This function is called when Solarus starts.
 function sol.main:on_started()
-  -- This function is called when Solarus starts.
+
   print("This is a sample quest for Solarus.")
 
   -- Setting a language is useful to display text and dialogs.
   sol.language.set_language("en")
+
+  -- Set the window title.
+  sol.video.set_window_title("Sample quest - Solarus " .. sol.main.get_solarus_version())
 
   -- Show the Solarus logo initially.
   local solarus_logo = require("menus/solarus_logo")
@@ -22,3 +26,28 @@ function sol.main:on_started()
 
 end
 
+-- Event called when the player pressed a keyboard key.
+function sol.main:on_key_pressed(key, modifiers)
+
+  local handled = false
+  if key == "f5" then
+    -- F5: change the video mode.
+    sol.video.switch_mode()
+    handled = true
+  elseif key == "f11" or
+    (key == "return" and (modifiers.alt or modifiers.control)) then
+    -- F11 or Ctrl + return or Alt + Return: switch fullscreen.
+    sol.video.set_fullscreen(not sol.video.is_fullscreen())
+    handled = true
+  elseif key == "f4" and modifiers.alt then
+    -- Alt + F4: stop the program.
+    sol.main.exit()
+    handled = true
+  elseif key == "escape" and sol.main.game == nil then
+    -- Escape in title screens: stop the program.
+    sol.main.exit()
+    handled = true
+  end
+
+  return handled
+end

274756ede04c204768d83d54fb7fa41552828ee4
val <valentin.soudier@gmail.com>
2016-09-04 11:49:02 +0200

    Make Solarus runs on iOS

--- src/lowlevel/apple/AppleInterface.mm
+++ src/lowlevel/apple/AppleInterface.mm
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License along
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+#include "solarus/lowlevel/Debug.h"
 #include "solarus/lowlevel/apple/AppleInterface.h"
 
 #if defined(SOLARUS_OSX) || defined(SOLARUS_IOS)
@@ -27,29 +28,29 @@
 
 
 /**
- * @brief Return "~/Library/Application Support" or equivalent from the official
- * way, which is available in OSX 10.6+ and iOS 4.0+.
+ * @brief Create and return the Application Support folder from the User Domain
+ * which doesn't exist by default in the iOS sandboxed environment.
  *
- * Return an OSX 10.0+ and iOS 1.0+ (not compatible with Iphone Simulator)
- * hardcoded equivalent workaround if the build configuration is set for a
- * lower minimum version.
  * @return The Application Support folder from the User Domain.
  */
 std::string get_user_application_support_directory()
 {
   @autoreleasepool {
 
-#if defined(SOLARUS_OSX) && __MAC_OS_X_VERSION_MIN_REQUIRED  >= MAC_OS_X_VERSION_10_6 \
- || defined(SOLARUS_IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_4_0
-    return [[[[[NSFileManager defaultManager]
-               URLsForDirectory:NSApplicationSupportDirectory inDomains:NSUserDomainMask]
-              objectAtIndex:0]
-             path]
-            UTF8String];
-#else
-    return [[NSHomeDirectory() stringByAppendingPathComponent:@"Library/Application Support"]
-            UTF8String];
-#endif
+    NSString *appSupportDirectory = [NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES)
+        objectAtIndex:0];
+
+    // Create the Application Support folder if it doesn't exist yet.
+    if (![[NSFileManager defaultManager] fileExistsAtPath:appSupportDirectory isDirectory:NULL]) {
+
+      NSError *error = nil;
+      if (![[NSFileManager defaultManager]
+            createDirectoryAtPath:appSupportDirectory withIntermediateDirectories:YES attributes:nil error:&error]) {
+        Solarus::Debug::error("Cannot create " + std::string([appSupportDirectory UTF8String]));
+      }
+    }
+
+    return [appSupportDirectory UTF8String];
   }
 }
 

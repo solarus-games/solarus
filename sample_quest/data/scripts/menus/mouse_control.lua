88d4b5561e58d8fdcb9b0b6465d8ab665e7e0264
Christopho <christopho@solarus-games.org>
2016-09-22 10:44:28 +0200

    Make mouse control code readable

--- sample_quest/data/scripts/menus/mouse_control.lua
+++ sample_quest/data/scripts/menus/mouse_control.lua
@@ -8,11 +8,11 @@ function mouse_control:create(game, hud)
   self.is_mouse_button_pushed = false
 
   -- Movement of the hero.
-  self.directions = {
-      {"right", false},
-      {"up", false},
-      {"left", false},
-      {"down", false}
+  self.directions_pressed = {
+      right = false,
+      up = false,
+      left = false,
+      down = false
   }
   self.deadzone_ray = 10 -- Should be set to the max number of pixel that the hero can move in one frame.
 end
@@ -29,53 +29,55 @@ end
 
 function mouse_control:on_update()
 
-  if self.game ~= nil and self.game:get_map() ~= nil then
-    if self.is_mouse_button_pushed then
+  if self.game == nil or self.game:get_map() == nil then
+    return
+  end
 
-      local mouse_position = {sol.input.get_mouse_position()}
+  if self.is_mouse_button_pushed then
 
-      -- If the mouse position is not over a button, move the hero.
-      if self.hud ~= nil and not self.hud:has_button_pushed() then
-        local hero_position = {self.game:get_hero():get_position()}
+    local mouse_x, mouse_y = sol.input.get_mouse_position()
 
-        -- Compare the position of the hero and the mouse
-        -- and simulate the appropriate command for each directions.
-        self:update_direction(1, mouse_position[1] > hero_position[1] + self.deadzone_ray)
-        self:update_direction(2, mouse_position[2] < hero_position[2] - self.deadzone_ray)
-        self:update_direction(3, mouse_position[1] < hero_position[1] - self.deadzone_ray)
-        self:update_direction(4, mouse_position[2] > hero_position[2] + self.deadzone_ray)
-      end
-    else
+    -- If the mouse position is not over a button, move the hero.
+    if self.hud ~= nil and not self.hud:has_button_pushed() then
+      local hero_x, hero_y = self.game:get_hero():get_position()
 
-      for i=1, 4, 1 do
-        self:stop_direction(i)
-      end
+      -- Compare the position of the hero and the mouse
+      -- and simulate the appropriate command for each directions.
+      self:update_direction("right", mouse_x > hero_x + self.deadzone_ray)
+      self:update_direction("up", mouse_y < hero_y - self.deadzone_ray)
+      self:update_direction("left", mouse_x < hero_x - self.deadzone_ray)
+      self:update_direction("down", mouse_y > hero_y + self.deadzone_ray)
+    end
+  else
+
+    for direction, _ in pairs(self.directions_pressed) do
+      self:stop_direction(direction)
     end
   end
 end
 
-function mouse_control:update_direction(direction_id, is_movement_needed)
+function mouse_control:update_direction(direction, is_movement_needed)
 
   if is_movement_needed then
-    self:start_direction(direction_id)
+    self:start_direction(direction)
   else
-    self:stop_direction(direction_id)
+    self:stop_direction(direction)
   end
 end
 
-function mouse_control:start_direction(direction_id)
+function mouse_control:start_direction(direction)
 
-  if not self.directions[direction_id][2] then
-    self.directions[direction_id][2] = true;
-    self.game:simulate_command_pressed(self.directions[direction_id][1])
+  if not self.directions_pressed[direction] then
+    self.directions_pressed[direction] = true
+    self.game:simulate_command_pressed(direction)
   end
 end
 
-function mouse_control:stop_direction(direction_id)
+function mouse_control:stop_direction(direction)
 
-  if self.directions[direction_id][2] then
-    self.directions[direction_id][2] = false;
-    self.game:simulate_command_released(self.directions[direction_id][1])
+  if self.directions_pressed[direction] then
+    self.directions_pressed[direction] = false
+    self.game:simulate_command_released(direction)
   end
 end
 

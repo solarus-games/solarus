1d5cb589cc532e477d784907e776d5ce3d6fed3a
orbea <ovariegata@yahoo.com>
2017-08-07 19:19:10 -0700

    Remove SOLARUS_BUNDLE in cmake/AddInstallTargets.cmake

--- cmake/AddInstallTargets.cmake
+++ cmake/AddInstallTargets.cmake
@@ -17,22 +17,14 @@ set(SOLARUS_MANUAL_INSTALL_DESTINATION "share/man" CACHE PATH "Manual install de
 set(SOLARUS_HEADERS_INSTALL_DESTINATION "include" CACHE PATH "Headers install destination")
 
 # Files to install with make install.
-if(SOLARUS_BUNDLE)
-  # Install the bundle if requested.
-  install(TARGETS solarus-run
-    BUNDLE DESTINATION ${SOLARUS_EXECUTABLE_INSTALL_DESTINATION}
-  )
-else()
-  # Install the shared library and the solarus-run executable.
-  install(TARGETS solarus solarus-run
-    LIBRARY DESTINATION ${SOLARUS_LIBRARY_INSTALL_DESTINATION}
-    RUNTIME DESTINATION ${SOLARUS_EXECUTABLE_INSTALL_DESTINATION}
-  )
-  # Install headers: useful for projects that use Solarus as a library.
-  install(DIRECTORY
-    "${CMAKE_BINARY_DIR}/include/solarus"  # For config.h.
-    "${CMAKE_SOURCE_DIR}/include/solarus"
-    DESTINATION ${SOLARUS_HEADERS_INSTALL_DESTINATION}
-  )
-endif()
-
+# Install the shared library and the solarus-run executable.
+install(TARGETS solarus solarus-run
+  LIBRARY DESTINATION ${SOLARUS_LIBRARY_INSTALL_DESTINATION}
+  RUNTIME DESTINATION ${SOLARUS_EXECUTABLE_INSTALL_DESTINATION}
+)
+# Install headers: useful for projects that use Solarus as a library.
+install(DIRECTORY
+  "${CMAKE_BINARY_DIR}/include/solarus"  # For config.h.
+  "${CMAKE_SOURCE_DIR}/include/solarus"
+  DESTINATION ${SOLARUS_HEADERS_INSTALL_DESTINATION}
+)

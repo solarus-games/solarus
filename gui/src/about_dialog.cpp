d4ba8719848cf44936cec411226e7fc1a3ec2ceb
Christopho <christopho@solarus-games.org>
2018-01-05 14:57:30 +0100

    Update copyright year

--- gui/src/about_dialog.cpp
+++ gui/src/about_dialog.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2016 Christopho, Solarus - http://www.solarus-games.org
+ * Copyright (C) 2006-2018 Christopho, Solarus - http://www.solarus-games.org
  *
  * Solarus is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

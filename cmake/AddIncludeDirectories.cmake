457fd9e57c3466d268a5b40a502f0242521b7a33
Christopho <christopho@solarus-games.org>
2018-05-21 21:45:40 -0700

    Fix cmake config for Windows

--- cmake/AddIncludeDirectories.cmake
+++ cmake/AddIncludeDirectories.cmake
@@ -1,27 +1,31 @@
 # Include paths.
 include_directories(
-	"${CMAKE_BINARY_DIR}/include"  # For config.h.
-	"${CMAKE_SOURCE_DIR}/include"
+    "${CMAKE_BINARY_DIR}/include"  # For config.h.
+    "${CMAKE_SOURCE_DIR}/include"
 )
 
 # Third-party include paths, marked as system ones to disable their warnings.
 include_directories(SYSTEM
-	"${CMAKE_SOURCE_DIR}/include/solarus/third_party"
-	"${CMAKE_SOURCE_DIR}/include/solarus/third_party/snes_spc"
+    "${CMAKE_SOURCE_DIR}/include/solarus/third_party"
+    "${CMAKE_SOURCE_DIR}/include/solarus/third_party/snes_spc"
 )
-
+message("LuaJIT ${LUA_INCLUDE_DIR}")
 # External include paths, marked as system ones to disable their warnings.
 include_directories(SYSTEM
-	"${MODPLUG_INCLUDE_DIR}"  # Before SDL2 because we want the sndfile.h of ModPlug.
-	"${SDL2_INCLUDE_DIR}"
-	"${SDL2_TTF_INCLUDE_DIR}"
-	"${SDL2_IMAGE_INCLUDE_DIR}"
-	"${OPENGL_INCLUDE_DIR}"
-	"${OPENAL_INCLUDE_DIR}"
-	"${VORBISFILE_INCLUDE_DIR}"
-	"${OGG_INCLUDE_DIR}"
-	"${LUA_INCLUDE_DIR}"
-	"${PHYSFS_INCLUDE_DIR}"
-	"${GLM_INCLUDE_DIRS}"
+        "${MODPLUG_INCLUDE_DIRS}"  # Before SDL2 because we want the sndfile.h of ModPlug.
+        "${SDL2_INCLUDE_DIR}"
+        "${SDL2_TTF_INCLUDE_DIR}"
+        "${SDL2_IMAGE_INCLUDE_DIR}"
+        "${OPENAL_INCLUDE_DIR}"
+        "${VORBISFILE_INCLUDE_DIR}"
+        "${OGG_INCLUDE_DIR}"
+        "${LUA_INCLUDE_DIR}"
+        "${PHYSFS_INCLUDE_DIR}"
+        "${GLM_INCLUDE_DIRS}"
 )
 
+if (OPENGL_INCLUDE_DIR)
+    include_directories(SYSTEM
+        ${OPENGL_INCLUDE_DIR}
+    )
+endif()
--- cmake/modules/FindOgg.cmake
+++ cmake/modules/FindOgg.cmake
@@ -1,14 +1,9 @@
 # - Find ogg
 # Find the native ogg includes and libraries
 #
-#  OGG_INCLUDE_DIR - where to find ogg.h, etc.
-#  OGG_LIBRARIES   - List of libraries when using libogg.
-#  OGG_FOUND       - True if ogg was found.
-
-if(OGG_INCLUDE_DIR)
-    # Already in cache, be silent
-    set(OGG_FIND_QUIETLY TRUE)
-endif(OGG_INCLUDE_DIR)
+#  OGG_INCLUDE_DIRS - where to find ogg.h, etc.
+#  OGG_LIBRARIES    - List of libraries when using libogg.
+#  OGG_FOUND        - True if ogg was found.
 
 find_path(OGG_INCLUDE_DIR ogg.h
     HINTS /usr/local/angstrom/arm/arm-angstrom-linux-gnueabi/usr
@@ -16,16 +11,16 @@ find_path(OGG_INCLUDE_DIR ogg.h
 
 find_library(OGG_LIBRARY NAMES ogg)
 
-# Handle the QUIETLY and REQUIRED arguments and set OGG_FOUND to TRUE if
-# all listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(OGG DEFAULT_MSG
-    OGG_INCLUDE_DIR OGG_LIBRARY)
+find_package_handle_standard_args(OGG
+    FOUND_VAR OGG_FOUND
+    REQUIRED_VARS
+        OGG_INCLUDE_DIR
+        OGG_LIBRARY)
 
 if(OGG_FOUND)
-  set(OGG_LIBRARIES ${OGG_LIBRARY})
-else(OGG_FOUND)
-  set(OGG_LIBRARIES)
-endif(OGG_FOUND)
+    set(OGG_INCLUDE_DIRS ${OGG_INCLUDE_DIR})
+    set(OGG_LIBRARIES ${OGG_LIBRARY})
+endif()
 
 mark_as_advanced(OGG_INCLUDE_DIR OGG_LIBRARY)
